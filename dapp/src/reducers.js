51239b651d50e9e14ee84b3bbf7d8424ec04c306
Atmosfearful <atmosfearful@protonmail.com>
2021-10-19 16:22:49 -0700

    fix deep merge state in reducers

--- dapp/src/reducers.js
+++ dapp/src/reducers.js
@@ -1,16 +1,28 @@
 import { Actions } from "./constants";
 
+const deepMergeState = (payload, oldState) => {
+  return {
+    ...oldState,
+    bonding: { ...oldState.bonding, ...payload.bonding },
+    balances: { ...oldState.balances, ...payload.balances },
+    staking: { ...oldState.staking, ...payload.staking },
+    migrate: { ...oldState.migrate, ...payload.migrate },
+    exercise: { ...oldState.exercise, ...payload.exercise },
+  };
+};
+
 export function app(state = {}, action) {
   switch (action.type) {
     case Actions.FETCH_APP_SUCCESS:
+      return { ...state, ...action.payload };
     case Actions.FETCH_ACCOUNT_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_STAKE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_MIGRATE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_EXERCISE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     default:
       return state;
   }
