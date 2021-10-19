3b86309711131ab631142c8b52dbbe561424baf8
Atmosfearful <atmosfearful@protonmail.com>
2021-10-19 15:53:04 -0700

    Deeply merge state in reducer

--- dapp/src/reducers.js
+++ dapp/src/reducers.js
@@ -1,16 +1,29 @@
 import { Actions } from "./constants";
 
+const deepMergeState = (payload, oldState) => {
+  return ["balances", "staking", "migrate", "bonding", "exercise"].reduce((newState, prop) => {
+    return {
+      ...newState,
+      [prop]: {
+        ...oldState[prop],
+        ...payload[prop],
+      },
+    };
+  }, {});
+};
+
 export function app(state = {}, action) {
   switch (action.type) {
     case Actions.FETCH_APP_SUCCESS:
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_ACCOUNT_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_STAKE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_MIGRATE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     case Actions.FETCH_EXERCISE_SUCCESS:
-      return { ...state, ...action.payload };
+      return deepMergeState(action.payload, state);
     default:
       return state;
   }
