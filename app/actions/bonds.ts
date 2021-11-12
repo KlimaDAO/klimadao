da2a71ea6ace5b0ceaba4b9d9388cccfa8b5237b
Atmosfearful <Atmosfearful@protonmail.com>
2021-11-12 10:13:36 -0800

    fix bond contract addresses

--- app/actions/bonds.ts
+++ app/actions/bonds.ts
@@ -1,16 +1,16 @@
 import { ethers, providers } from "ethers";
 import { Thunk } from "state";
-import { addresses, Bond } from "@klimadao/lib/constants";
+import { setBond } from "state/bonds";
+import { OnStatusHandler } from "./utils";
+import { setBondAllowance } from "state/user";
 
+import { formatUnits } from "@klimadao/lib/utils";
+import { addresses, Bond } from "@klimadao/lib/constants";
 import Depository from "@klimadao/lib/abi/KlimaBondDepository_Regular.json";
 import PairContract from "@klimadao/lib/abi/PairContract.json";
 import BondCalcContract from "@klimadao/lib/abi/BondCalcContract.json";
 import OhmDai from "@klimadao/lib/abi/OhmDai.json";
 import IERC20 from "@klimadao/lib/abi/IERC20.json";
-import { setBond } from "state/bonds";
-import { OnStatusHandler } from "./utils";
-import { setBondAllowance } from "state/user";
-import { formatUnits } from "@klimadao/lib/utils";
 
 export const DEFAULT_QUOTE_SLP = "0.001"; // Use a realistic SLP ownership so we have a quote before the user inputs any value
 
@@ -165,9 +165,9 @@ export const changeApprovalTransaction = async (params: {
       ),
     }[params.bond];
     const approvalAddress = {
-      klima_bct_lp: addresses["mainnet"].klimaBctLp,
-      bct: addresses["mainnet"].bct,
-      bct_usdc_lp: addresses["mainnet"].bctUsdcLp,
+      klima_bct_lp: addresses["mainnet"].bond_klimaBctLp,
+      bct: addresses["mainnet"].bond_bct,
+      bct_usdc_lp: addresses["mainnet"].bond_bctUsdcLp,
     }[params.bond];
     const value = ethers.utils.parseUnits("1000000000", "ether");
     params.onStatus("userConfirmation");
@@ -218,21 +218,21 @@ export const calculateUserBondDetails = (params: {
     if (params.bond === "klima_bct_lp") {
       allowance = await reserveContract.allowance(
         params.address,
-        addresses["mainnet"].klimaBctLp
+        addresses["mainnet"].bond_klimaBctLp
       );
       balance = await reserveContract.balanceOf(params.address);
       balance = ethers.utils.formatUnits(balance, "ether");
     } else if (params.bond === "bct") {
       allowance = await reserveContract.allowance(
         params.address,
-        addresses["mainnet"].bct
+        addresses["mainnet"].bond_bct
       );
       balance = await reserveContract.balanceOf(params.address);
       balance = ethers.utils.formatEther(balance);
     } else if (params.bond === "bct_usdc_lp") {
       allowance = await reserveContract.allowance(
         params.address,
-        addresses["mainnet"].bctUsdcLp
+        addresses["mainnet"].bond_bctUsdcLp
       );
       balance = await reserveContract.balanceOf(params.address);
       balance = ethers.utils.formatEther(balance);
@@ -266,9 +266,9 @@ export const bondTransaction = async (params: {
   try {
     const signer = params.provider.getSigner();
     const contractAddress = {
-      klima_bct_lp: addresses["mainnet"].klimaBctLp,
-      bct: addresses["mainnet"].bct,
-      bct_usdc_lp: addresses["mainnet"].bctUsdcLp,
+      klima_bct_lp: addresses["mainnet"].bond_klimaBctLp,
+      bct: addresses["mainnet"].bond_bct,
+      bct_usdc_lp: addresses["mainnet"].bond_bctUsdcLp,
     }[params.bond];
     const contract = new ethers.Contract(
       contractAddress,
@@ -309,9 +309,9 @@ export const redeemTransaction = async (params: {
     const autostake = false;
     const signer = params.provider.getSigner();
     const contractAddress = {
-      klima_bct_lp: addresses["mainnet"].klimaBctLp,
-      bct: addresses["mainnet"].bct,
-      bct_usdc_lp: addresses["mainnet"].bctUsdcLp,
+      klima_bct_lp: addresses["mainnet"].bond_bctUsdcLp,
+      bct: addresses["mainnet"].bond_bct,
+      bct_usdc_lp: addresses["mainnet"].bond_bctUsdcLp,
     }[params.bond];
     const contract = new ethers.Contract(
       contractAddress,
