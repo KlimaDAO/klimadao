0ec9afb6f3118bc7b47524555371d908549dd230
rstycedar <rstycedar@protonmail.com>
2021-12-10 14:40:55 -0800

    change VERCEL_URL to NEXT_PUBLIC_VERCEL_URL

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,8 +1,6 @@
 export const IS_PRODUCTION =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 
-const VERCEL_URL = process.env.VERCEL_URL;
-
-export const BASE_URL = VERCEL_URL
-  ? `https://${VERCEL_URL}`
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3001";
