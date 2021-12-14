5bffa060f20d7fcbf155fe285afb9d43b6fed006
Atmosfearful <atmosfearful@protonmail.com>
2021-12-14 13:11:26 -0800

    migrate breakpoints and typography to Emotion

---
+++ lib/theme/breakpoints.ts
@@ -0,0 +1,6 @@
+export default {
+  small: "@media (min-width: 32.5rem)",
+  medium: "@media (min-width: 57.6rem)",
+  large: "@media (min-width: 76.8rem)",
+  desktop: "@media (min-width: 99.2rem)",
+};
