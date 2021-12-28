165016272afd1754f262a5c11a9f131dcc445e1a
biwano <biwano@gmail.com>
2021-12-29 00:59:11 +0100

    Translation framework (#43)
    * update package-lock files
    
    * update gitignores
    
    * i18n minimal
    
    * Fixed newline add end of file in config files
    
    * Trying to add a postinstall script to compile translations so the application deploys properly on Vercel
    
    * Nicer localization menu
    
    * Translated Home
    
    * Save selected language in LocalStorage
    
    * Translation: Stake page
    
    * Updated translation files
    
    * Optimized package.json dependencies
    
    * Rebased with klima/staging
    
    * Made files pretty.
    Updated .prettierignore
    
    * Commiting compiled translation files
    
    * Fixed merge of .prettierignore
    
    * Commited package-lock.json
    
    Co-authored-by: Atmosfearful <atmosfearful@protonmail.com>
    Co-authored-by: Bruno Ilponse <bruno.ilponse@gmail.com>
    Co-authored-by: bilponse <bilponse@localhost.localdomain>

--- app/lib/i18n.ts
+++ app/lib/i18n.ts
@@ -1,9 +1,12 @@
 import { i18n } from "@lingui/core";
 import { en, fr } from "make-plural/plurals";
+<<<<<<< HEAD
 import enTime from "time-delta/locales/en";
 import frTime from "time-delta/locales/fr";
 import * as timeDelta from "time-delta";
 import { prettifySeconds as prettifySecondsLib } from "@klimadao/lib/utils";
+=======
+>>>>>>> 80779ba... Translation framework (#43)
 
 interface ILocales {
   [locale: string]: {
@@ -11,18 +14,30 @@ interface ILocales {
       n: number | string,
       ord?: boolean
     ) => "zero" | "one" | "two" | "few" | "many" | "other";
+<<<<<<< HEAD
     time: any;
   };
 }
 const locales: ILocales = {
   en: { plurals: en, time: enTime },
   fr: { plurals: fr, time: frTime },
+=======
+  };
+}
+const locales: ILocales = {
+  en: { plurals: en },
+  fr: { plurals: fr },
+>>>>>>> 80779ba... Translation framework (#43)
 };
 
 for (const key in locales) {
   const locale = locales[key];
   i18n.loadLocaleData(key, { plurals: locale.plurals });
+<<<<<<< HEAD
   timeDelta.addLocale(locale.time);
+=======
+  //i18n.loadLocaleData("fr", { plurals: fr });
+>>>>>>> 80779ba... Translation framework (#43)
 }
 async function load(locale: string) {
   const { messages } = await import(`../locale/${locale}/messages.js`);
@@ -32,22 +47,31 @@ async function load(locale: string) {
 
 /**
  * Load messages for requested locale and activate it.
+<<<<<<< HEAD
  * Stores the choice in localestorage
+=======
+ * This function isn't part of the LinguiJS library because there're
+ * many ways how to load messages  from REST API, from file, from cache, etc.
+>>>>>>> 80779ba... Translation framework (#43)
  */
 async function activate(locale: string) {
   load(locale);
   window.localStorage.setItem("locale", locale);
 }
 
+<<<<<<< HEAD
 /**
  * Initializes locale (retrieve current locale fron localstorage if possible)
  */
+=======
+>>>>>>> 80779ba... Translation framework (#43)
 function init() {
   var locale = window.localStorage.getItem("locale") as string;
   if (!Object.keys(locales).includes(locale)) locale = "en";
   load(locale);
 }
 
+<<<<<<< HEAD
 /**
  * Localizes an amount of seconds
  */
@@ -56,3 +80,6 @@ function prettifySeconds(seconds: number) {
 }
 
 export { locales, activate, init, prettifySeconds };
+=======
+export { locales, activate, init };
+>>>>>>> 80779ba... Translation framework (#43)
