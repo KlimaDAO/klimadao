a256bd1379699b228308c5884be3c41b9bba4573
ladytrekker <ladytrekker@protonmail.com>
2022-01-08 22:52:21 +0100

    feat: add Logo With Claim

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -3,3 +3,4 @@ export { WebFonts } from "./WebFonts";
 export { TextInfoTooltip, useTooltipSingleton } from "./TextInfoTooltip";
 export { GithubIcon } from "./Icons/GithubIcon";
 export { TwitterIcon } from "./Icons/TwitterIcon";
+export { LogoWithClaim } from "./Logos/LogoWithClaim";
