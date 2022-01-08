619fa1527b788f06a21e86d808857263a8a83045
ladytrekker <ladytrekker@protonmail.com>
2022-01-08 22:57:48 +0100

    feat: render start of Redesign on Home

---
+++ site/components/pages/Redesign/index.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from "next";
+
+import {
+  PageWrap,
+  HeaderDesktop,
+  NavItemDesktop,
+  HeaderMobile,
+  NavItemMobile,
+  ButtonPrimary,
+} from "@klimadao/lib/components";
+
+import styles from "./index.module.css";
+import { PageHead } from "components/PageHead";
+import { IS_PRODUCTION } from "lib/constants";
+import { urls } from "@klimadao/lib/constants";
+
+export interface Props {
+  treasuryBalance: number;
+  stakingAPY: number;
+  price: number;
+}
+
+export const Home: NextPage<Props> = (props) => {
+  return (
+    <>
+      <PageHead
+        production={IS_PRODUCTION}
+        title="KlimaDAO"
+        mediaTitle="KlimaDAO"
+        metaDescription="Drive climate action and earn rewards with a carbon-backed digital currency."
+        mediaImageSrc="/og-media.jpg"
+      />
+      <PageWrap>
+        <HeaderDesktop
+          buttons={[
+            <ButtonPrimary
+              key="Connect Wallet"
+              label="Connect Wallet"
+              onClick={() => console.log("Do we want this here?")}
+            />,
+          ]}
+        >
+          <NavItemDesktop url={urls.home} name="Home" active={true} />
+          <NavItemDesktop url={urls.app} name="Buy Klima" />
+          <NavItemDesktop url={urls.app} name="Stake" />
+          <NavItemDesktop url={urls.app} name="Wrap" />
+        </HeaderDesktop>
+        <HeaderMobile>
+          <NavItemMobile url={urls.home} name="Home" />
+          <NavItemMobile url={urls.app} name="Buy Klima" />
+          <NavItemMobile url={urls.app} name="Stake" />
+          <NavItemMobile url={urls.app} name="Wrap" />
+        </HeaderMobile>
+      </PageWrap>
+    </>
+  );
+};
