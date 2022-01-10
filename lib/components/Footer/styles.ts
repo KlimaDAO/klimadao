7a4f99aed90e2502bc985674884b26a1b23735fc
ladytrekker <95881624+ladytrekker@users.noreply.github.com>
2022-01-10 20:19:54 +0100

    Redesign: Add Header, Footer and Navigation for Desktop & Mobile (#69)
    * feat: add Logo With Claim
    
    * feat: add Inter font from Google APIs
    
    - any fallback for that?
    
    * fewat: add basic PageWrap with new theme vars
    
    * feat: add Primary Button
    
    * feat: add navs for Desktop & Mobile
    
    * feat: add hedaer for Desktop & Mobile
    
    * feat: render start of Redesign on Home
    
    * fix: hide mobile background on larger screens
    
    * refactor: clean up some styles
    
    * refactor: link to app hashes and tutorial
    
    * feat: first version of footer
    
    * refactor: open external links in new tab
    
    * refactor: rename Footer => index.tsx
    
    * feat: render links as primary button too
    
    * refactor: remove children from FC props
    
    * refactor: improve styles for headers
    
    * refactor: better styles for primary button
    
    * refactor: better styles for navigations

---
+++ lib/components/Footer/styles.ts
@@ -0,0 +1,72 @@
+import { css } from "@emotion/css";
+import breakpoints from "../../theme/breakpoints";
+
+export const footer = css`
+  background-color: var(--surface-07);
+  padding: 2rem 2.4rem;
+  position: relative;
+
+  ${breakpoints.medium} {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 4.8rem 2.4rem;
+  }
+
+  margin-top: auto;
+`;
+
+export const footer_content = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+
+  ${breakpoints.medium} {
+    text-align: center;
+  }
+`;
+
+export const footer_nav = css`
+  --font-size: var(--font-size);
+  padding: 1rem 0rem;
+  display: flex;
+  flex-direction: column;
+
+  & a {
+    color: var(--surface-04);
+    padding: 0.5rem 0rem;
+  }
+
+  & a:hover {
+    color: var(--surface-02);
+  }
+
+  ${breakpoints.medium} {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    & a {
+      padding: 0rem 1rem;
+    }
+  }
+`;
+
+export const footer_icons = css`
+  display: flex;
+  align-items: center;
+
+  & a {
+    margin: 0 1rem;
+  }
+
+  & svg path {
+    fill: var(--surface-04);
+  }
+
+  & svg path:hover {
+    fill: var(--surface-02);
+  }
+`;
