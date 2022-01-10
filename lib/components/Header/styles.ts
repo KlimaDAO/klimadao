852870966ac7da13dcd6c71581a9c6e1e8a093b7
ladytrekker <95881624+ladytrekker@users.noreply.github.com>
2022-01-10 20:19:54 +0100

    Redesign: Add Header, Footer and Navigation for Desktop & Mobile (#69)
    * feat: add Logo With Claim
    
    * feat: add Inter font from Google APIs
    
    - any fallback for that?
    
    * fewat: add basic PageWrap with new theme vars
    
    * feat: add Primary Button
    
    * feat: add navs for Desktop & Mobile
    
    * feat: add hedaer for Desktop & Mobile
    
    * feat: render start of Redesign on Home
    
    * fix: hide mobile background on larger screens
    
    * refactor: clean up some styles
    
    * refactor: link to app hashes and tutorial
    
    * feat: first version of footer
    
    * refactor: open external links in new tab
    
    * refactor: rename Footer => index.tsx
    
    * feat: render links as primary button too
    
    * refactor: remove children from FC props
    
    * refactor: improve styles for headers
    
    * refactor: better styles for primary button
    
    * refactor: better styles for navigations

---
+++ lib/components/Header/styles.ts
@@ -0,0 +1,93 @@
+import { css } from "@emotion/css";
+import breakpoints from "../../theme/breakpoints";
+
+export const headerDesktop = css`
+  display: none;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 6.4rem;
+  padding: 0rem 3.4rem;
+  background: var(--background);
+  ${breakpoints.medium} {
+    display: flex;
+  }
+`;
+
+export const headerMobileWrap = css`
+  position: fixed;
+  pointer-events: none;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0);
+  transition: background-color 0.5s ease 0s;
+  z-index: 100;
+  ${breakpoints.medium} {
+    display: none;
+  }
+`;
+
+export const headerMobileWrap_toggled = css`
+  ${headerMobileWrap};
+  pointer-events: all;
+`;
+
+export const headerMobile = css`
+  position: relative;
+  background-color: var(--white);
+  pointer-events: all;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0rem 2.4rem;
+  height: 6.4rem;
+  z-index: 1;
+  ${breakpoints.medium} {
+    display: none;
+  }
+`;
+
+export const menuDesktop = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  max-width: var(--site-max-width);
+`;
+
+export const logo_Desktop = css`
+  flex: 1 1 0%;
+  box-sizing: border-box;
+  margin: 0px;
+  min-width: 0px;
+  padding: 2rem;
+  padding-left: 0;
+`;
+
+export const navMain_Desktop = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 0.2rem;
+  font-size: 1.2rem;
+  flex: 0 0 auto;
+`;
+
+export const navMain_DesktopLink = css`
+  text-decoration: none;
+  padding: 0 2rem;
+  font-weight: bold;
+  color: var(--surface);
+  display: flex;
+  align-items: center;
+`;
+
+export const navMain_Buttons = css`
+  box-sizing: border-box;
+  flex: 1 1 0%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
---
+++ lib/components/Layout/styles.ts
@@ -0,0 +1,46 @@
+import { css } from "@emotion/css";
+
+// temporarly put theme values to this class
+
+export const theme = css`
+  --gray: rgb(160, 160, 160);
+  --white: #ffffff;
+
+  /* Material Design naming convention for theme colors */
+  --surface-01: #0e0e0e;
+  --surface-02: #313131;
+  --surface-03: #6e6e6e;
+  --surface-04: #767676;
+  --surface-05: #a1a1a1;
+  --surface-06: #cccccc;
+  --surface-07: #e7e7e7;
+  --surface-08: #f2f2f2;
+  --surface-09: #fafafa;
+  --surface-10: #cccccc;
+
+  --surface: var(--surface-01);
+
+  --primary: #00cc33;
+  --primary-variant: #33732f;
+  --secondary: #0ba1ff;
+  --secondary-variant: #1ea39b;
+  /* Material Design naming convention for theme colors */
+
+  --font-family-decorative: Righteous;
+  --font-family: "Inter", sans-serif;
+  --font-size: 1.4rem;
+
+  --background: var(--surface-08);
+  --site-max-width: 180rem;
+  --border-radius: 0.4rem;
+
+  background-color: var(--background);
+  min-height: 100vh;
+  font-family: var(--font-family);
+  font-size: var(--font-size);
+
+  /* Push footer to bottom -- */
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
