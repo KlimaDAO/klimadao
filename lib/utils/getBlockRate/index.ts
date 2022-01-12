16166385348e094862ddf4ad9110b15df74f40d5
Vienio99 <kyve96@gmail.com>
2022-01-12 22:42:20 +0100

    added try/catch to getBlockRate function

--- lib/utils/getBlockRate/index.ts
+++ lib/utils/getBlockRate/index.ts
@@ -5,15 +5,18 @@ import { API_KEY } from "../../constants";
 
 export async function getBlockRate() {
   let blockRateSeconds = 2.5;
-  const data = await (
-    await fetch(
-      `https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno=999999999&apikey=${API_KEY}`
-    )
-  ).json();
-
-  if (data.status === "1") {
-    blockRateSeconds =
-      data.result.EstimateTimeInSec / data.result.RemainingBlock;
+  try {
+    const data = await (
+      await fetch(
+        `https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno=999999999&apikey=${API_KEY}`
+      )
+    ).json();
+    if (data.status === "1") {
+      blockRateSeconds =
+        data.result.EstimateTimeInSec / data.result.RemainingBlock;
+    }
+  } catch (err) {
+    console.log(err);
   }
   return blockRateSeconds;
 }
