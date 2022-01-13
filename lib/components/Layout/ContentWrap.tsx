5fdeaa688876d78a8d77a95cf2eed770635f5e60
ladytrekker <ladytrekker@protonmail.com>
2022-01-13 22:02:34 +0100

    feat: add white centered section with columns

--- lib/components/Layout/ContentWrap.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from "react";
-import * as styles from "./styles";
-
-export const ContentWrap: FC = (props) => {
-  return <div className={styles.content}>{props.children}</div>;
-};
