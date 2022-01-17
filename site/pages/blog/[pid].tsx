fa5cac0e610285c50e7958eed8d0181813123529
Atmosfearful <atmosfearful@protonmail.com>
2022-01-16 20:36:51 -0800

    add more debugs

--- site/pages/blog/[pid].tsx
+++ site/pages/blog/[pid].tsx
@@ -6,13 +6,23 @@ import { loadTranslation } from "lib/i18n";
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   try {
+    console.log("getting static props for", ctx.params?.pid, ctx.locale);
     const post = await fetchCMSContent("post", {
       slug: ctx.params?.pid,
     });
     const translation = await loadTranslation(ctx.locale);
+
     if (!post) {
       throw new Error("No content found");
     }
+    if (!translation) {
+      throw new Error("No translation found");
+    }
+    console.log(
+      `static props for ${ctx.params?.pid} got:`,
+      JSON.stringify(post, undefined, " "),
+      translation
+    );
     return {
       props: {
         post,
@@ -21,6 +31,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       revalidate: 120,
     };
   } catch (e) {
+    console.error("Failed to generate", e);
     return {
       notFound: true,
       revalidate: 120,
