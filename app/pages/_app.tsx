cd998186ce2d31b5c0f1d39bf4a8686a8f492f7b
0xAeterno <aeterno.klima@gmail.com>
2022-01-19 19:26:38 +1100

    linting: remove unused deps

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,10 +2,6 @@ import "@klimadao/lib/theme/colors.css";
 import "@klimadao/lib/theme/normalize.css";
 import "@klimadao/lib/theme/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { selectAppState } from "state/selectors";
-import { useAppDispatch } from "state";
-import { setLocale } from "actions/app";
 
 import { I18nProvider } from "@lingui/react";
 import { i18n } from "@lingui/core";
