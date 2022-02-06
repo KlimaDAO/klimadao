7185a95bc41fa2c3743d1338bd4e7c42896b3361
Keith Axline <keith@axline.io>
2022-02-06 16:33:01 +0000

    Fix for rendering issue in Resources nav on mobile (#129)
    * First draft
    
    * Tried to fix more than I should.

--- site/components/Navigation/index.tsx
+++ site/components/Navigation/index.tsx
@@ -51,7 +51,7 @@ export const Navigation: FC<Props> = (props) => {
           name={t({ message: "Bond", id: "mainNav.bond" })}
         />
         <NavItemDesktop
-          url="/resources"
+          url="/blog"
           name={t`Resources`}
           link={Link}
           active={props.activePage === "Resources"}
@@ -59,7 +59,7 @@ export const Navigation: FC<Props> = (props) => {
       </HeaderDesktop>
       <HeaderMobile>
         <NavItemMobile
-          url={urls.home}
+          url={"/"}
           name={t({ message: "Home", id: "mainNav.home" })}
         />
         <NavItemMobile
@@ -76,7 +76,7 @@ export const Navigation: FC<Props> = (props) => {
           url={urls.bond}
           name={t({ message: "Bond", id: "mainNav.bond" })}
         />
-        <NavItemMobile url="/resources" name={t`Resources`} />
+        <NavItemMobile url="/blog" name={t`Resources`} />
       </HeaderMobile>
     </>
   );
