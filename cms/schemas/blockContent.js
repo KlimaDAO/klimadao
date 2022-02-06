b91a1079fef025fbb5f3b048e5db2e5096d81c19
rstycedar <94987759+rstycedar@users.noreply.github.com>
2022-02-06 08:34:57 -0800

    Cleanup /blog and /[post] styles (#126)
    * add --shadow-07 var
    
    * update join discord styles
    
    * change bgColor of post page
    
    * remove h3 and h4 from schema
    
    * update blockContentRenderer
    
    * update post styles
    
    * cleanup pages/blog styles
    
    * add body3 typography
    
    * add body3 typography
    
    * add surface-04 and surface-05 theme variables
    
    * adjust blog card styles to match design
    
    * update blockContentRenderer
    
    * change body2s to body3s
    
    * update font color of card content

--- cms/schemas/blockContent.js
+++ cms/schemas/blockContent.js
@@ -22,6 +22,7 @@ export default {
       // use your content.
       styles: [
         { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
         { title: "H2", value: "h2" },
         { title: "Quote", value: "blockquote" },
       ],
