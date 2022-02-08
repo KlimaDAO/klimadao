650e9581dedabb7dfb386a3af25cfd3b417c40ef
ladytrekker <ladytrekker@protonmail.com>
2022-02-08 15:56:34 +0100

    refactor: better height for Logo

---
+++ lib/components/Logos/styles.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/css";
+import breakpoints from "../../theme/breakpoints";
+
+export const logoWithClaim = css`
+  max-width: 15rem;
+
+  ${breakpoints.medium} {
+    max-width: 20rem;
+  }
+`;
