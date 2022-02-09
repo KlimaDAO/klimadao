791b753ac393b265d87cddbcadb487a5e4b39c3d
Atmosfearful <atmosfearful@protonmail.com>
2022-02-08 17:33:16 -0800

    fix merge conflict mistakes

--- lib/components/Buttons/styles.ts
+++ lib/components/Buttons/styles.ts
@@ -11,19 +11,22 @@ export const button_primary = css`
   text-decoration: none;
   /* min-height to conform with Lighthouse min tap-target */
   min-height: 4.8rem;
-  padding: 0rem
+  padding: 0rem;
   cursor: pointer;
   border-radius: 0.4rem;
   transition: opacity 0.3s ease 0s;
   background-color: var(--klima-green);
 
-  &:hover, &:focus {
+  &:hover,
+  &:focus {
     opacity: 0.7;
   }
   &:focus {
     transform: scale(0.9);
   }
-  &, &:hover, &:visited {
+  &,
+  &:hover,
+  &:visited {
     color: white; /* same in darkmode */
   }
 `;
@@ -37,9 +40,4 @@ export const button_gray = css`
   &:visited {
     color: var(--font-02); /* same in darkmode */
   }
-`;
-
-export const button_gray = css`
-  background-color: var(--surface-01);
-  color: var(--surface-02);
 `;
