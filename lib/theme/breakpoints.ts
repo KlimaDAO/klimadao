5746ee05b7a4eba7444e3bfaa0886727f1880698
Atmosfearful <atmosfearful@protonmail.com>
2022-02-09 11:04:33 -0800

    switch breakpoints to em

--- lib/theme/breakpoints.ts
+++ lib/theme/breakpoints.ts
@@ -1,7 +1,13 @@
+/* em units inside media queries take browser default (16px) */
 export default {
-  small: "@media (min-width: 376px)",
-  medium: "@media (min-width: 576px)",
-  large: "@media (min-width: 768px)",
-  desktop: "@media (min-width: 992px)",
-  desktopLarge: "@media (min-width: 1200px)",
+  /** 23.5em, 376px */
+  small: "@media (min-width: 23.5em)",
+  /** 36em, 576px */
+  medium: "@media (min-width: 36em)",
+  /**  48em, 768px */
+  large: "@media (min-width: 48em)",
+  /** 62em, 992px */
+  desktop: "@media (min-width: 62em)",
+  /** 75em, 1200px */
+  desktopLarge: "@media (min-width: 75em)",
 };
