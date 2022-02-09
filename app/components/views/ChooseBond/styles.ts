a55b084762579edcc05ad3b374897b1c57deabaa
Atmosfearful <atmosfearful@protonmail.com>
2022-02-08 17:47:17 -0800

    ChooseBond View complete

--- app/components/views/ChooseBond/styles.ts
+++ app/components/views/ChooseBond/styles.ts
@@ -17,18 +17,13 @@ export const chooseBondCard = css`
     background-color: var(--surface-01);
   }
 
-  ${breakpoints.medium} {
-    gap: 3.2rem;
-  }
-
   ${breakpoints.desktop} {
     grid-column: cardsleft;
     grid-row: 2 / span 3;
-    gap: 4.8rem;
     grid-template-rows: 1fr 1fr 1fr;
     align-items: start;
     /* hacky fix to keep grid-rows from collapsing on short screens */
-    min-height: 88rem;
+    min-height: 94rem;
   }
 
   ${breakpoints.desktopLarge} {
