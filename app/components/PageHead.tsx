41ab0502e29a405be1ffdb521889d1892e029a09
Atmosfearful <atmosfearful@protonmail.com>
2022-02-10 11:21:55 -0800

    update dapp meta tags and favicons, remove unused images

--- app/components/PageHead.tsx
+++ app/components/PageHead.tsx
@@ -23,7 +23,7 @@ export const PageHead = (props: PageHeadProps) => {
           defer
           data-domain="dapp.klimadao.finance"
           src="https://plausible.io/js/plausible.js"
-        ></script>
+        />
       )}
       <title>{props.title}</title>
       <meta name="description" content={props.metaDescription} />
@@ -33,7 +33,26 @@ export const PageHead = (props: PageHeadProps) => {
         <meta property="og:image" content={props.mediaImageSrc} />
       )}
       <meta property="og:type" content="website" />
-      <link rel="icon" href="/favicon.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="theme-color" content="#ffffff" />
       <meta property="og:locale" content="en" />
     </Head>
   );
