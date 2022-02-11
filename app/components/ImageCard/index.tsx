d498f1ceda82dd32621a1097e30ab01d1883f64d
Atmosfearful <atmosfearful@protonmail.com>
2022-02-10 17:04:34 -0800

    fix image card

--- app/components/ImageCard/index.tsx
+++ app/components/ImageCard/index.tsx
@@ -6,17 +6,15 @@ import { urls } from "@klimadao/lib/constants";
 
 export const ImageCard: FC = () => {
   return (
-    <a href={urls.buy}>
-      <div className={styles.card}>
-        <div className="header">
-          <Text t="caption" className="title">
-            New to KLIMA?
-          </Text>
-          <Text t="h4">How to get started</Text>
-        </div>
-        <div className="footer">
-          <KeyboardReturnOutlined />
-        </div>
+    <a href={urls.buy} className={styles.card}>
+      <div className="header">
+        <Text t="caption" className="title">
+          New to KLIMA?
+        </Text>
+        <Text t="h4">How to get started</Text>
+      </div>
+      <div className="footer">
+        <KeyboardReturnOutlined />
       </div>
     </a>
   );
