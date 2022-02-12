ce6e9f5dd180caee54fd210527d320ad8c54becd
Atmosfearful <atmosfearful@protonmail.com>
2022-02-12 12:07:57 -0800

    experiment: dont do materialui stylesheets

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheets } from "@material-ui/core/styles";
+// import { ServerStyleSheets } from "@material-ui/core/styles";
 import React from "react";
 import { WebFonts, InitializeTheme } from "@klimadao/lib/components";
 
@@ -22,26 +22,26 @@ class MyDocument extends Document {
 
 // pasted from material-ui example
 // it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+// MyDocument.getInitialProps = async (ctx) => {
+//   // Render app and page and get the context of the page with collected side effects.
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+//     });
 
-  const initialProps = await Document.getInitialProps(ctx);
+//   const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
+//   return {
+//     ...initialProps,
+//     // Styles fragment is rendered after the app and page rendering finish.
+//     styles: [
+//       ...React.Children.toArray(initialProps.styles),
+//       sheets.getStyleElement(),
+//     ],
+//   };
+// };
 
 export default MyDocument;
