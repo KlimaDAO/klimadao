8ff74e6a9148c56dbf4774052e147dbe24185f4d
Atmosfearful <atmosfearful@protonmail.com>
2022-02-12 13:43:03 -0800

    remove @mui/core from site

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import { cache } from "@emotion/css";
-// import { ServerStyleSheets } from "@material-ui/core/styles";
 import React from "react";
 import { WebFonts, InitializeTheme } from "@klimadao/lib/components";
 
@@ -49,29 +48,5 @@ MyDocument.getInitialProps = async (ctx) => {
     ),
   };
 };
-
-// pasted from material-ui example
-// it's compatible with server-side generation (SSG).
-// MyDocument.getInitialProps = async (ctx) => {
-//   // Render app and page and get the context of the page with collected side effects.
-//   const sheets = new ServerStyleSheets();
-//   const originalRenderPage = ctx.renderPage;
-
-//   ctx.renderPage = () =>
-//     originalRenderPage({
-//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-//     });
-
-//   const initialProps = await Document.getInitialProps(ctx);
-
-//   return {
-//     ...initialProps,
-//     // Styles fragment is rendered after the app and page rendering finish.
-//     styles: [
-//       ...React.Children.toArray(initialProps.styles),
-//       sheets.getStyleElement(),
-//     ],
-//   };
-// };
 
 export default MyDocument;
