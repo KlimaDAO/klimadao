83bdf204586a32979e1e009cb85b7900721121b8
0xAeterno <aeterno.klima@gmail.com>
2022-02-12 16:32:11 +1100

    refactor: move mobile navigation above text group

--- site/components/pages/Resources/ResourcesHeader/index.tsx
+++ site/components/pages/Resources/ResourcesHeader/index.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import Link from "next/link";
 import * as styles from "./styles";
 
@@ -5,97 +6,93 @@ import { ButtonPrimary, Section, Text } from "@klimadao/lib/components";
 import { t, Trans } from "@lingui/macro";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 
-import { FC, PropsWithChildren, ReactNode } from "react";
-
 type PageName = "blog" | "community" | "contact";
 
 export type Props = {
   activePage: PageName;
   title: string;
   subline: string;
-  headerElements?: FC<PropsWithChildren<ReactNode>>;
+  headerElements?: FC;
 };
 
-export const ResourcesHeader: FC<Props> = (props) => {
-  return (
-    <>
-      <div className={styles.navigationDesktopWrapper}>
-        <div className={styles.navigationDesktop}>
-          <ul className={styles.list}>
-            <li
-              className={styles.listItem}
-              data-active={props.activePage === "blog"}
-            >
-              <Link href="/blog">
-                <a>
-                  <Trans>Blog</Trans>
-                  <ArrowBack className="arrow" />
-                </a>
-              </Link>
-            </li>
-            <li
-              className={styles.listItem}
-              data-active={props.activePage === "community"}
-            >
-              <Link href="/community">
-                <a>
-                  <Trans>Community</Trans>
-                  <ArrowBack className="arrow" />
-                </a>
-              </Link>
-            </li>
-            <li
-              className={styles.listItem}
-              data-active={props.activePage === "contact"}
-            >
-              <Link href="/contact">
-                <a>
-                  <Trans>Contact Us</Trans>
-                  <ArrowBack className="arrow" />
-                </a>
-              </Link>
-            </li>
-          </ul>
+export const ResourcesHeader: FC<Props> = (props) => (
+  <>
+    <div className={styles.navigationDesktopWrapper}>
+      <div className={styles.navigationDesktop}>
+        <ul className={styles.list}>
+          <li
+            className={styles.listItem}
+            data-active={props.activePage === "blog"}
+          >
+            <Link href="/blog">
+              <a>
+                <Trans>Blog</Trans>
+                <ArrowBack className="arrow" />
+              </a>
+            </Link>
+          </li>
+          <li
+            className={styles.listItem}
+            data-active={props.activePage === "community"}
+          >
+            <Link href="/community">
+              <a>
+                <Trans>Community</Trans>
+                <ArrowBack className="arrow" />
+              </a>
+            </Link>
+          </li>
+          <li
+            className={styles.listItem}
+            data-active={props.activePage === "contact"}
+          >
+            <Link href="/contact">
+              <a>
+                <Trans>Contact Us</Trans>
+                <ArrowBack className="arrow" />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <Section variant="gray" style={{ padding: "unset" }}>
+      <div className={styles.resourcesHeader}>
+        <div className={styles.navigationMobile}>
+          <ButtonPrimary
+            className="navigationMobile_navItem"
+            label={t`Blog`}
+            href={"/blog"}
+            variant={props.activePage !== "blog" ? "gray" : null}
+            link={Link}
+          />
+          <ButtonPrimary
+            className="navigationMobile_navItem"
+            label={t`Community`}
+            href={"/community"}
+            variant={props.activePage !== "community" ? "gray" : null}
+            link={Link}
+          />
+          <ButtonPrimary
+            className="navigationMobile_navItem"
+            label={t`Contact`}
+            href={"/contact"}
+            variant={props.activePage !== "contact" ? "gray" : null}
+            link={Link}
+          />
+        </div>
+
+        <div className="resourcesHeader_textGroup">
+          <Text t="h2" as="h2">
+            <Trans>{props.title}</Trans>
+          </Text>
+          <Text align="center" t="body3" color="lighter">
+            <Trans>{props.subline}</Trans>
+          </Text>
+          {props.headerElements && <props.headerElements />}
         </div>
       </div>
-
-      <Section variant="gray" style={{ padding: "unset" }}>
-        <div className={styles.resourcesHeader}>
-          <div className="resourcesHeader_textGroup">
-            <Text t="h2" as="h2">
-              <Trans>{props.title}</Trans>
-            </Text>
-            <Text align="center" t="body3" color="lighter">
-              <Trans>{props.subline}</Trans>
-            </Text>
-            {props.headerElements && <props.headerElements />}
-          </div>
-
-          <div className={styles.navigationMobile}>
-            <ButtonPrimary
-              className="navigationMobile_navItem"
-              label={t`Blog`}
-              href={"/blog"}
-              variant={props.activePage !== "blog" ? "gray" : null}
-              link={Link}
-            />
-            <ButtonPrimary
-              className="navigationMobile_navItem"
-              label={t`Community`}
-              href={"/community"}
-              variant={props.activePage !== "community" ? "gray" : null}
-              link={Link}
-            />
-            <ButtonPrimary
-              className="navigationMobile_navItem"
-              label={t`Contact`}
-              href={"/contact"}
-              variant={props.activePage !== "contact" ? "gray" : null}
-              link={Link}
-            />
-          </div>
-        </div>
-      </Section>
-    </>
-  );
-};
+    </Section>
+  </>
+);
