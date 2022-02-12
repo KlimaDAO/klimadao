7897c38e4cffb90435cd9bcd2eeb48ab0cee4547
Atmosfearful <atmosfearful@protonmail.com>
2022-02-12 12:45:27 -0800

    try emotion nextjs example

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import { cache } from "@emotion/css";
 // import { ServerStyleSheets } from "@material-ui/core/styles";
 import React from "react";
 import { WebFonts, InitializeTheme } from "@klimadao/lib/components";
@@ -19,6 +21,34 @@ class MyDocument extends Document {
     );
   }
 }
+
+const renderStatic = async (html?: string) => {
+  if (html === undefined) {
+    throw new Error("did you forget to return html from renderToString?");
+  }
+  const { extractCritical } = createEmotionServer(cache);
+  const { ids, css } = extractCritical(html);
+
+  return { html, ids, css };
+};
+
+MyDocument.getInitialProps = async (ctx) => {
+  const page = await ctx.renderPage();
+  const { css, ids } = await renderStatic(page.html);
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: (
+      <React.Fragment>
+        {initialProps.styles}
+        <style
+          data-emotion={`css ${ids.join(" ")}`}
+          dangerouslySetInnerHTML={{ __html: css }}
+        />
+      </React.Fragment>
+    ),
+  };
+};
 
 // pasted from material-ui example
 // it's compatible with server-side generation (SSG).
