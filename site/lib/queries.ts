3d2e86616713c7b1f26f0e88af322b771b16a101
rstycedar <rstycedar@protonmail.com>
2022-02-24 18:08:46 -0800

    add hideFromProduction filter to blog query and schema

--- site/lib/queries.ts
+++ site/lib/queries.ts
@@ -1,3 +1,5 @@
+import { IS_PRODUCTION } from "./constants";
+
 const post = /* groq */ `
   *[slug.current == $slug][0] {
     body[] {
@@ -18,12 +20,26 @@ const post = /* groq */ `
   }
 `;
 
+/* Posts can have a hideFromProduction field that may be set to true if the author
+wants to publish their posts but dont want it to appear in the production environment. 
+
+NOTE: When constructing a new query for posts, please add the hideFromProduction filter to your query
+so the query wont reveal any hidden posts in the production environment */
+
+const hideFromProduction = IS_PRODUCTION
+  ? " && hideFromProduction != true"
+  : "";
+
 export const queries = {
   /** fetch all blog posts, sorted by publishedAt */
   allPosts:
-    '*[_type == "post"] | order(publishedAt desc) {summary, "slug": slug.current, title, publishedAt, author->, "imageUrl": mainImage.asset->url}',
+    '*[_type == "post"' +
+    hideFromProduction +
+    '] | order(publishedAt desc) {summary, "slug": slug.current, title, publishedAt, author->, "imageUrl": mainImage.asset->url}',
   latestPost:
-    '*[_type == "post"] | order(publishedAt desc){"slug": slug.current, title}[0]',
+    '*[_type == "post"' +
+    hideFromProduction +
+    '] | order(publishedAt desc) {"slug": slug.current, title}[0]',
   /** fetch a blog post based on slug */
   post,
 };
