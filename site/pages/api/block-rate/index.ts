130b284bedb7f8015f783ee9018d2bcce1eed72a
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2022-03-01 17:39:20 -0500

    Add vienio's dynamic block rate API

---
+++ site/pages/api/block-rate/index.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next/types";
+import { getBlockRate } from "lib/getBlockRate";
+
+type ResponseData = {
+  blockRate30Day?: string;
+};
+interface ErrorObj {
+  message: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData | ErrorObj>
+) {
+  try {
+    // share-cache after last cached request:
+    // 0-12 hours -> fresh, return from cache
+    // 12-24 hours -> stale, return from cache but revalidate in background
+    // >24 hours -> stale, recalculate & return fresh value
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=43200, stale-while-revalidate=43200"
+    );
+
+    const blockRate = await getBlockRate();
+    res.status(200).json({ blockRate30Day: blockRate.toFixed(4) });
+  } catch (err) {
+    res.status(500).json({ message: "failed to fetch data" });
+  }
+}
