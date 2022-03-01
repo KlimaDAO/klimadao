45aa29c00d2d43e35fe512a9f91cdba690b8a694
Vienio99 <73585288+Vienio99@users.noreply.github.com>
2022-03-01 23:32:59 +0100

    minor changes and variables rename in getBlockRate (#219)

--- lib/utils/getBlockRate/index.ts
+++ lib/utils/getBlockRate/index.ts
@@ -3,30 +3,30 @@ import { API_KEY, MONTH_IN_SECONDS } from "../../constants";
 // Calculates block rate by dividing one month in seconds by number of blocks that got created during that month
 
 export async function getBlockRate() {
-  let blockRateSeconds = 2.5;
+  const fallbackBlockRate = 2.5;
 
   // Unix timestamp in seconds
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const timestamp30DaysAgo = currentTimestamp - MONTH_IN_SECONDS;
   try {
-    const blockRateFuture = await (
-      await fetch(
-        `https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno=999999999&apikey=${API_KEY}`
-      )
-    ).json();
+    const url = `https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno=999999999&apikey=${API_KEY}`;
+    const res = await fetch(url);
+    const blocksData = await res.json();
 
-    const blockRate30DaysAgo = await (
-      await fetch(
-        `https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp30DaysAgo}&closest=before&apikey=${API_KEY}`
-      )
-    ).json();
+    const url2 = `https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp30DaysAgo}&closest=before&apikey=${API_KEY}`;
+    const res2 = await fetch(url2);
+    const block30DaysAgo = await res2.json();
 
-    blockRateSeconds =
+    const blockRateSeconds =
       MONTH_IN_SECONDS /
-      (blockRateFuture.result.CurrentBlock - blockRate30DaysAgo.result);
+      (blocksData.result.CurrentBlock - block30DaysAgo.result);
+
+    if (!Number.isNaN(blockRateSeconds)) {
+      return blockRateSeconds;
+    }
   } catch (err) {
     console.log(err);
   }
 
-  return blockRateSeconds;
+  return fallbackBlockRate;
 }
