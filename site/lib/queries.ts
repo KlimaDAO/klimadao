5b6590e0a8855ad7c27d107557847c704f90590b
rstycedar <94987759+rstycedar@users.noreply.github.com>
2022-03-01 17:09:40 -0800

    update queries (#226)

--- site/lib/queries.ts
+++ site/lib/queries.ts
@@ -1,25 +1,5 @@
 import { IS_PRODUCTION } from "./constants";
 
-const post = /* groq */ `
-  *[slug.current == $slug][0] {
-    body[] {
-      ...,
-      _type == "image" => {
-        ...,
-        asset -> {
-          url,
-          "width": metadata.dimensions.width,
-          "height": metadata.dimensions.height,
-        }
-      }
-    },
-    title,
-    author->,
-    "imageUrl": mainImage.asset->url,
-    publishedAt
-  }
-`;
-
 /* Posts can have a hideFromProduction field that may be set to true if the author
 wants to publish their posts but dont want it to appear in the production environment. 
 
@@ -28,21 +8,84 @@ so the query wont reveal any hidden posts in the production environment */
 
 const prodQueries = {
   /** fetch all blog posts, sorted by publishedAt */
-  allPosts:
-    '*[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {summary, "slug": slug.current, title, publishedAt, author->, "imageUrl": mainImage.asset->url}',
-  latestPost:
-    '*[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {"slug": slug.current, title}[0]',
+  allPosts: /* groq */ `
+    *[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {
+      summary, 
+      "slug": slug.current, 
+      title, 
+      publishedAt, 
+      author->, 
+      "imageUrl": mainImage.asset->url
+    }
+  `,
+  /** fetch the last published post slug and title */
+  latestPost: /* groq */ `
+    *[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {
+      "slug": slug.current, 
+      title
+    }[0]
+  `,
   /** fetch a blog post based on slug */
-  post,
+  post: /* groq */ `
+    *[_type == "post" && slug.current == $slug && hideFromProduction != true][0] {
+      body[] {
+        ...,
+        _type == "image" => {
+          ...,
+          asset -> {
+            url,
+            "width": metadata.dimensions.width,
+            "height": metadata.dimensions.height,
+          }
+        }
+      },
+      title,
+      author->,
+      "imageUrl": mainImage.asset->url,
+      publishedAt
+    }
+  `,
 };
+
 const stagingQueries: typeof prodQueries = {
   /** fetch all blog posts, sorted by publishedAt */
-  allPosts:
-    '*[_type == "post"] | order(publishedAt desc) {summary, "slug": slug.current, title, publishedAt, author->, "imageUrl": mainImage.asset->url}',
-  latestPost:
-    '*[_type == "post"] | order(publishedAt desc) {"slug": slug.current, title}[0]',
+  allPosts: /* groq */ `
+    *[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {
+      summary, 
+      "slug": slug.current, 
+      title, 
+      publishedAt, 
+      author->, 
+      "imageUrl": mainImage.asset->url
+    }
+  `,
+  /** fetch the last published post slug and title */
+  latestPost: /* groq */ `
+    *[_type == "post" && hideFromProduction != true] | order(publishedAt desc) {
+      "slug": slug.current, 
+      title
+    }[0]
+  `,
   /** fetch a blog post based on slug */
-  post,
+  post: /* groq */ `
+    *[_type == "post" && slug.current == $slug][0] {
+      body[] {
+        ...,
+        _type == "image" => {
+          ...,
+          asset -> {
+            url,
+            "width": metadata.dimensions.width,
+            "height": metadata.dimensions.height,
+          }
+        }
+      },
+      title,
+      author->,
+      "imageUrl": mainImage.asset->url,
+      publishedAt
+    }
+  `,
 };
 
 export const queries = IS_PRODUCTION ? prodQueries : stagingQueries;
