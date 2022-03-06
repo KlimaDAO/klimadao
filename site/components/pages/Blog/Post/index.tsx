b6246053be8ed560f3bcb323d8b4e1e6ffbc4621
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-03-06 15:39:22 +1100

    refactor: simplify Post component

--- site/components/pages/Blog/Post/index.tsx
+++ site/components/pages/Blog/Post/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-
 import { Text } from "@klimadao/lib/components";
 
 import { Post } from "lib/queries";
@@ -17,52 +16,62 @@ interface PostProps {
   post?: Post;
 }
 
-export function PostPage(props: PostProps) {
-  const body = props.post ? (
-    <div className={styles.container}>
-      <div className={styles.banner}>
-        <div className={styles.bannerImage}>
-          <Image
-            priority={true}
-            src={props.post.imageUrl || defaultImage}
-            alt={props.post.title}
-            objectFit="cover"
-            layout="fill"
-          />
+export const PostPage = (props: PostProps) => {
+  if (!props.post) {
+    return (
+      <>
+        <Navigation activePage="Resources" />
+        <div className={styles.fallbackContainer}>
+          <Text className={styles.loadingArticle}>Loading article...</Text>
         </div>
-      </div>
-      <section className={styles.blogContainer}>
-        <div className={styles.content}>
-          <Text t="h2" as="h1" className={styles.title}>
-            {props.post.title}
-          </Text>
-          <Text t="h5" as="p" className={styles.date}>
-            Published {new Date(props.post.publishedAt).toDateString()}
-          </Text>
-          <BlockContentRenderer blocks={props.post.body} />
-        </div>
-      </section>
-    </div>
-  ) : (
-    <div className={styles.fallbackContainer}>
-      <Text className={styles.loadingArticle}>Loading article...</Text>
-    </div>
-  );
+        <Footer />
+      </>
+    );
+  }
+
+  const publishedDate = `Published ${new Date(
+    props.post.publishedAt
+  ).toDateString()}`;
 
   return (
     <>
-      {props.post && (
-        <PageHead
-          production={IS_PRODUCTION}
-          title={props.post.title}
-          mediaTitle={props.post.title}
-          metaDescription={props.post.summary}
-          mediaImageSrc={props.post.imageUrl || "/og-media.png"}
-        />
-      )}
+      <PageHead
+        production={IS_PRODUCTION}
+        title={props.post.title}
+        mediaTitle={props.post.title}
+        metaDescription={props.post.summary}
+        mediaImageSrc={props.post.imageUrl || "/og-media.png"}
+      />
+
       <Navigation activePage="Resources" />
-      {body}
+
+      <div className={styles.container}>
+        <div className={styles.banner}>
+          <div className={styles.bannerImage}>
+            <Image
+              priority={true}
+              src={props.post.imageUrl || defaultImage}
+              alt={props.post.title}
+              objectFit="cover"
+              layout="fill"
+            />
+          </div>
+        </div>
+
+        <section className={styles.blogContainer}>
+          <div className={styles.content}>
+            <Text t="h2" as="h1" className={styles.title}>
+              {props.post.title}
+            </Text>
+            <Text t="h5" as="p" className={styles.date}>
+              {publishedDate}
+            </Text>
+            <BlockContentRenderer blocks={props.post.body} />
+          </div>
+        </section>
+      </div>
+
       <Footer />
     </>
   );
-}
+};
