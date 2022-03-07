32b53ad21424ce9b7b1ea6999713f71c42286fdb
Atmosfearful <atmosfearful@protonmail.com>
2022-03-07 13:09:54 -0800

    update headers for block-rate api

--- site/next.config.js
+++ site/next.config.js
@@ -19,7 +19,20 @@ module.exports = {
     return [
       {
         source: "/api/block-rate",
-        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+        // from vercel docs example
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
