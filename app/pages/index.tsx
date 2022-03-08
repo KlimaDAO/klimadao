b6db3d43571f00b6fe1d6f44e8e11730a1349b15
biwano <biwano@gmail.com>
2022-03-08 08:27:28 +0100

    Preload english translations in app

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,7 +4,16 @@ import { WithRedux } from "components/WithRedux";
 import { NextPage } from "next";
 import { PageHead } from "components/PageHead";
 import { IS_PRODUCTION } from "lib/constants";
+import { messages as default_messages } from "../locale/en/messages";
+import { i18n } from "@lingui/core";
 
+export async function getStaticProps() {
+  i18n.load("en", default_messages);
+  i18n.activate("en");
+  return {
+    props: {},
+  };
+}
 const HomePage: NextPage = () => {
   return (
     <>
