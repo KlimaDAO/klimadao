c5e6f7bce662ec9c0f0d33ec392a9679d7d11e90
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-03-20 19:44:43 +1100

    feat: set up basic pledge dashboard structure

--- site/components/pages/Pledge/index.tsx
+++ site/components/pages/Pledge/index.tsx
@@ -1,13 +1,93 @@
+import React from "react";
 import { NextPage } from "next";
-import { Navigation } from "components/Navigation";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { Text } from "@klimadao/lib/components";
 
-import { Auth } from "./Auth";
+import klimaInfinityBlackLogo from "public/logo-klima-infinity-black.png";
+import klimaInfinityWhiteLogo from "public/logo-klima-infinity-white.png";
+
+import CloudQueueIcon from "@mui/icons-material/CloudQueue";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import LocalGasStationOutlinedIcon from "@mui/icons-material/LocalGasStationOutlined";
+import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
+
+import * as styles from "./styles";
+
+const ThemeToggle = dynamic(() => import("components/Navigation/ThemeToggle"), {
+  ssr: false,
+});
+
+type CardProps = {
+  title: string;
+  icon: JSX.Element;
+};
+
+const Card: React.FC<CardProps> = (props) => (
+  <div className={styles.card}>
+    <div className={styles.cardHeader}>
+      {props.icon}
+      <Text t="h4">{props.title}</Text>
+    </div>
+  </div>
+);
 
 export const Pledge: NextPage = () => {
   return (
-    <>
-      <Navigation activePage="Home" />
-      <Auth />
-    </>
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.header}>
+          <div className={styles.logo}>
+            <Image alt="Klima Infinity logo" src={klimaInfinityWhiteLogo} />
+          </div>
+          <ThemeToggle />
+        </div>
+
+        <div className={styles.profile}>
+          <Card title="Profile" icon={<EmailOutlinedIcon fontSize="large" />} />
+        </div>
+
+        <div className={styles.pledgeChart}>
+          <Card
+            title="Pledge vs Assets"
+            icon={<EmailOutlinedIcon fontSize="large" />}
+          />
+        </div>
+
+        <div className={styles.column}>
+          <Card title="Pledge" icon={<MailOutlineIcon fontSize="large" />} />
+          <Card
+            title="Footprint"
+            icon={<LocalGasStationOutlinedIcon fontSize="large" />}
+          />
+          <Card
+            title="Methodology"
+            icon={<HowToRegOutlinedIcon fontSize="large" />}
+          />
+          <Card title="Pledge" icon={<MailOutlineIcon fontSize="large" />} />
+          <Card
+            title="Footprint"
+            icon={<LocalGasStationOutlinedIcon fontSize="large" />}
+          />
+          <Card
+            title="Methodology"
+            icon={<HowToRegOutlinedIcon fontSize="large" />}
+          />
+        </div>
+
+        <div className={styles.column}>
+          <Card
+            title="Carbon Assets"
+            icon={<CloudQueueIcon fontSize="large" />}
+          />
+          <Card
+            title="Retired Assets"
+            icon={<LocalFireDepartmentIcon fontSize="large" />}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
---
+++ site/components/pages/Pledge/styles.ts
@@ -0,0 +1,76 @@
+import { css } from "@emotion/css";
+import breakpoints from "@klimadao/lib/theme/breakpoints";
+import * as common from "@klimadao/lib/theme/common";
+
+export const container = css`
+  grid-column: full;
+  display: grid;
+  grid-template-columns: inherit;
+  padding: 4rem 0;
+  background-color: var(--surface-01);
+`;
+
+export const contentContainer = css`
+  grid-column: main;
+  gap: 2rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+export const header = css`
+  grid-column: span 2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const logo = css`
+  max-width: 28rem;
+`
+
+export const cardHeader = css`
+  display: flex;
+  flex-direction: row;
+  gap: 0.8rem;
+  align-items: center;
+
+  p {
+    text-transform: uppercase;
+  }
+
+  svg {
+    fill: var(--font-01);
+  }
+`
+
+export const card = css`
+  ${common.cardSurface}
+  display: flex;
+  flex-direction: column;
+  align-content: start;
+
+  height: 200px;
+`;
+
+export const icon = css`
+  background-color: var(--font-02);
+`;
+
+export const profile = css`
+  grid-column: span 2;
+`;
+
+export const pledgeChart = css`
+  grid-column: span 2;
+`;
+
+export const column = css`
+  display: grid;
+  gap: 2rem;
+  grid-column: span 2;
+  align-content: start;
+
+  ${breakpoints.desktop} {
+    grid-column: span 1;  
+  }
+`;
