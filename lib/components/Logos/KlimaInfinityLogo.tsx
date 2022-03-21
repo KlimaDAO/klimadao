65c2155161d26a113b4798329faae139031b4381
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-03-21 14:19:36 +1100

    fix: explicit svg height

--- lib/components/Logos/KlimaInfinityLogo.tsx
+++ lib/components/Logos/KlimaInfinityLogo.tsx
@@ -8,7 +8,7 @@ export const KlimaInfinityLogo = (props: Props) => (
   <svg
     className={props.className}
     width="100%"
-    height="auto"
+    height="100%"
     viewBox="0 0 881 126"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
