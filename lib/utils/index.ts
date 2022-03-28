6ddde79339f937fa6c98c9e3354de283b898feb3
klimaheroine <102233694+klimaheroine@users.noreply.github.com>
2022-03-29 00:50:22 +0200

    Fix MCO2 bonds using deprecated market price (#265)

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,6 +1,5 @@
 export { getInteger } from "./getInteger";
 export { getKlimaSupply } from "./getKlimaSupply";
-export { getKlimaUsdcPrice } from "./getKlimaUsdcPrice";
 export { getJsonRpcProvider } from "./getJsonRpcProvider";
 export { getTreasuryBalance } from "./getTreasuryBalance";
 export { getStakingRewards } from "./getStakingRewards";
