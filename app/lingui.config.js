172310cf4f967a08aebb69508b66197bcc83df60
ladytrekker <ladytrekker@protonmail.com>
2022-03-29 19:27:32 +0200

    Fail script only if on Github

--- app/lingui.config.js
+++ app/lingui.config.js
@@ -4,15 +4,15 @@ const config = require("../lingui.config.js");
 const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
 const TRANSLATIONIO_KEY_APP = process.env.TRANSLATIONIO_KEY_APP;
 
-// Adds translation io service if env var is available
-if (GITHUB_WORKSPACE && TRANSLATIONIO_KEY_APP) {
+if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_APP) {
+  console.log("Running on GITHUB without the API key => exit");
+  throw new Error("ON GITHUB WITHOUT THE API KEY");
+} else if (TRANSLATIONIO_KEY_APP) {
+  // Adds translation io service if env var is available
   config.service = {
     name: "TranslationIO",
     apiKey: TRANSLATIONIO_KEY_APP,
   };
-} else {
-  console.log("NOT RUNNING ON GITHUB");
-  throw new Error("NOT RUNNING ON GITHUB");
 }
 
 module.exports = config;
--- site/lingui.config.js
+++ site/lingui.config.js
@@ -4,15 +4,15 @@ const config = require("../lingui.config.js");
 const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
 const TRANSLATIONIO_KEY_SITE = process.env.TRANSLATIONIO_KEY_SITE;
 
-// Adds translation io service if env var is available
-if (GITHUB_WORKSPACE && TRANSLATIONIO_KEY_SITE) {
+if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_SITE) {
+  console.log("Running on GITHUB without the API key => exit");
+  throw new Error("ON GITHUB WITHOUT THE API KEY");
+} else if (TRANSLATIONIO_KEY_SITE) {
+  // Adds translation io service if env var is available
   config.service = {
     name: "TranslationIO",
     apiKey: TRANSLATIONIO_KEY_SITE,
   };
-} else {
-  console.log("NOT RUNNING ON GITHUB");
-  throw new Error("NOT RUNNING ON GITHUB");
 }
 
 module.exports = config;
