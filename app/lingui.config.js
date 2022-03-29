1ef4a9f62e767c1acda90ca77e2e4aa1cf343f40
ladytrekker <ladytrekker@protonmail.com>
2022-03-29 14:57:00 +0200

    Do not compile strings if not on github

--- app/lingui.config.js
+++ app/lingui.config.js
@@ -1,12 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require("../lingui.config.js");
 
+const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+const TRANSLATIONIO_KEY_APP = process.env.TRANSLATIONIO_KEY_APP;
+
 // Adds translation io service if env var is available
-if (process.env.TRANSLATIONIO_KEY_APP) {
+if (GITHUB_WORKSPACE && TRANSLATIONIO_KEY_APP) {
   config.service = {
     name: "TranslationIO",
-    apiKey: process.env.TRANSLATIONIO_KEY_APP,
+    apiKey: TRANSLATIONIO_KEY_APP,
   };
+} else {
+  console.log("NOT RUNNING ON GITHUB");
+  throw new Error("NOT RUNNING ON GITHUB");
 }
 
 module.exports = config;
--- site/lingui.config.js
+++ site/lingui.config.js
@@ -1,12 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require("../lingui.config.js");
 
+const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+const TRANSLATIONIO_KEY_SITE = process.env.TRANSLATIONIO_KEY_SITE;
+
 // Adds translation io service if env var is available
-if (process.env.TRANSLATIONIO_KEY_SITE) {
+if (GITHUB_WORKSPACE && TRANSLATIONIO_KEY_SITE) {
   config.service = {
     name: "TranslationIO",
-    apiKey: process.env.TRANSLATIONIO_KEY_SITE,
+    apiKey: TRANSLATIONIO_KEY_SITE,
   };
+} else {
+  console.log("NOT RUNNING ON GITHUB");
+  throw new Error("NOT RUNNING ON GITHUB");
 }
 
 module.exports = config;
