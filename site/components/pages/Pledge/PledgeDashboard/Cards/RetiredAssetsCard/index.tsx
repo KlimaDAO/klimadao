012aae130942c9fba8db55dc7fbcf5cf45d35ace
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-04-11 00:19:29 +1000

    feat: dont display 0 retirements as decimals

--- site/components/pages/Pledge/PledgeDashboard/Cards/RetiredAssetsCard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/Cards/RetiredAssetsCard/index.tsx
@@ -12,18 +12,25 @@ type Props = {
   retirements: Retirements;
 };
 
-export const RetiredAssetsCard: FC<Props> = (props) => (
-  <BaseCard
-    title="Retired Assets"
-    icon={<LocalFireDepartmentIcon fontSize="large" />}
-  >
-    <div className={styles.value}>
-      <Text t="h1" uppercase>
-        {trimStringDecimals(props.retirements.totalTonnesRetired, 2)}k
-      </Text>
-      <Text t="h4" color="lightest">
-        Total Carbon Tonnes Retired
-      </Text>
-    </div>
-  </BaseCard>
-);
+export const RetiredAssetsCard: FC<Props> = (props) => {
+  const totalTonnesRetired =
+    Number(props.retirements.totalTonnesRetired) > 0
+      ? trimStringDecimals(props.retirements.totalTonnesRetired, 2)
+      : 0;
+
+  return (
+    <BaseCard
+      title="Retired Assets"
+      icon={<LocalFireDepartmentIcon fontSize="large" />}
+    >
+      <div className={styles.value}>
+        <Text t="h1" uppercase>
+          {totalTonnesRetired}k
+        </Text>
+        <Text t="h4" color="lightest">
+          Total Carbon Tonnes Retired
+        </Text>
+      </div>
+    </BaseCard>
+  );
+};
