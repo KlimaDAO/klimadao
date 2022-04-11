faf2188c38541b141de7a26b2915b6e02e133f92
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-04-11 23:40:48 +1000

    feat: redirect to /pledge if address is invalid

--- site/components/pages/Pledge/PledgeDashboard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { ButtonPrimary, Text } from "@klimadao/lib/components";
 import { concatAddress } from "@klimadao/lib/utils";
 
@@ -36,7 +38,9 @@ const defaultValues = (pledge: Pledge): Pledge =>
   );
 
 export const PledgeDashboard: NextPage<Props> = (props) => {
+  const router = useRouter();
   const [showModal, setShowModal] = useState(false);
+  const [validAddress, setValidAddress] = useState(false);
   const [pledge, _setPledge] = useState<Pledge>(defaultValues(props.pledge));
 
   const ToggleModal = (
@@ -47,41 +51,56 @@ export const PledgeDashboard: NextPage<Props> = (props) => {
     />
   );
 
+  useEffect(() => {
+    try {
+      ethers.utils.getAddress(props.pageAddress);
+      setValidAddress(true);
+    } catch {
+      router.push("/pledge");
+    }
+  });
+
   return (
     <PledgeLayout buttons={[ToggleModal]}>
-      <Modal
-        title="Your pledge"
-        showModal={showModal}
-        onToggleModal={() => setShowModal(!showModal)}
-      >
-        <PledgeForm />
-      </Modal>
+      {validAddress && (
+        <>
+          <Modal
+            title="Your pledge"
+            showModal={showModal}
+            onToggleModal={() => setShowModal(!showModal)}
+          >
+            <PledgeForm />
+          </Modal>
 
-      <div className={styles.contentContainer}>
-        <div className={styles.profile}>
-          <Text t="h3" className="profileImage" align="center">
-            -
-          </Text>
-          <Text t="h4">
-            {pledge.name || pledge.address || concatAddress(props.pageAddress)}
-          </Text>
-        </div>
+          <div className={styles.contentContainer}>
+            <div className={styles.profile}>
+              <Text t="h3" className="profileImage" align="center">
+                -
+              </Text>
+              <Text t="h4">
+                {pledge.name ||
+                  pledge.address ||
+                  concatAddress(props.pageAddress)}
+              </Text>
+            </div>
 
-        <div className={styles.pledgeChart}>
-          <AssetsOverTimeCard />
-        </div>
+            <div className={styles.pledgeChart}>
+              <AssetsOverTimeCard />
+            </div>
 
-        <div className={styles.column}>
-          <PledgeCard pledge={pledge.description} />
-          <FootprintCard footprint={pledge.footprint} />
-          <MethodologyCard methodology={pledge.methodology} />
-        </div>
+            <div className={styles.column}>
+              <PledgeCard pledge={pledge.description} />
+              <FootprintCard footprint={pledge.footprint} />
+              <MethodologyCard methodology={pledge.methodology} />
+            </div>
 
-        <div className={styles.column}>
-          <AssetBalanceCard pageAddress={props.pageAddress} />
-          <RetirementsCard pageAddress={props.pageAddress} />
-        </div>
-      </div>
+            <div className={styles.column}>
+              <AssetBalanceCard pageAddress={props.pageAddress} />
+              <RetirementsCard pageAddress={props.pageAddress} />
+            </div>
+          </div>
+        </>
+      )}
     </PledgeLayout>
   );
 };
