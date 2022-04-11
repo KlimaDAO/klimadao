fca57fd2f6bc99bd04e6ec5dc3ab4fb99073fa96
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-04-11 23:49:49 +1000

    refactor: base api url

--- site/queries/pledge/index.ts
+++ site/queries/pledge/index.ts
@@ -1,5 +1,10 @@
+import { urls } from "@klimadao/lib/constants";
+import { IS_LOCAL_DEVELOPMENT } from "lib/constants";
+
+const API_BASE_URL = IS_LOCAL_DEVELOPMENT ? "http://localhost:3000" : urls.home;
+
 export const getPledge = (address: string) =>
-  fetch(`http://localhost:3000/api/pledge?address=${address}`, {
+  fetch(`${API_BASE_URL}/api/pledge?address=${address}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
