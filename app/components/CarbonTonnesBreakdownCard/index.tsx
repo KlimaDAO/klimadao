d8eb8e3ee0f25308e8964c904d586c0169a288be
Atmosfearful <atmosfearful@protonmail.com>
2022-04-18 13:56:24 -0700

    enhance Breakdown cards to only show retired tokens

--- app/components/CarbonTonnesBreakdownCard/index.tsx
+++ app/components/CarbonTonnesBreakdownCard/index.tsx
@@ -9,13 +9,31 @@ import { Text } from "@klimadao/lib/components";
 import BCT from "public/icons/BCT.png";
 import MCO2 from "public/icons/MCO2.png";
 import NCT from "public/icons/NCT.png";
+import UBO from "public/icons/UBO.png";
+import NBO from "public/icons/NBO.png";
 
 import * as styles from "./styles";
 import { useSelector } from "react-redux";
 import { selectCarbonRetired } from "state/selectors";
+import { RetirementToken, retirementTokens } from "@klimadao/lib/constants";
+
+type IconMap = { [key in RetirementToken]: StaticImageData };
+
+const iconMap: IconMap = {
+  ubo: UBO,
+  nbo: NBO,
+  mco2: MCO2,
+  nct: NCT,
+  bct: BCT,
+};
 
 export const CarbonTonnesBreakdownCard: FC = () => {
-  const totalCarbonRetired = useSelector(selectCarbonRetired);
+  const carbonRetired = useSelector(selectCarbonRetired);
+  const retiredTokens = retirementTokens.filter(
+    (tkn) => carbonRetired && Number(carbonRetired[tkn]) > 0
+  );
+  const isLoading = !carbonRetired;
+  const isEmpty = !isLoading && !retiredTokens.length;
   return (
     <div className={styles.card}>
       <div className="header">
@@ -26,48 +44,36 @@ export const CarbonTonnesBreakdownCard: FC = () => {
       </div>
       <div className="cardContent">
         <div className="stack">
-          <Text t="body3">
-            <Trans id="offset.retired">Retired</Trans>
-          </Text>
-          <div className={styles.row}>
-            <div className="image">
-              <Image src={BCT} width={48} height={48} alt="BCT" />
-            </div>
-            <div className="content">
-              <Text className="value">{totalCarbonRetired?.bct || 0}</Text>
-              <Text className="label" color="lightest">
-                BCT
+          {isLoading && (
+            <Text color="lightest">
+              <Trans id="shared.loading" />
+            </Text>
+          )}
+          {isEmpty && (
+            <Text color="lightest">
+              <Trans id="offset.empty">
+                This address has not completed any retirements.
+              </Trans>
+            </Text>
+          )}
+          {retiredTokens.map((tkn) => (
+            <>
+              <Text t="body3">
+                <Trans id="offset.retired">Retired</Trans>
               </Text>
-            </div>
-          </div>
-          <Text t="body3">
-            <Trans id="offset.retired">Retired</Trans>
-          </Text>
-          <div className={styles.row}>
-            <div className="image">
-              <Image src={NCT} width={48} height={48} alt="NCT" />
-            </div>
-            <div className="content">
-              <Text className="value">{totalCarbonRetired?.nct || 0}</Text>
-              <Text className="label" color="lightest">
-                NCT
-              </Text>
-            </div>
-          </div>
-          <Text t="body3">
-            <Trans id="offset.retired">Retired</Trans>
-          </Text>
-          <div className={styles.row}>
-            <div className="image">
-              <Image src={MCO2} width={48} height={48} alt="MCO2" />
-            </div>
-            <div className="content">
-              <Text className="value">{totalCarbonRetired?.mco2 || 0}</Text>
-              <Text className="label" color="lightest">
-                MCO2
-              </Text>
-            </div>
-          </div>
+              <div className={styles.row}>
+                <div className="image">
+                  <Image src={iconMap[tkn]} width={48} height={48} alt="" />
+                </div>
+                <div className="content">
+                  <Text className="value">{carbonRetired?.[tkn] || 0}</Text>
+                  <Text className="label" color="lightest">
+                    {tkn.toUpperCase()}
+                  </Text>
+                </div>
+              </div>
+            </>
+          ))}
         </div>
       </div>
     </div>
