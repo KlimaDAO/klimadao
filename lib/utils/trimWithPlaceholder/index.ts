4a33d0560a444a9a3e897cae422d46ecc824f21b
ladytrekker <ladytrekker@protonmail.com>
2022-04-20 11:51:08 +0200

    Add locale to trimWithPlaceholder

--- lib/utils/trimWithPlaceholder/index.ts
+++ lib/utils/trimWithPlaceholder/index.ts
@@ -1,12 +1,13 @@
 /** Returns localized string, or falls back to "loading..." if no number provided */
 export const trimWithPlaceholder = (
   number: string | number | undefined,
-  precision: number
+  precision: number,
+  locale: string
 ) => {
   if (typeof number === "undefined" || Number.isNaN(number)) {
     return "Loading... ";
   }
-  return Number(number).toLocaleString("en", {
+  return Number(number).toLocaleString(locale, {
     maximumFractionDigits: precision,
   });
 };
