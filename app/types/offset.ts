89995e8875f25152b849653bfe0bfec05d4eb0f3
ladytrekker <ladytrekker@protonmail.com>
2022-05-02 12:00:32 +0200

    Add types for RetirementReceipt and RetirementTotals

---
+++ app/types/offset.ts
@@ -0,0 +1,65 @@
+import { BigNumber } from "ethers";
+
+interface GasUsed {
+  type: string;
+  hex: string;
+}
+
+interface Log {
+  transactionIndex: number;
+  blockNumber: number;
+  transactionHash: string;
+  address: string;
+  topics: string[];
+  data: string;
+  logIndex: number;
+  blockHash: string;
+}
+
+interface CumulativeGasUsed {
+  type: string;
+  hex: string;
+}
+
+interface EffectiveGasPrice {
+  type: string;
+  hex: string;
+}
+
+interface Event {
+  transactionIndex: number;
+  blockNumber: number;
+  transactionHash: string;
+  address: string;
+  topics: string[];
+  data: string;
+  logIndex: number;
+  blockHash: string;
+}
+
+export interface RetirementReceipt {
+  to: string;
+  from: string;
+  contractAddress?: any;
+  transactionIndex: number;
+  gasUsed: GasUsed;
+  logsBloom: string;
+  blockHash: string;
+  transactionHash: string;
+  logs: Log[];
+  blockNumber: number;
+  confirmations: number;
+  cumulativeGasUsed: CumulativeGasUsed;
+  effectiveGasPrice: EffectiveGasPrice;
+  status: number;
+  type: number;
+  byzantium: boolean;
+  events: Event[];
+}
+
+export type RetirementTotals = [BigNumber, BigNumber, BigNumber];
+
+export type RetireCarbonTransactionResult = {
+  receipt: RetirementReceipt;
+  retirementTotals: ReturnType<BigNumber["toNumber"]>;
+};
