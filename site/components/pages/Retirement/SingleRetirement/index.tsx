b0d0be0ae44ebecf4842f7a0873df8ac6f21535b
ladytrekker <ladytrekker@protonmail.com>
2022-05-09 18:36:37 +0200

    Render Single Retirement

--- site/components/pages/Retirement/SingleRetirement/index.tsx
+++ site/components/pages/Retirement/SingleRetirement/index.tsx
@@ -7,15 +7,22 @@ import { RetirementIndexInfoResult } from "@klimadao/lib/types/offset";
 import { Navigation } from "components/Navigation";
 import { PageHead } from "components/PageHead";
 import { Footer } from "components/Footer";
+import { RetirementHeader } from "./RetirementHeader";
+import { RetirementMessage } from "./RetirementMessage";
+import { RetirementValue } from "./RetirementValue";
+import { RetirementDate } from "./RetirementDate";
+import { TextGroup } from "./TextGroup";
 
 import { IS_PRODUCTION } from "lib/constants";
-import { t } from "@lingui/macro";
-import * as styles from "../styles";
+import { Trans, t } from "@lingui/macro";
+import * as styles from "./styles";
+
+import { retirementTokenInfoMap } from "../../../../lib/getTokenInfo";
 
 type Props = {
   beneficiaryAddress: string;
   retirementTotals: string;
-  retirement: KlimaRetire;
+  retirement?: KlimaRetire;
   retirementIndexInfo: RetirementIndexInfoResult;
 };
 
@@ -26,6 +33,18 @@ export const SingleRetirementPage: NextPage<Props> = (props) => {
     retirement,
     retirementIndexInfo,
   } = props;
+
+  // collect from indexInfo and optional data from subgraph
+  const retireData = {
+    amount: retirementIndexInfo.amount,
+    typeOfToken: retirementIndexInfo.typeOfToken,
+    beneficiaryName: retirementIndexInfo.beneficiaryName,
+    retirementMessage: retirementIndexInfo.retirementMessage,
+    timestamp: retirement?.timestamp,
+    transactionID: retirement?.transaction?.id,
+  };
+
+  const tokenData = retirementTokenInfoMap[retireData.typeOfToken];
 
   return (
     <>
@@ -50,37 +69,82 @@ export const SingleRetirementPage: NextPage<Props> = (props) => {
       <Navigation activePage="Home" />
 
       <Section variant="gray" className={styles.section}>
-        <div className={styles.retirementContainer}>
-          <div className={styles.retirement_textGroup}>
-            <Text t="h2" as="h2" align="center">
-              Retirement SINGLE
-            </Text>
-            <Text align="center">timestamp: {retirement.timestamp}</Text>
-            <Text align="center">
-              tokenAddress: {retirement.offset.tokenAddress}
-            </Text>
-            <Text align="center">amount: {retirement.amount}</Text>
-            <Text align="center">
-              typeOfToken: {retirementIndexInfo.typeOfToken}
-            </Text>
-            <Text align="center">
-              beneficiaryName: {retirement.beneficiary}
-            </Text>
-            <Text align="center">
-              beneficiaryAddress: {retirement.beneficiaryAddress}
-            </Text>
-            <Text align="center">message: {retirement.retirementMessage}</Text>
-            <Text align="center">
-              View on{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={`https://polygonscan.com/tx/${retirement.transaction.id}`}
-              >
-                polygonscan.com
-              </a>
-            </Text>
+        <RetirementHeader
+          title={`
+            ${retireData.amount} ${tokenData.label} ${t({
+            id: "retirement.single.header.offset",
+            message: "Offset",
+          })}`}
+          subline={retireData.beneficiaryName}
+        />
+        <div className={styles.retirementContent}>
+          {!!retireData.retirementMessage && (
+            <RetirementMessage message={retireData.retirementMessage} />
+          )}
+          <RetirementValue
+            value={retireData.amount}
+            label={tokenData.label}
+            icon={tokenData.icon}
+          />
+          <div className={styles.metaData}>
+            <div className="column">
+              <TextGroup
+                title={
+                  <Trans id="retirement.single.beneficiary.title">
+                    Beneficiary
+                  </Trans>
+                }
+                text={retireData.beneficiaryName}
+              />
+              <TextGroup
+                title={
+                  <Trans id="retirement.single.beneficiaryAddress.title">
+                    Beneficiary Address
+                  </Trans>
+                }
+                text={
+                  retireData.transactionID ? (
+                    <a
+                      className="address"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://polygonscan.com/tx/${retireData.transactionID}`}
+                    >
+                      {beneficiaryAddress}
+                    </a>
+                  ) : (
+                    { beneficiaryAddress }
+                  )
+                }
+              />
+            </div>
+            <div className="column">
+              {retireData.timestamp && (
+                <RetirementDate timestamp={retireData.timestamp} />
+              )}
+              <TextGroup
+                title={
+                  <Trans id="retirement.single.retirementCertificate.title">
+                    Certificate
+                  </Trans>
+                }
+                text={
+                  <Trans id="retirement.single.retirementCertificate.link">
+                    ...coming soon
+                  </Trans>
+                }
+              />
+            </div>
           </div>
+          <Text className={styles.data_description}>
+            <Trans id="retirement.single.description">
+              This represents the permanent on-chain retirement of tokenized
+              carbon assets. This data is written publically and immutably on
+              the Polygon blockchain. Our open-source, permissionless,
+              decentralized system ensures that these tokens or the underlying
+              offsets can never be retired more than once.
+            </Trans>
+          </Text>
         </div>
       </Section>
       <Footer />
---
+++ site/components/pages/Retirement/SingleRetirement/styles.ts
@@ -0,0 +1,49 @@
+import { css } from "@emotion/css";
+import breakpoints from "@klimadao/lib/theme/breakpoints";
+
+export const section = css`
+  ${breakpoints.medium} {
+    padding-top: 9rem;
+    padding-bottom: 9rem;
+  }
+`;
+
+export const retirementContent = css`
+  background-color: var(--surface-01);
+  border-radius: 0 0 1.2rem 1.2rem;
+  grid-column: main;
+  display: grid;
+  gap: 2.8rem;
+  padding: 2.8rem 1.5rem;
+
+  ${breakpoints.medium} {
+    gap: 5.2rem;
+    padding: 5.2rem;
+  }
+`;
+
+export const metaData = css`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+
+  ${breakpoints.medium} {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 3rem;
+    flex-direction: row;
+  }
+
+  .column {
+    display: flex;
+    gap: 4rem;
+    flex-direction: column;
+  }
+`;
+
+export const data_description = css`
+  margin: 0 auto;
+  ${breakpoints.medium} {
+    max-width: 75%;
+  }
+`;
