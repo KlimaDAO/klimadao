7fe6b339db982fd144b4b0ae1d5d25f604e20065
ladytrekker <ladytrekker@protonmail.com>
2022-05-09 18:35:17 +0200

    Add RetirementValue

---
+++ site/components/pages/Retirement/SingleRetirement/RetirementValue/index.tsx
@@ -0,0 +1,28 @@
+import { Text } from "@klimadao/lib/components";
+import * as styles from "./styles";
+import { FC } from "react";
+import { RetirementToken } from "@klimadao/lib/constants";
+import Image from "next/image";
+import { Trans, t } from "@lingui/macro";
+
+type Props = {
+  value: string;
+  label: Uppercase<RetirementToken>;
+  icon: StaticImageData;
+};
+
+export const RetirementValue: FC<Props> = ({ value, label, icon }) => {
+  return (
+    <div className={styles.retirementValue_textGroup}>
+      <Text t="caption" align="center" color="lightest" uppercase>
+        <Trans id="retirement.single.value.title">Value</Trans>
+      </Text>
+      <div className={styles.tokenInfo}>
+        <Image alt={label} src={icon} width={48} height={48} />
+        <Text t="h3" as="h2" align="center" className="amount">
+          {value} {label}
+        </Text>
+      </div>
+    </div>
+  );
+};
---
+++ site/components/pages/Retirement/SingleRetirement/RetirementValue/styles.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/css";
+
+export const retirementValue_textGroup = css`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const tokenInfo = css`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
