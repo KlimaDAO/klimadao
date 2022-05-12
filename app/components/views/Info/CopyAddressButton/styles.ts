308aca52edafa22b96d9274795118fc6fee276ac
Joey <joeyr84@gmail.com>
2022-05-12 18:41:43 -0400

    info components moved from css modules to emotion css (#390)
    * info components moved from css modules to emotion css
    
    * fixed import and deleted css module
    
    * prettier format

---
+++ app/components/views/Info/CopyAddressButton/styles.ts
@@ -0,0 +1,17 @@
+import { css } from "@emotion/css";
+
+// used !important here bc .Home-container class is more specific than this one
+export const copyAddressButtonIcon = css`
+  height: 1.6rem !important;
+  width: 1.6rem !important;
+  cursor: pointer;
+`;
+
+export const checkmark = css`
+  padding: 0.8rem;
+  max-height: 3.2rem;
+`;
+
+export const copyAddressButton = css`
+  padding: 0.8rem;
+`;
