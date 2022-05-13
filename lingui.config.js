0438e31a9ece796ea9fc837ddaf2a02fd4ecf4f7
ladytrekker <ladytrekker@protonmail.com>
2022-05-13 12:49:25 +0200

    Add spanish and korean

--- lingui.config.js
+++ lingui.config.js
@@ -4,7 +4,7 @@
 
 /** @type {Config} */
 const config = {
-  locales: ["en", "zh-CN", "en-pseudo", "fr", "de", "ru"],
+  locales: ["en", "zh-CN", "en-pseudo", "fr", "de", "ru", "es", "ko"],
   sourceLocale: "en",
   fallbackLocales: {
     default: "en",
