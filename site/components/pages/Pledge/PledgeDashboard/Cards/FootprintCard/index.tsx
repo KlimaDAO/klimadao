62c96394eaf538e2b1bafaf0e8faabf6ff9df0fd
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-05-22 14:36:16 +1000

    display categories in footprint card

--- site/components/pages/Pledge/PledgeDashboard/Cards/FootprintCard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/Cards/FootprintCard/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import LocalGasStationOutlinedIcon from "@mui/icons-material/LocalGasStationOutlined";
 import { Text } from "@klimadao/lib/components";
 
-import { Footprint } from "../../../types";
+import { Footprint, Category } from "../../../types";
 import { BaseCard } from "../BaseCard";
 import * as styles from "./styles";
 
@@ -10,15 +10,34 @@ type Props = {
   footprint: Footprint[];
 };
 
+interface CategoryWithPercent extends Category {
+  percent: number;
+}
+
+const calculatePercent = (
+  total: number,
+  categories: Category[]
+): CategoryWithPercent[] => {
+  return categories.map((category) => ({
+    ...category,
+    percent: Math.round((category.quantity / total) * 100),
+  }));
+};
+
 export const FootprintCard: FC<Props> = (props) => {
   const footprint = props.footprint[props.footprint.length - 1];
+
+  const categoriesWithPercent = calculatePercent(
+    footprint.total,
+    footprint.categories
+  );
 
   return (
     <BaseCard
       title="Footprint"
       icon={<LocalGasStationOutlinedIcon fontSize="large" />}
     >
-      <div className={styles.value}>
+      <div className={styles.footprintTotal}>
         <Text t="h1" uppercase>
           {footprint.total}k
         </Text>
@@ -26,6 +45,27 @@ export const FootprintCard: FC<Props> = (props) => {
           Tonnes
         </Text>
       </div>
+
+      {footprint.total === 0 ? null : (
+        <div className={styles.categories}>
+          {categoriesWithPercent.map((category, index) => (
+            <div key={index}>
+              <div className={styles.categoryRow} key={index}>
+                <Text t="h4" className={styles.categoryRow_name} uppercase>
+                  {category.name}
+                </Text>
+                <Text t="h4" uppercase>
+                  {category.quantity}k{" "}
+                  <span className={styles.categoryRow_divider}>|</span>{" "}
+                  <span className={styles.categoryRow_percentage}>
+                    {category.percent}%
+                  </span>
+                </Text>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </BaseCard>
   );
 };
--- site/components/pages/Pledge/PledgeDashboard/Cards/FootprintCard/styles.ts
+++ site/components/pages/Pledge/PledgeDashboard/Cards/FootprintCard/styles.ts
@@ -1,6 +1,44 @@
 import { css } from "@emotion/css";
+import breakpoints from "@klimadao/lib/theme/breakpoints";
 
-export const value = css`
+export const footprintTotal = css`
   display: grid;
   gap: 1rem;
 `;
+
+export const categories = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const categoryRow = css`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  flex-direction: column;
+
+  ${breakpoints.medium} {
+    flex-direction: row;
+  }
+
+  p {
+    font-size: 2.0rem;
+
+    ${breakpoints.medium} {
+      font-size: 2.6rem;
+    }
+  }
+`
+
+export const categoryRow_name = css`
+  text-overflow: ellipsis;
+`
+
+export const categoryRow_divider = css`
+  color: var(--font-03);
+`
+
+export const categoryRow_percentage = css`
+  color: var(--klima-green)
+`
