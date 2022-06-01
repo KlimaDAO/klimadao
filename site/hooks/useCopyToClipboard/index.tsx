e3cbeb8ac4721f2102720b448781558f024780a6
ladytrekker <ladytrekker@protonmail.com>
2022-06-01 16:06:48 +0200

    Add hook useCopyToClipboard

---
+++ site/hooks/useCopyToClipboard/index.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useCopyToClipboard = (
+  timeOut = 3000
+): [boolean, (stringToCopy: string) => void] => {
+  const [copied, setCopied] = useState(false);
+
+  const doCopy = async (stringToCopy: string) => {
+    await navigator.clipboard.writeText(stringToCopy);
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    if (copied) {
+      const timer = setTimeout(() => {
+        setCopied(false);
+      }, timeOut);
+      return () => {
+        !!timer && clearTimeout(timer);
+      };
+    }
+  }, [copied]);
+
+  return [copied, doCopy];
+};
