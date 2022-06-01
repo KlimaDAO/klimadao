35f416aade3e94e159ec0735df33aff67a2db881
ladytrekker <ladytrekker@protonmail.com>
2022-06-01 14:09:48 +0200

    Changes for Breakdown

--- site/components/pages/Retirements/Breakdown/index.tsx
+++ site/components/pages/Retirements/Breakdown/index.tsx
@@ -1,11 +1,9 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import { Text } from "@klimadao/lib/components";
-
+import { useRouter } from "next/router";
 import { RetirementsTotalsAndBalances } from "@klimadao/lib/types/offset";
-
-import CloudQueueIcon from "@mui/icons-material/CloudQueue";
-
+import { trimStringDecimals } from "@klimadao/lib/utils";
 import { allRetirementTokenInfos } from "lib/getTokenInfo";
 
 import { Trans } from "@lingui/macro";
@@ -17,40 +15,27 @@ type Props = {
 
 export const Breakdown: NextPage<Props> = (props) => {
   const { totalsAndBalances } = props;
-
+  const { locale } = useRouter();
   const breakdownTokens = allRetirementTokenInfos
     .map((tkn) => ({
       ...tkn,
       amount:
         totalsAndBalances[tkn.key as keyof RetirementsTotalsAndBalances] || "0",
     }))
-    .sort((a, b) => Number(b.amount) - Number(a.amount));
+    .sort((a, b) => Number(b.amount) - Number(a.amount))
+    .filter((tkn) => Number(tkn.amount) > 0);
 
   return (
     <div className={styles.breakdown}>
       <div className={styles.breakdownHeadline}>
         <Text t="h3" as="h3" align="center" className="title">
-          <CloudQueueIcon fontSize="inherit" />
-          <Trans id="retirement.totals.breakdown_headline">Breakdown</Trans>
-        </Text>
-        <Text t="h4" color="lightest" align="center">
-          <Trans id="retirement.totals.breakdown_subline">
-            Tokens used for retirements
+          <Trans id="retirement.totals.assets_used_headline">
+            Assets used for retirements
           </Trans>
         </Text>
       </div>
       <div className={styles.breakdownList}>
         {breakdownTokens.map((tkn, index) => {
-          const amount =
-            Number(tkn.amount) > 0 ? (
-              <Text>{tkn.amount}</Text>
-            ) : (
-              <Text color="lightest" t="caption">
-                <Trans id="retirement.totals.breakdown.no_retirement_found">
-                  No retirements found
-                </Trans>
-              </Text>
-            );
           return (
             <div className={styles.breakdownListItem} key={`${tkn}-${index}`}>
               <Image
@@ -60,7 +45,11 @@ export const Breakdown: NextPage<Props> = (props) => {
                 alt={`${tkn.label}-icon`}
               />
               <div className="content">
-                {amount}
+                <Text>
+                  {Number(trimStringDecimals(tkn.amount, 5)).toLocaleString(
+                    locale
+                  )}
+                </Text>
                 <Text color="lightest">{tkn.label}</Text>
               </div>
             </div>
