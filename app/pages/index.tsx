b89a1d2f5c5c3e689663c510680f6018cca42881
Atmosfearful <atmosfearful@protonmail.com>
2022-06-02 16:19:42 -0700

    add context to index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,6 +7,8 @@ import { IS_PRODUCTION } from "lib/constants";
 import { urls } from "@klimadao/lib/constants";
 import { messages as default_messages } from "../locale/en/messages";
 import { i18n } from "@lingui/core";
+import { Web3ContextProvider } from "@klimadao/lib/components";
+import { getWeb3ModalStrings } from "lib/getWeb3ModalStrings";
 
 export async function getStaticProps() {
   i18n.load("en", default_messages);
@@ -15,19 +17,25 @@ export async function getStaticProps() {
     props: {},
   };
 }
+
 const HomePage: NextPage = () => {
+  /**
+   * TODO: force re-render when redux locale changes.
+   */
   return (
     <WithRedux>
-      <WithIsomorphicRouter location="/#">
-        <PageHead
-          production={IS_PRODUCTION}
-          title="KlimaDAO | Official App"
-          mediaTitle="KlimaDAO | Official App"
-          metaDescription="Use the KLIMA web app to bond, stake and earn rewards."
-          mediaImageSrc={urls.mediaImage}
-        />
-        <Home />
-      </WithIsomorphicRouter>
+      <Web3ContextProvider strings={getWeb3ModalStrings()}>
+        <WithIsomorphicRouter location="/#">
+          <PageHead
+            production={IS_PRODUCTION}
+            title="KlimaDAO | Official App"
+            mediaTitle="KlimaDAO | Official App"
+            metaDescription="Use the KLIMA web app to bond, stake and earn rewards."
+            mediaImageSrc={urls.mediaImage}
+          />
+          <Home />
+        </WithIsomorphicRouter>
+      </Web3ContextProvider>
     </WithRedux>
   );
 };
