f229bdcea524358333bebfea732dc2e9dccaafad
ladytrekker <ladytrekker@protonmail.com>
2022-06-06 16:34:09 +0200

    Add Site lib getAddressByDomain

---
+++ site/lib/getAddressByDomain.ts
@@ -0,0 +1,25 @@
+import {
+  isKNSDomain,
+  getAddressByKNS,
+  isENSDomain,
+  getAddressByENS,
+} from "@klimadao/lib/utils";
+
+export const getAddressByDomain = async (
+  domain: string
+): Promise<string | null> => {
+  try {
+    let address;
+    if (isKNSDomain(domain)) {
+      address = await getAddressByKNS(domain);
+    } else if (isENSDomain(domain)) {
+      address = await getAddressByENS(domain);
+    } else {
+      address = null;
+    }
+    return address;
+  } catch (e) {
+    console.log("Error in getAddressByDomain", e);
+    return Promise.reject(e);
+  }
+};
