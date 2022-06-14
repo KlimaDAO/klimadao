b3f4a5c54417d1f7cc6d8143a891b27342a61316
ladytrekker <ladytrekker@protonmail.com>
2022-06-14 12:58:27 +0200

    Add site lib getDomainByAddress

---
+++ site/lib/getDomainByAddress.ts
@@ -0,0 +1,20 @@
+import {
+  getKNSByAddress,
+  createKNSDomainFromName,
+  getENSByAddress,
+} from "@klimadao/lib/utils";
+import { getInfuraUrlEther } from "lib/getInfuraUrl";
+
+export const getDomainByAddress = async (
+  address: string
+): Promise<string | null> => {
+  try {
+    const knsName = await getKNSByAddress(address);
+    const kns = !!knsName && createKNSDomainFromName(knsName);
+    const ens = !kns && (await getENSByAddress(address, getInfuraUrlEther())); // Caution: needs to be InfuraUrl for Ether here
+    return kns || ens || null;
+  } catch (e) {
+    console.error("Error in getDomainByAddress", e);
+    return Promise.reject(e);
+  }
+};
