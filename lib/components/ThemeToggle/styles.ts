10617b320606dc4b9fa922e4c35622f66d16c416
ladytrekker <ladytrekker@protonmail.com>
2022-07-04 12:26:45 +0200

    Fix intendation of styles

--- lib/components/ThemeToggle/styles.ts
+++ lib/components/ThemeToggle/styles.ts
@@ -11,19 +11,22 @@ export const buttonToggle = css`
   /* min-height to conform with Lighthouse min tap-target */
   min-height: 4.8rem;
   min-width: 4.8rem;
-  padding: 0rem
+  padding: 0rem;
   cursor: pointer;
   border-radius: var(--border-radius);
   transition: opacity 0.3s ease 0s;
   background-color: var(--surface-01);
 
-  &:hover, &:focus {
+  &:hover,
+  &:focus {
     opacity: 0.7;
   }
   &:active {
     transform: scale(0.9);
   }
-  &, &:hover, &:visited {
+  &,
+  &:hover,
+  &:visited {
     color: white; /* same in darkmode */
   }
 `;
