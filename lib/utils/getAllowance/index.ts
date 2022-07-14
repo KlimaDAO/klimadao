222a9e28fa140ee5d6d0fc4bad711f0eae41e869
ladytrekker <ladytrekker@protonmail.com>
2022-07-14 18:52:18 +0200

    Add lib getSpenderAddress

--- lib/utils/getAllowance/index.ts
+++ lib/utils/getAllowance/index.ts
@@ -42,3 +42,6 @@ export const getAllowance = async (params: {
 
 export const getTokensFromSpender = (spender: AllowancesSpender) =>
   allowancesContracts[spender];
+
+export const getSpenderAddress = (spender: AllowancesSpender): string | false =>
+  isSpenderInAddresses(spender) && addresses["mainnet"][spender];
