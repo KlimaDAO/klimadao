edee43997691493c2a71c21293308131434bf540
Angus Fretwell <angusfretwell@me.com>
2022-07-19 23:15:21 +1000

    add social proofs section to home page

--- site/components/pages/Home/index.tsx
+++ site/components/pages/Home/index.tsx
@@ -24,6 +24,15 @@ import burningForest from "public/burning-forest.jpg";
 import sprouts from "public/sprouts.jpg";
 import dummyswap from "public/dummyswap.png";
 
+import BloombergLogo from "./Logos/BloombergLogo";
+import CointelegraphLogo from "./Logos/CointelegraphLogo";
+import ForbesLogo from "./Logos/ForbesLogo";
+import NasdaqLogo from "./Logos/NasdaqLogo";
+import TheTimesLogo from "./Logos/TheTimesLogo";
+import WiredLogo from "./Logos/WiredLogo";
+import WallStreetJournalLogo from "./Logos/WallStreetJournalLogo";
+import YahooFinanceLogo from "./Logos/YahooFinanceLogo";
+
 import * as styles from "./styles";
 
 export interface Props {
@@ -171,6 +180,47 @@ export const Home: NextPage<Props> = (props) => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
+          </div>
+        </div>
+      </Section>
+      <Section variant="gray" className={styles.socialProofSection}>
+        <div className="socialProof_container">
+          <Text t="h5" as="h2">
+            <Trans id="home.featured_on">KlimaDAO featured on</Trans>
+          </Text>
+
+          <div className="socialProof_logos">
+            <div className="socialProof_logos_item">
+              <BloombergLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <CointelegraphLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <ForbesLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <NasdaqLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <TheTimesLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <WiredLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <WallStreetJournalLogo />
+            </div>
+
+            <div className="socialProof_logos_item">
+              <YahooFinanceLogo />
+            </div>
           </div>
         </div>
       </Section>
