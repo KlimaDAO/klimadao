8c90f21185fc81b6e3b950685492715b067eddc5
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-07-21 18:11:55 +1000

    change methodology card icon

--- site/components/pages/Pledge/PledgeDashboard/Cards/MethodologyCard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/Cards/MethodologyCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import { Text } from "@klimadao/lib/components";
 
 import { BaseCard } from "../BaseCard";
@@ -13,7 +13,7 @@ export const MethodologyCard: FC<Props> = (props) => {
   return (
     <BaseCard
       title="Methodology"
-      icon={<HowToRegOutlinedIcon fontSize="large" />}
+      icon={<DescriptionOutlinedIcon fontSize="large" />}
     >
       <Text t="body2">{props.methodology || defaultText}</Text>
     </BaseCard>
