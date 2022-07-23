2cccb0336d376a94f3d735fd376714e0eeb8a7eb
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-07-23 17:58:02 +1000

    fix getBalances

--- site/lib/getBalances.ts
+++ site/lib/getBalances.ts
@@ -9,30 +9,38 @@ export type Balances = {
   klima: string;
   sklima: string;
   bct: string;
-  mco2: string;
   nct: string;
+  mco2: string;
+  nbo: string;
+  ubo: string;
 };
 
 export const getBalances = async (params: Params): Promise<Balances> => {
   const provider = getJsonRpcProvider();
 
+  const klimaContract = getContract({ contractName: "klima", provider });
+  const sklimaContract = getContract({ contractName: "sklima", provider });
   const bctContract = getContract({ contractName: "bct", provider });
   const nctContract = getContract({ contractName: "nct", provider });
   const mco2Contract = getContract({ contractName: "mco2", provider });
-  const klimaContract = getContract({ contractName: "klima", provider });
-  const sklimaContract = getContract({ contractName: "sklima", provider });
+  const nboContract = getContract({ contractName: "nbo", provider });
+  const uboContract = getContract({ contractName: "ubo", provider });
 
   const klimaBalance = await klimaContract.balanceOf(params.address);
   const sklimaBalance = await sklimaContract.balanceOf(params.address);
   const bctBalance = await bctContract.balanceOf(params.address);
-  const mco2Balance = await mco2Contract.balanceOf(params.address);
   const nctBalance = await nctContract.balanceOf(params.address);
+  const mco2Balance = await mco2Contract.balanceOf(params.address);
+  const nboBalance = await nboContract.balanceOf(params.address);
+  const uboBalance = await uboContract.balanceOf(params.address);
 
   return {
     klima: formatUnits(klimaBalance, 9),
     sklima: formatUnits(sklimaBalance, 9),
     bct: formatUnits(bctBalance),
-    mco2: formatUnits(nctBalance),
-    nct: formatUnits(mco2Balance),
+    nct: formatUnits(nctBalance),
+    mco2: formatUnits(mco2Balance),
+    nbo: formatUnits(nboBalance),
+    ubo: formatUnits(uboBalance),
   };
 };
