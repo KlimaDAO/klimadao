496cae13b8741b2b92d1c0cb36f9c2c8d5a1787a
ladytrekker <95881624+ladytrekker@users.noreply.github.com>
2022-07-25 15:01:42 +0200

    Use same Modal component (#537)
    * Add generic Component Modal
    
    * Use Modal in DropdownWithModal component
    
    * Use Modal for InvalidRPCModal
    
    * Use Modal in InvalidNetworkModal
    
    * Usw Modal for InvalidNetworkModal
    
    * Use Modal in NotificationModal
    
    * Use Modal in RetirementSuccessModal
    
    * Remove console.log
    
    * extract strings after rebase

--- app/components/InvalidRPCModal/index.tsx
+++ app/components/InvalidRPCModal/index.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
-import styles from "./index.module.css";
-import t from "@klimadao/lib/theme/typography.module.css";
+import { Modal } from "components/Modal";
+import { Trans } from "@lingui/macro";
 import { Anchor as A } from "@klimadao/lib/components";
 import { urls } from "@klimadao/lib/constants";
+
+import { Text, ButtonPrimary } from "@klimadao/lib/components";
 
 interface Props {
   onHide: () => void;
@@ -12,23 +14,31 @@ const METAMASK_TROUBLESHOOT_LINK =
   "https://klimadao.notion.site/Fix-LOADING-and-MetaMask-Network-RPC-Issues-ea26b4805440406c95385e57177a6407";
 
 export const InvalidRPCModal: FC<Props> = (props) => (
-  <div className={styles.bg}>
-    <div className={styles.card}>
-      <div className={styles.card_header}>
-        <h2 className={t.h4}>⚠ Check your network settings</h2>
-        <p className={t.body2}>
+  <Modal
+    title={
+      <>
+        ⚠<Trans id="invalid_rpc.modal.title">Check your network settings</Trans>
+      </>
+    }
+    onToggleModal={props.onHide}
+  >
+    <div>
+      <Text color="lighter" t="caption">
+        <Trans id="invalid_rpc.modal.text">
           The network could not be reached. This is most likely caused by an old
           Polygon RPC configuration in your wallet. See{" "}
           <A href={METAMASK_TROUBLESHOOT_LINK}>this guide</A> for a fix.
           Otherwise, reach out to us on <A href={urls.discordInvite}>Discord</A>{" "}
           if problems persist.
-        </p>
-      </div>
-      <div style={{ display: "flex", justifyContent: "end", gap: "0.8rem" }}>
-        <button onClick={props.onHide} className={styles.switchNetworkButton}>
-          OK
-        </button>
-      </div>
+        </Trans>
+      </Text>
     </div>
-  </div>
+    <div style={{ display: "flex", justifyContent: "end", gap: "0.8rem" }}>
+      <ButtonPrimary
+        onClick={props.onHide}
+        label="OK"
+        variant="blue"
+      ></ButtonPrimary>
+    </div>
+  </Modal>
 );
