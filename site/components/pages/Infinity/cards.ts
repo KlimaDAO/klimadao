1060b76bfc6e08e04a22393ecda068f692e1fdff
ladytrekker <ladytrekker@protonmail.com>
2022-07-26 13:38:14 +0200

    Remove doubled cards

--- site/components/pages/Infinity/cards.ts
+++ site/components/pages/Infinity/cards.ts
@@ -22,45 +22,14 @@ export const cards = [
   },
   {
     logo: markCubanLogo,
-    link: "google.com",
+    link: "https://google.com",
     description: "Retire to offset corporate carbon usage.",
     tonsRetired: "2",
     date: "8 MAY 2022",
   },
   {
     logo: stadiusLogo,
-    link: "google.com",
-    description:
-      "Offsetting team and operational carbon emissions since founding.",
-    tonsRetired: 12,
-    date: "4 MAY 2022",
-  },
-  {
-    logo: olympusLogo,
-    link: "https://app.olympusdao.finance/#/stake",
-    description:
-      "We're proud to offset 10+ years of carbon emissions from Olympus to become Climate Positive. From the Ohmies, with Love.",
-    tonsRetired: 33,
-    date: "14 MAY 2022",
-  },
-  {
-    logo: polygonLogo,
-    link: "https://polygon.technology/",
-    description:
-      "We care deeply about our work, and in turn we care deeply about preserving it. This also applies more generally, and we wish to preserve the planet which has afforded us this freedom.",
-    tonsRetired: 104,
-    date: "10 MAY 2022",
-  },
-  {
-    logo: markCubanLogo,
-    link: "google.com",
-    description: "Retire to offset corporate carbon usage.",
-    tonsRetired: "2",
-    date: "",
-  },
-  {
-    logo: stadiusLogo,
-    link: "google.com",
+    link: "https://google.com",
     description:
       "Offsetting team and operational carbon emissions since founding.",
     tonsRetired: 12,
