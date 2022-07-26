3e1699ef27b61fad0c7e5420c77a955b39fad249
ladytrekker <ladytrekker@protonmail.com>
2022-07-26 17:45:59 +0200

    Fix links to retirement, dates and tonnes

--- site/components/pages/Infinity/cards.ts
+++ site/components/pages/Infinity/cards.ts
@@ -6,7 +6,7 @@ import stadiusLogo from "public/logo-stadius.png";
 export const cards = [
   {
     logo: olympusLogo,
-    link: "https://app.olympusdao.finance/#/stake",
+    link: "https://www.klimadao.finance/retirements/0xe06efa3d9ee6923240ee1195a16ddd96b5cce8f7",
     description:
       "We're proud to offset 10+ years of carbon emissions from Olympus to become Climate Positive. From the Ohmies, with Love.",
     tonsRetired: 33,
@@ -14,25 +14,25 @@ export const cards = [
   },
   {
     logo: polygonLogo,
-    link: "https://polygon.technology/",
+    link: "https://www.klimadao.finance/retirements/0x087a7afb6975a2837453be685eb6272576c0bc06",
     description:
       "We care deeply about our work, and in turn we care deeply about preserving it. This also applies more generally, and we wish to preserve the planet which has afforded us this freedom.",
-    tonsRetired: 104,
+    tonsRetired: 65201,
     date: "10 MAY 2022",
   },
   {
     logo: markCubanLogo,
-    link: "https://google.com",
+    link: "https://www.klimadao.finance/retirements/markcuban.klima",
     description: "Retire to offset corporate carbon usage.",
-    tonsRetired: "2",
-    date: "8 MAY 2022",
+    tonsRetired: 1100,
+    date: "4 APRIL 2022",
   },
   {
     logo: stadiusLogo,
-    link: "https://google.com",
+    link: "https://www.klimadao.finance/retirements/stadius.eth",
     description:
       "Offsetting team and operational carbon emissions since founding.",
-    tonsRetired: 12,
-    date: "4 MAY 2022",
+    tonsRetired: 75,
+    date: "6 APRIL 2022",
   },
 ];
