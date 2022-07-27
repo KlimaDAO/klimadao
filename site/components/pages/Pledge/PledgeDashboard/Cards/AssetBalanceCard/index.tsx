aa9857c8c2e52e971983fd151fb61adb32ceaa5b
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-07-27 22:59:40 +1000

    pull TokenRow into own file

--- site/components/pages/Pledge/PledgeDashboard/Cards/AssetBalanceCard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/Cards/AssetBalanceCard/index.tsx
@@ -18,6 +18,7 @@ import { Holding } from "../../../lib/subgraph";
 import { BaseCard } from "../BaseCard";
 import { PlaceholderHoldingsChart } from "./PlaceholderHoldingsChart";
 import { HoldingsChart } from "./HoldingsChart";
+import { TokenRow } from "./TokenRow";
 import * as styles from "./styles";
 
 type Props = {
@@ -61,52 +62,6 @@ const TOKEN_MAP: TokenMap = {
     label: "UBO",
     icon: UBOIcon,
   },
-};
-
-interface TokenRowProps {
-  label: string;
-  icon: StaticImageData;
-  balance: string | null;
-  holdings: Holding[];
-}
-
-const TokenRow: FC<TokenRowProps> = (props) => {
-  const { locale } = useRouter();
-  const formatBalance = (balance: string) =>
-    Number(balance) > 0.01
-      ? Number(trimStringDecimals(balance, 2)).toLocaleString(locale)
-      : 0;
-
-  return (
-    <div className={styles.tokenRow}>
-      <div className={styles.tokenHoldings}>
-        <img src={props.icon.src} alt={`${props.label} token`} />
-
-        {props.balance ? (
-          <div className={styles.tokenBalance}>
-            <Text t="h4">{formatBalance(props.balance)} </Text>
-            <Text t="h4" color="lightest">
-              {props.label}
-            </Text>
-          </div>
-        ) : (
-          <Text t="h4" color="lightest">
-            Loading...
-          </Text>
-        )}
-      </div>
-
-      {props.holdings ? (
-        <div className={styles.holdingsChart}>
-          <HoldingsChart data={props.holdings} />
-        </div>
-      ) : (
-        <div className={styles.holdingsChart}>
-          <PlaceholderHoldingsChart />
-        </div>
-      )}
-    </div>
-  );
 };
 
 export const AssetBalanceCard: FC<Props> = (props) => {
