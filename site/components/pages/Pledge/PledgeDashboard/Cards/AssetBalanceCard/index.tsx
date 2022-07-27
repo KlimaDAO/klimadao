21942070beea39e5c4379332ffa672fcc1f483e9
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-07-27 13:36:13 +1000

    add placeholder asset holding chart

--- site/components/pages/Pledge/PledgeDashboard/Cards/AssetBalanceCard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/Cards/AssetBalanceCard/index.tsx
@@ -32,8 +32,44 @@ import {
   YAxis,
 } from "recharts";
 
+const NoHoldingsChart = () => {
+  const data = [
+    {
+      date: new Date(1634475600 * 1000),
+      tokenAmount: 33,
+    },
+    {
+      date: new Date(),
+      tokenAmount: 33,
+    },
+  ];
+
+  return (
+    <ResponsiveContainer>
+      <AreaChart data={data}>
+        <defs>
+          <linearGradient id="nullGradient" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="var(--font-03)" stopOpacity={0.33} />
+            <stop offset="100%" stopColor="var(--font-03)" stopOpacity={0.05} />
+          </linearGradient>
+        </defs>
+        <XAxis hide={true} dataKey="date" />
+        <YAxis hide={true} domain={[0, 100]} />
+        <Tooltip />
+        <Area
+          isAnimationActive={true}
+          type="linear"
+          dataKey="tokenAmount"
+          stroke="var(--font-03)"
+          fillOpacity={0.6}
+          fill="url(#nullGradient)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
+
 const HoldingsOverTimeChart = (props) => {
-  console.log(props.data);
   const data = [
     {
       timestamp: 1634475600,
@@ -57,7 +93,7 @@ const HoldingsOverTimeChart = (props) => {
     })),
   ];
 
-  console.log(data);
+  // calculate chart boundaries
   const tokenValues = props.data.map((tx) => Math.floor(tx.tokenAmount));
   const maxValue = last(orderBy(tokenValues)) * 1.01;
 
@@ -148,9 +184,13 @@ const TokenRow = (props) => {
         )}
       </div>
 
-      {props.holdings && (
+      {props.holdings ? (
         <div className={styles.holdingsChart}>
           <HoldingsOverTimeChart data={props.holdings} />
+        </div>
+      ) : (
+        <div className={styles.holdingsChart}>
+          <NoHoldingsChart />
         </div>
       )}
     </div>
