c362d12b7071058cedbdda6103e8c837875f1912
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-07-29 10:53:23 +1000

    add types to /pledge/[address]

--- site/pages/pledge/[address].tsx
+++ site/pages/pledge/[address].tsx
@@ -1,7 +1,9 @@
 import { GetStaticProps } from "next";
 import { ethers } from "ethers";
+import { ParsedUrlQuery } from "querystring";
 import { urls } from "@klimadao/lib/constants";
 import { getRetirementTotalsAndBalances } from "@klimadao/lib/utils";
+import { RetirementsTotalsAndBalances } from "@klimadao/lib/types/offset";
 
 import { loadTranslation } from "lib/i18n";
 import { getIsDomainInURL } from "lib/getIsDomainInURL";
@@ -12,59 +14,83 @@ import {
   DEFAULT_VALUES,
   queryHoldingsByAddress,
 } from "components/pages/Pledge/lib";
+import { Pledge } from "components/pages/Pledge/types";
+import { Holding } from "components/pages/Pledge/lib/subgraph";
+interface Params extends ParsedUrlQuery {
+  /** Either an 0x or a nameservice domain like atmosfearful.klima */
+  address: string;
+}
+interface PageProps {
+  canonicalUrl: string;
+  domain: string | null;
+  holdings: Holding[];
+  pageAddress: string;
+  pledge: Pledge;
+  retirements: RetirementsTotalsAndBalances;
+}
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  const translation = await loadTranslation(ctx.locale);
-  const { address } = ctx.params as { address: string };
-  let pledge;
-  let resolvedAddress;
-  const isDomainInURL = getIsDomainInURL(address);
-  const domain = isDomainInURL ? address : null;
-
+export const getStaticProps: GetStaticProps<PageProps, Params> = async (
+  ctx
+) => {
   try {
-    if (!isDomainInURL && !ethers.utils.isAddress(address))
-      throw new Error("Invalid address");
+    const translation = await loadTranslation(ctx.locale);
+    const { address } = ctx.params as { address: string };
+    let pledge;
+    let resolvedAddress;
+    const isDomainInURL = getIsDomainInURL(address);
+    const domain = isDomainInURL ? address : null;
 
-    if (isDomainInURL) {
-      resolvedAddress = await getAddressByDomain(address);
-    } else {
-      resolvedAddress = address.toLowerCase();
+    try {
+      if (!isDomainInURL && !ethers.utils.isAddress(address))
+        throw new Error("Invalid address");
+
+      if (isDomainInURL) {
+        resolvedAddress = await getAddressByDomain(address);
+      } else {
+        resolvedAddress = address.toLowerCase();
+      }
+    } catch {
+      return {
+        redirect: {
+          destination: "/pledge",
+          permanent: false,
+        },
+      };
     }
-  } catch {
+
+    const holdings = await queryHoldingsByAddress(resolvedAddress);
+    const retirements = await getRetirementTotalsAndBalances({
+      address: resolvedAddress,
+    });
+
+    try {
+      const data = await getPledgeByAddress(resolvedAddress);
+      if (!data) throw new Error("Not found");
+
+      pledge = data;
+    } catch (error) {
+      pledge = { ...DEFAULT_VALUES, ownerAddress: resolvedAddress };
+    }
+
     return {
-      redirect: {
-        destination: "/pledge",
-        permanent: false,
+      props: {
+        canonicalUrl: `${urls.pledges}/${address}`,
+        domain,
+        holdings,
+        pageAddress: resolvedAddress,
+        pledge,
+        retirements,
+        translation,
       },
+      revalidate: 180,
+    };
+  } catch (error) {
+    console.error("Failed to generate", error);
+    return {
+      notFound: true,
+      revalidate: 240,
     };
   }
-
-  const holdings = await queryHoldingsByAddress(resolvedAddress);
-  const retirements = await getRetirementTotalsAndBalances({
-    address: resolvedAddress,
-  });
-
-  try {
-    const data = await getPledgeByAddress(resolvedAddress);
-    if (!data) throw new Error("Not found");
-
-    pledge = data;
-  } catch (error) {
-    pledge = { ...DEFAULT_VALUES, ownerAddress: resolvedAddress };
-  }
-
-  return {
-    props: {
-      canonicalUrl: `${urls.pledges}/${address}`,
-      domain,
-      holdings,
-      pageAddress: resolvedAddress,
-      pledge,
-      retirements,
-      translation,
-    },
-    revalidate: 180,
-  };
 };
 
 export const getStaticPaths = async () => ({
