21003c0d3a5fee1484084c1e29af7c91b39967a1
ladytrekker <ladytrekker@protonmail.com>
2022-08-02 11:36:44 +0200

    Check for empty subgraph data on Single Retirement

--- site/components/pages/Retirements/SingleRetirement/RetirementDate/index.tsx
+++ site/components/pages/Retirements/SingleRetirement/RetirementDate/index.tsx
@@ -4,20 +4,28 @@ import { useRouter } from "next/router";
 import { TextGroup } from "../TextGroup";
 
 type Props = {
-  timestamp: string; // 10 digits
+  timestamp: string | null; // 10 digits
 };
 
 export const RetirementDate: FC<Props> = ({ timestamp }) => {
   const { locale } = useRouter();
-  const retirementDate = new Date(parseInt(timestamp) * 1000); //expects milliseconds
-  const formattedDate = new Intl.DateTimeFormat(locale, {
-    dateStyle: "full",
-  }).format(retirementDate);
+  const retirementDate = timestamp && new Date(parseInt(timestamp) * 1000); //expects milliseconds
+  const formattedDate =
+    retirementDate &&
+    new Intl.DateTimeFormat(locale, {
+      dateStyle: "full",
+    }).format(retirementDate);
 
   return (
     <TextGroup
       title={<Trans id="retirement.single.retirementDate.title">Date</Trans>}
-      text={formattedDate}
+      text={
+        formattedDate || (
+          <Trans id="retirement.single.timestamp.placeholder">
+            No retirement timestamp provided
+          </Trans>
+        )
+      }
     />
   );
 };
