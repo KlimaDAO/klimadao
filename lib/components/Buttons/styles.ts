4809ccac197815717459c4c02167a6f1e0a5dd7a
Joey <joeyr84@gmail.com>
2022-08-09 10:25:23 -0400

    combining button primary and button secondary (#562)
    * combining button primary and button secondary
    
    * remove export
    
    * button updates from PR feedback

--- lib/components/Buttons/styles.ts
+++ lib/components/Buttons/styles.ts
@@ -1,7 +1,7 @@
 import { css } from "@emotion/css";
 import { button } from "../../theme/typography";
 
-export const button_primary = css`
+const buttonBase = css`
   ${button};
   display: flex;
   padding: 0rem 2.4rem;
@@ -14,6 +14,10 @@ export const button_primary = css`
   cursor: pointer;
   border-radius: 0.4rem;
   transition: opacity 0.3s ease 0s;
+`;
+
+export const buttonPrimary = css`
+  ${buttonBase};
   background-color: var(--klima-green);
 
   &:active:not(:disabled) {
@@ -34,86 +38,64 @@ export const button_primary = css`
   &.icon {
     gap: 0.8rem;
   }
+
+  &.gray {
+    background-color: var(--surface-01);
+    color: var(--font-02);
+
+    &,
+    &:hover,
+    &:hover:not(:disabled),
+    &:visited {
+      color: var(--font-02); /* same in darkmode */
+    }
+  }
+
+  &.blueRounded {
+    background-color: var(--klima-blue);
+    color: var(--surface-01);
+    border-radius: 0.8rem;
+    &,
+    &:hover,
+    &:hover:not(:disabled),
+    &:visited {
+      color: var(--surface-01); /* same in darkmode */
+    }
+  }
 `;
-export const button_secondary = css`
-  ${button};
-  display: flex;
-  padding: 0rem 2.4rem;
-  align-items: center;
-  justify-content: center;
-  appearance: none;
-  text-decoration: none;
-  /* min-height to conform with Lighthouse min tap-target */
-  min-height: 4.8rem;
-  cursor: pointer;
+
+export const buttonSecondary = css`
+  ${buttonBase}
   border: 1px solid var(--klima-green);
   border-radius: 0.4rem;
-  transition: opacity 0.3s ease 0s;
   background-color: transparent;
-
-  &:active:not(:disabled) {
-    transform: scale(0.95);
-    opacity: 0.6;
-  }
   &,
   &:hover:not(:disabled),
   &:visited {
     color: var(--klima-green); /* same in darkmode */
   }
 
-  &:disabled {
-    border-color: var(--surface-03);
-    cursor: not-allowed;
+  &.gray {
+    border-color: var(--surface-01);
+    color: var(--surface-01);
+
+    &,
+    &:hover,
+    &:hover:not(:disabled),
+    &:visited {
+      color: var(--font-01); /* same in darkmode */
+    }
   }
 
-  &.icon {
-    gap: 0.8rem;
-  }
-`;
-
-export const button_primary_gray = css`
-  background-color: var(--surface-01);
-  color: var(--font-02);
-
-  &,
-  &:hover,
-  &:hover:not(:disabled),
-  &:visited {
-    color: var(--font-02); /* same in darkmode */
-  }
-`;
-
-export const button_primary_blue = css`
-  background-color: var(--klima-blue);
-  color: var(--surface-01);
-  border-radius: 0.8rem;
-  &,
-  &:hover,
-  &:hover:not(:disabled),
-  &:visited {
-    color: var(--surface-01); /* same in darkmode */
-  }
-`;
-export const button_secondary_gray = css`
-  border-color: var(--surface-01);
-  color: var(--surface-01);
-
-  &,
-  &:hover,
-  &:hover:not(:disabled),
-  &:visited {
-    color: var(--surface-01); /* same in darkmode */
-  }
-`;
-
-export const button_secondary_blue = css`
-  border-color: var(--klima-blue);
-  color: var(--klima-blue);
-  border-radius: 0.8rem;
-  &,
-  &:hover,
-  &:hover:not(:disabled),
-  &:visited {
-    color: var(--klima-blue); /* same in darkmode */
+  &.blueRounded {
+    border-color: var(--klima-blue);
+    color: var(--klima-blue);
+    border-radius: 0.8rem;
+    &,
+    &:hover,
+    &:hover:not(:disabled),
+    &:visited {
+      color: var(--klima-blue); /* same in darkmode */
+    }
   }
 `;
