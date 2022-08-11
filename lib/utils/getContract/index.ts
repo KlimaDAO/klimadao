346a47db475f17634f418cca4669604de6ad3453
ladytrekker <ladytrekker@protonmail.com>
2022-08-11 14:45:19 +0200

    Start matching addresses and their ABIs

--- lib/utils/getContract/index.ts
+++ lib/utils/getContract/index.ts
@@ -12,6 +12,9 @@ import KlimaStakingHelper from "../../abi/KlimaStakingHelper.json";
 import KlimaStakingv2 from "../../abi/KlimaStakingv2.json";
 import KlimaRetirementStorage from "../../abi/KlimaRetirementStorage.json";
 import SKlima from "../../abi/sKlima.json";
+// import OhmDai from "../../abi/OhmDai.json";
+import Depository from "../../abi/KlimaBondDepository_Regular.json";
+import KlimaProV2 from "../../abi/KlimaProV2.json";
 
 type Address = keyof typeof addresses["mainnet"];
 type ContractMap = {
@@ -37,13 +40,40 @@ const contractMap = {
   // Main Contracts
   sklimaMain: SKlima.abi,
 
-  // PairContracts
+  // Bonds PairContracts
   bctUsdcLp: PairContract.abi,
   klimaBctLp: PairContract.abi,
   klimaUboLp: PairContract.abi,
   klimaNboLp: PairContract.abi,
   klimaUsdcLp: PairContract.abi,
   klimaMco2Lp: PairContract.abi,
+
+  // Bonds Reserve
+  // klimaBctLp: OhmDai.abi,
+  // klimaUsdcLp: OhmDai.abi,
+  // bctUsdcLp: OhmDai.abi,
+  // klimaMco2Lp: OhmDai.abi,
+  klimaProV2: KlimaProV2.abi,
+
+  // Bonds Depository
+  bond_klimaBctLp: Depository.abi,
+  bond_klimaUsdcLp: Depository.abi,
+  bond_bctUsdcLp: Depository.abi,
+  bond_klimaMco2Lp: Depository.abi,
+  bond_mco2: Depository.abi,
+  bond_bct: Depository.abi,
+  bond_nbo: Depository.abi,
+  bond_ubo: Depository.abi,
+
+  // Bonds INVERSE
+  // bond_klimaBctLp: KlimaProV2.abi,
+  // bond_klimaUsdcLp: KlimaProV2.abi,
+  // bond_bctUsdcLp: KlimaProV2.abi,
+  // bond_klimaMco2Lp: KlimaProV2.abi,
+  // bond_mco2: KlimaProV2.abi,
+  // bond_bct: KlimaProV2.abi,
+  // bond_nbo: KlimaProV2.abi,
+  // bond_ubo: KlimaProV2.abi,
 
   // Others
   distributor: DistributorContractv4.abi,
