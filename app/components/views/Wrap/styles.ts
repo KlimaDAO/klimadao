8db7ec2ba6f4335097e78edbb0c1ba607be6eedb
goats_on_drugs <joeyr84@gmail.com>
2022-08-12 12:57:15 -0400

    prettier

--- app/components/views/Wrap/styles.ts
+++ app/components/views/Wrap/styles.ts
@@ -2,9 +2,8 @@ import { css } from "@emotion/css";
 import breakpoints from "@klimadao/lib/theme/breakpoints";
 
 export const wrapCard = css`
-grid-row: auto;
-${breakpoints.desktop} {
-
-  grid-row: 1 / span 3 !important;
-}
+  grid-row: auto;
+  ${breakpoints.desktop} {
+    grid-row: 1 / span 3 !important;
+  }
 `;
