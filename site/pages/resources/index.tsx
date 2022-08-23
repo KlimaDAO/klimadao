e8f0e11014f2daf584cc1095155330a04805a882
ladytrekker <ladytrekker@protonmail.com>
2022-08-23 13:53:30 +0200

    Add page „resources“

---
+++ site/pages/resources/index.tsx
@@ -0,0 +1,32 @@
+import { Resources } from "components/pages/Resources";
+import { fetchCMSContent } from "lib/fetchCMSContent";
+import { loadTranslation } from "lib/i18n";
+import { GetStaticPropsContext } from "next";
+
+export async function getStaticProps(ctx: GetStaticPropsContext) {
+  try {
+    const documents = await fetchCMSContent("allDocuments");
+
+    const translation = await loadTranslation(ctx.locale);
+    if (!documents) {
+      throw new Error("No documents found");
+    }
+
+    return {
+      props: {
+        documents,
+        translation,
+      },
+      revalidate: 240,
+    };
+  } catch (e) {
+    console.error("RESOURCES getStaticProps error: ", e);
+
+    return {
+      notFound: true,
+      revalidate: 240,
+    };
+  }
+}
+
+export default Resources;
