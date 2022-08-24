10f2aa74b08824ca141b3ee8729ca63ce9fee77a
ladytrekker <ladytrekker@protonmail.com>
2022-08-24 16:57:07 +0200

    Extract hook useElementWidth

---
+++ site/hooks/useElementWidth/index.ts
@@ -0,0 +1,42 @@
+import { useEffect, useCallback, useState, RefObject } from "react";
+
+export const useElementWidth = (elementRef: RefObject<HTMLElement>) => {
+  const [width, setWidth] = useState<number>(0);
+
+  const updateWidth = useCallback(() => {
+    if (elementRef && elementRef.current) {
+      const { width } = elementRef.current.getBoundingClientRect();
+      setWidth(width);
+    }
+  }, [elementRef]);
+
+  const firstUpdateWidth = useCallback(() => {
+    if (elementRef && elementRef.current) {
+      let { width } = elementRef.current.getBoundingClientRect();
+      width =
+        width -
+        parseInt(
+          window
+            .getComputedStyle(elementRef.current)
+            .getPropertyValue("border-left-width")
+        ) -
+        parseInt(
+          window
+            .getComputedStyle(elementRef.current)
+            .getPropertyValue("border-right-width")
+        );
+
+      setWidth(width);
+    }
+  }, [elementRef]);
+
+  useEffect(() => {
+    firstUpdateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => {
+      window.removeEventListener("resize", updateWidth);
+    };
+  }, [updateWidth]);
+
+  return [width];
+};
