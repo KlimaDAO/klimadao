4f341b225284189e5e623c94fc0e25c44cb8121d
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-08-25 18:28:04 +1000

    wip

--- site/components/pages/Pledge/lib/verifySignature.ts
+++ site/components/pages/Pledge/lib/verifySignature.ts
@@ -1,5 +1,7 @@
 import { ethers } from "ethers";
 import { editPledgeSignature } from ".";
+import { getJsonRpcProvider } from "@klimadao/lib/utils";
+import GnosisSafe from "@klimadao/lib/abi/GnosisSafe.json";
 
 interface Params {
   address: string;
@@ -7,13 +9,43 @@ interface Params {
   nonce: string;
 }
 
-export const verifySignature = (params: Params): void => {
-  const decodedAddress = ethers.utils.verifyMessage(
-    editPledgeSignature(params.nonce),
-    params.signature
+const EIP_1217_MAGIC_VALUE = "0x1626ba7e";
+
+const verifyGnosisSafeSignature = async (signature, address) => {
+  const messageHash = ethers.utils.hashMessage(signature);
+  const gnosisSafeContract = new ethers.Contract(
+    address,
+    GnosisSafe.abi,
+    getJsonRpcProvider().getSigner(address)
   );
 
-  if (decodedAddress.toLowerCase() !== params.address.toLowerCase()) {
+  const _signature = "0x";
+  const response = await gnosisSafeContract.isValidSignature(
+    messageHash,
+    _signature
+  );
+
+  console.log(response);
+
+  if (response !== EIP_1217_MAGIC_VALUE) {
     throw new Error("Invalid signature");
+  }
+};
+
+export const verifySignature = async (params: Params): Promise<void> => {
+  if (params.signature === "0x") {
+    await verifyGnosisSafeSignature(
+      editPledgeSignature(params.nonce),
+      params.address
+    );
+  } else {
+    const decodedAddress = ethers.utils.verifyMessage(
+      editPledgeSignature(params.nonce),
+      params.signature
+    );
+
+    if (decodedAddress.toLowerCase() !== params.address.toLowerCase()) {
+      throw new Error("Invalid signature");
+    }
   }
 };
