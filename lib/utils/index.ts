462d03a919c23a6ba647ab6fb806b57aa55f5aaf
Atmosfearful <atmosfearful@protonmail.com>
2022-09-08 15:55:37 -0700

    add usefocustrap to modal

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -55,4 +55,5 @@ export {
   getENSProfile,
 } from "./ens";
 export { useWeb3 } from "./useWeb3";
+export { useFocusTrap } from "./useFocusTrap";
 export { useTabListener } from "./useTabListener";
