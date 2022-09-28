7b562c803974450ec6558850295299f26965d267
goats_on_drugs <joeyr84@gmail.com>
2022-09-28 10:05:11 -0400

    working branch pr for multi wallet pledge

--- site/components/pages/Pledge/PledgeDashboard/index.tsx
+++ site/components/pages/Pledge/PledgeDashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { t } from "@lingui/macro";
 import { concatAddress, useWeb3 } from "@klimadao/lib/utils";
@@ -19,6 +19,7 @@ import { PledgeForm } from "../PledgeForm";
 import { PledgeLayout } from "../PledgeLayout";
 import { Holding, Pledge } from "../types";
 import * as styles from "./styles";
+import { ButtonPrimary, ButtonSecondary } from "@klimadao/lib/components";
 
 type Props = {
   canonicalUrl: string;
@@ -31,22 +32,42 @@ type Props = {
 
 export const PledgeDashboard: NextPage<Props> = (props) => {
   const { address, isConnected } = useWeb3();
-  const [showModal, setShowModal] = useState(false);
+  const [showFormModal, setShowFormModal] = useState(false);
+  const [isDeleteMode, setIsDeleteMode] = useState(false);
   const [pledge, setPledge] = useState<Pledge>(props.pledge);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [showAcceptModal, setShowAcceptModal] = useState(false);
 
-  const canEditPledge =
+  const isUnverifiedSecondaryWallet = props.pledge.wallets?.some(
+    (wallet) => wallet.address === address && wallet.verified === false
+  );
+  const isVerifiedSecondaryWallet = props.pledge.wallets?.some(
+    (wallet) => wallet.address === address && wallet.verified === true
+  );
+  const isPledgeOwner =
     address?.toLowerCase() === props.pageAddress && isConnected;
+  const canEditPledge =
+    (address?.toLowerCase() === props.pageAddress && isConnected) ||
+    isVerifiedSecondaryWallet;
+
+  useEffect(() => {
+    isUnverifiedSecondaryWallet && setShowAcceptModal(true);
+  }, [isUnverifiedSecondaryWallet]);
 
   const handleFormSubmit = async (data: Pledge) => {
     setPledge(data);
-    setShowModal(false);
+    setShowFormModal(false);
   };
 
   const pledgeOwnerTitle =
     pledge.name || props.domain || concatAddress(pledge.ownerAddress);
 
   return (
-    <PledgeLayout canEditPledge={canEditPledge} toggleEditModal={setShowModal}>
+    <PledgeLayout
+      canEditPledge={canEditPledge}
+      toggleEditModal={isPledgeOwner ? setShowFormModal : setShowRemoveModal}
+    >
       <PageHead
         title={t({
           id: "pledges.dashboard.head.title",
@@ -63,19 +84,81 @@ export const PledgeDashboard: NextPage<Props> = (props) => {
         })}
         canonicalUrl={props.canonicalUrl}
       />
+      {/* conditional props are used here because unmounting the modal will clear form state when `isDeleteMode` is changed */}
       <Modal
-        title={t({
-          id: "pledges.form.title",
-          message: "Your Pledge",
-        })}
-        showModal={showModal}
-        onToggleModal={() => setShowModal(!showModal)}
+        title={
+          !isDeleteMode
+            ? t({
+                id: "pledges.form.title",
+                message: "Your pledge",
+              })
+            : t({
+                id: "pledges.form.confirm_delete",
+                message: "confirm removal",
+              })
+        }
+        showModal={showFormModal}
+        onToggleModal={() =>
+          isDeleteMode
+            ? setIsDeleteMode(false)
+            : setShowFormModal(!showFormModal)
+        }
       >
         <PledgeForm
           pageAddress={props.pageAddress}
           pledge={pledge}
           onFormSubmit={handleFormSubmit}
+          setIsDeleteMode={setIsDeleteMode}
+          isDeleteMode={isDeleteMode}
         />
+      </Modal>
+      <Modal
+        title="Invitation to join"
+        showModal={showAcceptModal}
+        onToggleModal={() => setShowAcceptModal(false)}
+      >
+        Pledge Creator has invited you to add your wallet to this pledge. If you
+        accept, then your carbon retirements will count toward Pledge Creator's
+        pledge. You may remove your wallet from this pledge at any time.
+        <ButtonPrimary
+          label="Accept Invitation"
+          onClick={() => {
+            setShowConfirmModal(true);
+            setShowAcceptModal(false);
+          }}
+        />
+        <ButtonSecondary
+          label="Reject Invitation"
+          onClick={() => setShowAcceptModal(false)}
+        />
+        <ButtonSecondary
+          label="Remind me later"
+          onClick={() => setShowAcceptModal(false)}
+        />
+      </Modal>
+      <Modal
+        title="Are you sure?"
+        showModal={showConfirmModal}
+        onToggleModal={() => setShowConfirmModal(false)}
+      >
+        Adding your wallet to this pledge will remove your wallet from any
+        existing pledges under your wallet address. Those pledges will be
+        redirected to this new pledge.
+        <ButtonPrimary label="Confirm" />
+        <ButtonSecondary
+          label="Cancel"
+          onClick={() => {
+            setShowConfirmModal(false);
+            setShowAcceptModal(true);
+          }}
+        />
+      </Modal>
+      <Modal
+        title="Edit pledge"
+        showModal={showRemoveModal}
+        onToggleModal={() => setShowRemoveModal(false)}
+      >
+        verified in here
       </Modal>
 
       <div className={styles.contentContainer}>
