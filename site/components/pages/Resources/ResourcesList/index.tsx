e0b1525670c5d0ad5fb4205bb74e8af100f5d31b
ladytrekker <ladytrekker@protonmail.com>
2022-10-07 11:22:45 +0200

    Implement Filter checkboxes

--- site/components/pages/Resources/ResourcesList/index.tsx
+++ site/components/pages/Resources/ResourcesList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Trans, t } from "@lingui/macro";
 import { useForm, SubmitHandler } from "react-hook-form";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,9 +6,17 @@ import { Text, Section, ButtonPrimary } from "@klimadao/lib/components";
 
 import { Card } from "components/Card";
 import { PodcastCard } from "components/PodcastCard";
-import { InputField } from "components/Form";
+import { InputField, Checkbox } from "components/Form";
 
 import { fetchCMSContent } from "lib/fetchCMSContent";
+import {
+  mainTags,
+  subTags,
+  documentTypes,
+  TagSlug,
+  DocumentType,
+  SortQuery,
+} from "../lib/cmsDataMap";
 
 import * as styles from "./styles";
 import { Document } from "lib/queries";
@@ -19,6 +27,16 @@ export interface Props {
 
 type FormValues = {
   search: string;
+  tags: TagSlug[] | [];
+  types: DocumentType[] | [];
+  sortedBy: SortQuery | string;
+};
+
+const defaultValues: FormValues = {
+  search: "",
+  tags: [],
+  types: [],
+  sortedBy: "",
 };
 
 export const ResourcesList: FC<Props> = (props) => {
@@ -26,18 +44,38 @@ export const ResourcesList: FC<Props> = (props) => {
     props.documents
   );
 
-  const { register, handleSubmit } = useForm<FormValues>();
+  const { register, handleSubmit, watch, reset } = useForm<FormValues>({
+    defaultValues,
+  });
+
+  const selectedTags = watch("tags");
+  const selectedTypes = watch("types");
+  const selectedSortedBy = watch("sortedBy");
+
+  const onReset = (fields = defaultValues as Partial<FormValues>) => {
+    reset({ ...fields });
+  };
 
   const onSubmit: SubmitHandler<FormValues> = async (values: FormValues) => {
     // reset list to default documents on empty string
     if (!values.search) {
       setVisibleDocuments(props.documents);
+      // remove all field values
+      onReset();
       return;
     }
 
     try {
       const searchResult = await fetchCMSContent("searchByText", {
         searchQuery: values.search,
+      });
+
+      // remove all filters and sortBy as search results do not take filters into account and are sorted by relevance
+      // but keep current search term
+      onReset({
+        tags: [],
+        types: [],
+        sortedBy: "",
       });
 
       if (searchResult.length) {
@@ -50,70 +88,198 @@ export const ResourcesList: FC<Props> = (props) => {
     }
   };
 
+  const filterDocuments = async () => {
+    const types = selectedTypes.length ? selectedTypes : ["post", "podcast"];
+    const orderSelect = selectedSortedBy || "publishedAt desc";
+
+    if (selectedTags?.length) {
+      try {
+        const filteredDocuments = await fetchCMSContent(
+          "filterDocumentsByTags",
+          {
+            documentTypes: types,
+            referenceTags: selectedTags,
+            orderBy: orderSelect,
+          }
+        );
+        if (filteredDocuments.length) {
+          setVisibleDocuments(filteredDocuments);
+        } else {
+          setVisibleDocuments(null);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const filteredDocuments = await fetchCMSContent(
+          "filterDocumentsWithoutTags",
+          {
+            documentTypes: types,
+            orderBy: orderSelect,
+          }
+        );
+        if (filteredDocuments.length) {
+          setVisibleDocuments(filteredDocuments);
+        } else {
+          setVisibleDocuments(null);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (selectedTags?.length || selectedTypes?.length || selectedSortedBy) {
+      filterDocuments();
+    }
+  }, [selectedTags, selectedTypes, selectedSortedBy]);
+
   return (
     <Section variant="gray">
-      <div className={styles.listContainer}>
+      <div className={styles.resourcesListContainer}>
         <Text t="h2">
           <Trans id="resources.page.list.header">Explore All</Trans>
         </Text>
-        <form
-          className={styles.inputsContainer}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <InputField
-            id="search"
-            inputProps={{
-              placeholder: t({
-                id: "resources.form.input.search.placeholder",
-                message: "Search",
-              }),
-              type: "search",
-              autoComplete: "off",
-              className: styles.searchInput,
-              ...register("search", {
-                required: {
-                  value: false,
-                  message: "resources.form.input.search.error.required",
-                },
-              }),
-            }}
-            label={t({
-              id: "resources.form.input.search.label",
-              message: "Search",
-            })}
-            hideLabel
-          />
-          <ButtonPrimary
-            variant="icon"
-            type="submit"
-            className={styles.searchInputSubmit}
-            label={<SearchIcon fontSize="large" />}
-            onClick={handleSubmit(onSubmit)}
-          />
-        </form>
-        {!visibleDocuments && (
-          <>
-            <Text t="h4">
-              <Trans id="resources.page.list.no_search_results.title">
-                Sorry. We coudn't find any matching results.
-              </Trans>
-            </Text>
-            <Text>
-              <Trans id="resources.page.list.no_search_results.subline">
-                Double check your search for any typos or spelling errors - or
-                try a different search term.
-              </Trans>
-            </Text>
-          </>
-        )}
-        <div className={styles.list}>
-          {!!visibleDocuments?.length &&
-            visibleDocuments.map((doc) => {
-              if (doc.type === "post") {
-                return <Card key={doc.slug} post={doc} />;
-              }
-              return <PodcastCard podcast={doc} key={doc.slug} />;
-            })}
+        <div className={styles.inputsContainer}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.searchInputContainer}>
+              <InputField
+                id="search"
+                inputProps={{
+                  placeholder: t({
+                    id: "resources.form.input.search.placeholder",
+                    message: "Search",
+                  }),
+                  type: "search",
+                  autoComplete: "off",
+                  className: styles.searchInput,
+                  ...register("search"),
+                }}
+                label={t({
+                  id: "resources.form.input.search.label",
+                  message: "Search",
+                })}
+                hideLabel
+              />
+              <ButtonPrimary
+                variant="icon"
+                type="submit"
+                className={styles.searchInputSubmit}
+                label={<SearchIcon fontSize="large" />}
+                onClick={handleSubmit(onSubmit)}
+              />
+            </div>
+          </form>
+        </div>
+        <div className={styles.main}>
+          <div className={styles.filtersContainer}>
+            <div className={styles.filtersContainerInner}>
+              <div className={styles.filtersHeader}>
+                <Text t="h3">
+                  <Trans id="resources.form.categories.header">
+                    Categories
+                  </Trans>
+                </Text>
+                <Text t="caption">
+                  <Trans id="resources.form.categories.subheader">
+                    Select one or more
+                  </Trans>
+                </Text>
+              </div>
+              <div className={styles.filtersCheckboxGroup}>
+                {mainTags.map((tag) => (
+                  <Checkbox
+                    key={tag.id}
+                    label={tag.label}
+                    inputProps={{
+                      id: tag.id,
+                      value: tag.slug,
+                      ...register("tags"),
+                    }}
+                  />
+                ))}
+              </div>
+              <div>
+                <Text t="h4">
+                  <Trans id="resources.form.sub_topics.header">
+                    Sub-topics
+                  </Trans>
+                </Text>
+              </div>
+              <div className={styles.filtersCheckboxGroup}>
+                {subTags.map((tag) => (
+                  <Checkbox
+                    key={tag.id}
+                    label={tag.label}
+                    inputProps={{
+                      id: tag.id,
+                      value: tag.slug,
+                      ...register("tags"),
+                    }}
+                  />
+                ))}
+              </div>
+              <div>
+                <Text t="h4">
+                  <Trans id="resources.form.medium.header">Medium</Trans>
+                </Text>
+              </div>
+              <div className={styles.filtersCheckboxGroup}>
+                {documentTypes.map((type) => (
+                  <Checkbox
+                    key={type.type}
+                    label={type.label}
+                    inputProps={{
+                      id: type.type,
+                      value: type.type,
+                      ...register("types"),
+                    }}
+                  />
+                ))}
+              </div>
+              <Text
+                t="caption"
+                align="center"
+                role="button"
+                onClick={() => onReset()}
+                style={{ cursor: "pointer" }}
+              >
+                X{" "}
+                <Trans id="resources.form.filters.clear_all">
+                  Clear all filters
+                </Trans>
+              </Text>
+            </div>
+          </div>
+          <div className={styles.listContainer}>
+            {!visibleDocuments && (
+              <>
+                <Text t="h4">
+                  <Trans id="resources.page.list.no_search_results.title">
+                    Sorry. We coudn't find any matching results.
+                  </Trans>
+                </Text>
+                <Text>
+                  <Trans id="resources.page.list.no_search_results.subline">
+                    Double check your search for any typos or spelling errors -
+                    or try a different search term.
+                  </Trans>
+                </Text>
+              </>
+            )}
+            {!!visibleDocuments?.length && (
+              <div className={styles.list}>
+                {visibleDocuments.map((doc) => {
+                  if (doc.type === "post") {
+                    return <Card key={doc.slug} post={doc} />;
+                  }
+                  return <PodcastCard podcast={doc} key={doc.slug} />;
+                })}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </Section>
--- site/components/pages/Resources/ResourcesList/styles.ts
+++ site/components/pages/Resources/ResourcesList/styles.ts
@@ -1,20 +1,21 @@
 import { css } from "@emotion/css";
 
-export const listContainer = css`
+export const resourcesListContainer = css`
   grid-column: main;
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
 `;
 
-export const list = css`
-  padding-bottom: 5rem;
-  display: grid;
-  row-gap: 4.8rem;
+export const inputsContainer = css`
   grid-column: main;
+  display: flex;
+  flex-direction: row;
+  gap: 2.4rem;
+  justify-content: space-between;
 `;
 
-export const inputsContainer = css`
+export const searchInputContainer = css`
   display: flex;
 `;
 
@@ -45,4 +46,49 @@ export const searchInputSubmit = css`
   svg {
     fill: var(--font-03);
   }
+`;
+
+export const main = css`
+  display: grid;
+  grid-column: main;
+  gap: 2.4rem;
+  grid-template-columns:
+    [sidebar] 1fr
+    [list-view] 2fr;
+`;
+
+export const list = css`
+  padding-bottom: 5rem;
+  display: grid;
+  row-gap: 4.8rem;
+  grid-column: list-view;
+`;
+
+export const filtersContainer = css`
+  grid-column: sidebar;
+`;
+
+export const filtersContainerInner = css`
+  background-color: var(--surface-01);
+  border-radius: 1rem;
+  box-shadow: var(--shadow-07);
+  display: grid;
+  padding: 1.6rem;
+  padding-bottom: 2.4rem;
+  gap: 1.8rem;
+`;
+
+export const filtersHeader = css`
+  display: grid;
+  gap: 1.2rem;
+`;
+
+export const filtersCheckboxGroup = css`
+  display: grid;
+  gap: 1.2rem;
+  margin-bottom: 1.6rem; ;
+`;
+
+export const listContainer = css`
+  grid-column: list-view;
 `;
