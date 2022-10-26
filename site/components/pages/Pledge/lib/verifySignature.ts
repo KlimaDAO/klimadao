0803e97cf1944c994d5d07a3f678b4657515b37c
goats_on_drugs <joeyr84@gmail.com>
2022-10-26 17:19:42 -0400

    refactors

--- site/components/pages/Pledge/lib/verifySignature.ts
+++ site/components/pages/Pledge/lib/verifySignature.ts
@@ -9,16 +9,6 @@ interface Params {
   nonce: string;
 }
 
-<<<<<<< HEAD
-const verifyWalletSignature = (params: {
-  address: string;
-  signature: string;
-  message: string;
-}) => {
-  const decodedAddress = ethers.utils.verifyMessage(
-    params.message,
-    params.signature
-=======
 export const decodeSignerAddress = (params: {
   nonce: string;
   signature: string;
@@ -26,40 +16,29 @@ export const decodeSignerAddress = (params: {
   const decodedAddress = ethers.utils.verifyMessage(
     editPledgeSignature(params.nonce), // expected signed string and expected nonce e.g. "sign pledge 0123145181"
     params.signature // actual signature, which can be either 1. pledge owner, 2. secondary wallet 3. random wallet trying to hack us
->>>>>>> multi wallet pledge ready for final cleanup and code review
   );
 
   return decodedAddress;
 };
 
-export const verifySignature = (params: Params): void => {
+export const verifySignature = async (params: Params) => {
   const decodedAddress = decodeSignerAddress({
     nonce: params.nonce,
     signature: params.signature,
   });
-  if (decodedAddress?.toLowerCase() !== params.address.toLowerCase()) {
-    throw new Error("Invalid signature");
-  }
-};
-
-<<<<<<< HEAD
-export const verifySignature = async (params: Params): Promise<void> => {
-  const message = editPledgeMessage(params.nonce);
 
   // Gnosis
   if (params.signature === "0x") {
     await verifyGnosisSignature({
       address: params.address,
-      message,
-    });
-  } else {
-    verifyWalletSignature({
-      address: params.address,
-      signature: params.signature,
-      message,
+      message: editPledgeMessage(params.nonce),
     });
   }
-=======
+  if (decodedAddress?.toLowerCase() !== params.address.toLowerCase()) {
+    throw new Error("Invalid signature");
+  }
+};
+
 // return a boolean
 export const verifySignedMessage = (params: {
   expectedMessage: string;
@@ -70,5 +49,4 @@ export const verifySignedMessage = (params: {
     .verifyMessage(params.expectedMessage, params.signature)
     ?.toLowerCase();
   return decodedAddress === params.expectedAddress.toString().toLowerCase();
->>>>>>> multi wallet pledge ready for final cleanup and code review
 };
