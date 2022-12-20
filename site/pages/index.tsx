9204bead1b7cc7071e79c892c425bf740caf9598
goats <joeyr84@gmail.com>
2022-12-20 10:33:55 -0500

    strings

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,6 +1,6 @@
 import {
   fetchBlockRate,
-  getInfuraUrlPolygon,
+  getInfuraUrl,
   getStakingRewards,
   getTreasuryBalance,
 } from "@klimadao/lib/utils";
@@ -10,7 +10,10 @@ import { loadTranslation } from "lib/i18n";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps<Props> = async (ctx) => {
-  const infuraURL = getInfuraUrlPolygon();
+  const infuraURL = getInfuraUrl({
+    chain: "polygon",
+    infuraId: process.env.INFURA_ID!,
+  });
   const treasuryBalance = await getTreasuryBalance(infuraURL);
   const latestPost = await fetchCMSContent("latestPost");
   const translation = await loadTranslation(ctx.locale);
