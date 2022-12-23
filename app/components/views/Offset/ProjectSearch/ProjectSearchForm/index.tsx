b5fd1c6e12b6cfe20f548b3421267935be03fb69
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2022-12-23 23:06:40 +1100

    show filter options based on selected token

--- app/components/views/Offset/ProjectSearch/ProjectSearchForm/index.tsx
+++ app/components/views/Offset/ProjectSearch/ProjectSearchForm/index.tsx
@@ -11,7 +11,7 @@ import {
   queryCarbonProjectDetails,
 } from "../../SelectiveRetirement/queryProjectDetails";
 import { ProjectSearchFilter } from "../ProjectSearchFilter";
-import * as filterOptions from "../filterOptions";
+import { filterOptions } from "../filterOptions";
 import { ProjectSearchStep } from "../";
 
 type Props = {
@@ -84,7 +84,7 @@ export const ProjectSearchForm: FC<Props> = (props) => {
             id: "offset.selectiveRetirement.filters.type",
             message: "type",
           })}
-          options={filterOptions.types}
+          options={filterOptions[props.selectedRetirementToken].types}
           isOpen={currentFilter === "type"}
           onClick={setCurrentFilter}
           onChange={setType}
@@ -94,7 +94,7 @@ export const ProjectSearchForm: FC<Props> = (props) => {
             id: "offset.selectiveRetirement.filters.region",
             message: "region",
           })}
-          options={filterOptions.regions}
+          options={filterOptions[props.selectedRetirementToken].regions}
           isOpen={currentFilter === "region"}
           onClick={setCurrentFilter}
           onChange={setRegion}
@@ -104,7 +104,7 @@ export const ProjectSearchForm: FC<Props> = (props) => {
             id: "offset.selectiveRetirement.filters.vintage",
             message: "vintage",
           })}
-          options={filterOptions.vintages}
+          options={filterOptions[props.selectedRetirementToken].vintages}
           isOpen={currentFilter === "vintage"}
           onClick={setCurrentFilter}
           onChange={setVintage}
