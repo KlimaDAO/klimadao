30ca83d8e0d7de282c527c5fb4d9781f84e37e05
Atmosfearful <atmosfearful@protonmail.com>
2022-12-29 15:56:04 -0800

    revert host back to "matic"

--- lib/utils/useWeb3Modal/index.tsx
+++ lib/utils/useWeb3Modal/index.tsx
@@ -99,7 +99,7 @@ export const useWeb3Modal = (): Web3ModalState => {
         const torus = new Torus();
         await torus.init({
           network: {
-            host: urls.polygonMainnetRpc,
+            host: "matic",
             chainId: 137,
             networkName: "Polygon",
           },
