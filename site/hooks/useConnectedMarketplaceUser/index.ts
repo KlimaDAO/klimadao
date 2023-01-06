fb3d6909d4c9c357ca1cec2aeca390954b7e075c
ladytrekker <ladytrekker@protonmail.com>
2023-01-06 16:08:13 +0100

    Fix compare address in lowercase

--- site/hooks/useConnectedMarketplaceUser/index.ts
+++ site/hooks/useConnectedMarketplaceUser/index.ts
@@ -17,7 +17,9 @@ export const useConnectedMarketplaceUser = (
   const hasUserInURL = !!query?.user;
   const connectedUser = isConnected && !!address;
   const isConnectedUser =
-    connectedUser && hasUserInURL && address === userAddress;
+    connectedUser &&
+    hasUserInURL &&
+    address.toLowerCase() === userAddress.toLowerCase();
   const isUnconnectedUser = hasUserInURL && !isConnectedUser;
 
   return {
