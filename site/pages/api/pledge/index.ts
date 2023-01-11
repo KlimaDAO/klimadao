7d3be3dd0040fbb5ce41afe9e214e996b9172784
goats <joeyr84@gmail.com>
2023-01-11 09:55:00 -0500

    validation

--- site/pages/api/pledge/index.ts
+++ site/pages/api/pledge/index.ts
@@ -1,10 +1,39 @@
 import { findOrCreatePledge } from "components/pages/Pledge/lib/firebase";
 import { NextApiRequest, NextApiResponse } from "next/types";
+import { array, boolean, number, object, string } from "yup";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const pledgeSchema = object({
+    id: string().nullable(),
+    ownerAddress: string().required(),
+    nonce: string().required(),
+    name: string().required(),
+    profileImageUrl: string().url(),
+    profileWebsiteUrl: string().url(),
+    description: string().required().max(500),
+    wallets: array().of(
+      object({
+        address: string().required(),
+        status: string().required(),
+        saved: boolean().required(),
+      })
+    ),
+    methodology: string().required().max(1000),
+    footprint: number().required().min(0),
+    categories: array()
+      .of(
+        object({
+          name: string().required().max(32),
+          quantity: number().required().min(0),
+        })
+      )
+      .required()
+      .min(1),
+  });
+
   switch (req.method) {
     case "PUT":
       try {
@@ -13,10 +42,22 @@ export default async function handler(
         if (!signature) {
           return res.status(400).json({ message: "Bad request" });
         }
-
+        await pledgeSchema.validate(req.body.pledge);
         const pledge = await findOrCreatePledge({
           pageAddress: req.body.pageAddress,
-          pledge: req.body.pledge,
+          pledge: {
+            id: req.body.pledge.id,
+            ownerAddress: req.body.pledge.ownerAddress,
+            nonce: req.body.pledge.nonce,
+            name: req.body.pledge.name,
+            profileImageUrl: req.body.pledge.profileImageUrl,
+            profileWebsiteUrl: req.body.pledge.profileWebsiteUrl,
+            description: req.body.pledge.description,
+            wallets: req.body.pledge.wallets,
+            methodology: req.body.pledge.methodology,
+            footprint: req.body.pledge.footprint,
+            categories: req.body.pledge.categories,
+          },
           signature,
           secondaryWalletAddress: req.body.secondaryWalletAddress,
           action: req.body.action,
