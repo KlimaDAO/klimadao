a20a326f04a2ab1ef09c60bc87086acdb594027a
Atmosfearful <atmosfearful@protonmail.com>
2023-01-12 13:04:43 -0800

    add mumbai to getInfuraUrl

--- lib/utils/getInfuraUrl/index.ts
+++ lib/utils/getInfuraUrl/index.ts
@@ -4,10 +4,14 @@ import { urls } from "../../constants";
  * In some cases, like for stable backend APIs or for client-side ENS requests, we need an infura URL
  * This util takes generates the correct eth or polygon url for the given id */
 export const getInfuraUrl = (params: {
-  chain?: "polygon" | "eth";
+  chain?: "polygon" | "eth" | "mumbai";
   infuraId: string;
 }): string => {
   const { chain = "polygon" } = params; // fallback to polygon as default
-  const baseUrl = chain === "eth" ? urls.infuraEthRpc : urls.infuraPolygonRpc;
+  const baseUrl = {
+    eth: urls.infuraEthRpc,
+    polygon: urls.infuraPolygonRpc,
+    mumbai: urls.infuraMumbaiRpc,
+  }[chain];
   return `${baseUrl}/${params.infuraId}`;
 };
