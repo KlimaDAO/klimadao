53504ec0543076b947cc3ae62d1a05cc3cc179c2
Atmosfearful <atmosfearful@protonmail.com>
2023-01-12 13:04:34 -0800

    remove trailing slash

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -110,7 +110,7 @@ export const urls = {
   snapshot: "https://snapshot.org/#/klimadao.eth",
   polygonMainnetRpc: "https://polygon-rpc.com",
   polygonTestnetRpc: "https://rpc-mumbai.matic.today",
-  defaultEthRpc: "https://cloudflare-eth.com/",
+  defaultEthRpc: "https://cloudflare-eth.com",
   infuraPolygonRpc: "https://polygon-mainnet.infura.io/v3",
   infuraEthRpc: "https://mainnet.infura.io/v3",
   infuraMumbaiRpc: "https://polygon-mumbai.infura.io/v3",
