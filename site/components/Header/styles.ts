19261d6a5de854d564247603b0f2cb332c0971e8
goats <joeyr84@gmail.com>
2023-01-19 16:09:20 -0500

    quick fix for header height bug

--- site/components/Header/styles.ts
+++ site/components/Header/styles.ts
@@ -14,6 +14,7 @@ export const headerDesktop = css`
   z-index: 10; /* so the drop-shadow is visible over next section, 10 to make opened Translation menu flow over main content */
 
   ${breakpoints.desktop} {
+    height: 100%;
     display: grid;
   }
 `;
