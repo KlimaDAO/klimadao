87a4416b4201ae72f03b936ddc8b5827fea6412c
Joey <joeyr84@gmail.com>
2023-01-19 13:46:34 -0500

    dont hide dropdown on nav click (#903)

--- site/components/Navigation/DropdownItemDesktop.tsx
+++ site/components/Navigation/DropdownItemDesktop.tsx
@@ -1,6 +1,6 @@
 import { cx } from "@emotion/css";
 import Tippy from "@tippyjs/react";
-import { FC } from "react";
+import { FC, useRef } from "react";
 import * as styles from "./styles";
 
 interface Props {
@@ -10,6 +10,8 @@ interface Props {
 }
 
 export const DropdownItemDesktop: FC<Props> = (props) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const onClickHide = () => buttonRef.current?.blur(); // Tippy will keep the button focused (if clicked) after dropdown is hidden unless we blur it
   return (
     <div className={cx(styles.navMain_DesktopItemContainer)}>
       <Tippy
@@ -17,12 +19,15 @@ export const DropdownItemDesktop: FC<Props> = (props) => {
           <div className={styles.navMain_DesktopSubMenu}>{props.children}</div>
         }
         interactive={true}
+        hideOnClick={false}
+        onHide={onClickHide}
       >
         <button
           className={cx(styles.navMain_DesktopLink, {
             dropdown: true,
           })}
           data-active={props.active?.toString()}
+          ref={buttonRef}
         >
           {props.name}
         </button>
