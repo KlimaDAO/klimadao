46343d701eff4291356240596c95f09b807c6d5c
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2023-01-24 09:58:40 +1100

    Sanity v3 (#854)
    * upgrade sanity packages
    
    * update lock files
    
    * init sanity js config
    
    * update schema imports
    
    * update sanity vision
    
    * remove package-lock.json
    
    * update to latest sanity versions
    
    * configuration typo
    
    * point tsconfig to new types directory
    
    * use correct filed type
    
    * remove old config file
    
    * upgrade to latest sanity version
    
    * add compiled dev sanity to gitignore
    
    * latest versions and fix broken elements
    
    * upgrade configs
    
    Co-authored-by: Atmosfearful <atmosfearful@protonmail.com>

---
+++ cms/sanity.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+
+import schemas from "./schemas/schema";
+
+export default defineConfig({
+  title: "KlimaDAO",
+  projectId: "dk34t4vc",
+  dataset: "production",
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: schemas,
+  },
+  tools: (prev) => {
+    if (process.env.NODE_ENV === "development") {
+      return prev;
+    }
+
+    return prev.filter((tool) => tool.name !== "vision");
+  },
+});
