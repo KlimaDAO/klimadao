90d873df273a4a4d5def471f9c2305d06f70dfd5
Joey <joeyr84@gmail.com>
2023-02-01 11:48:53 -0500

    pledge enhancements (#874)
    * validation
    
    * update putPledge and schema

--- site/pages/api/pledge/index.ts
+++ site/pages/api/pledge/index.ts
@@ -1,10 +1,41 @@
 import { findOrCreatePledge } from "components/pages/Pledge/lib/firebase";
 import { NextApiRequest, NextApiResponse } from "next/types";
+import { array, boolean, number, object, string } from "yup";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const pledgeSchema = object({
+    id: string().nullable(),
+    ownerAddress: string().required(),
+    nonce: string().required(),
+    name: string().required(),
+    profileImageUrl: string().url(),
+    profileWebsiteUrl: string().url(),
+    description: string().required().max(500),
+    wallets: array().of(
+      object({
+        address: string().required(),
+        status: string().required(),
+        saved: boolean().required(),
+      })
+    ),
+    methodology: string().required().max(1000),
+    footprint: number().required().min(0),
+    categories: array()
+      .of(
+        object({
+          name: string().required().max(32),
+          quantity: number().required().min(0),
+        })
+      )
+      .required()
+      .min(1),
+    createdAt: number(),
+    updatedAt: number(),
+  }).noUnknown();
+
   switch (req.method) {
     case "PUT":
       try {
@@ -13,7 +44,7 @@ export default async function handler(
         if (!signature) {
           return res.status(400).json({ message: "Bad request" });
         }
-
+        await pledgeSchema.validate(req.body.pledge);
         const pledge = await findOrCreatePledge({
           pageAddress: req.body.pageAddress,
           pledge: req.body.pledge,
