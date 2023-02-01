ae539a2df9cb5b2380be2213790ca517542444e2
Atmosfearful <atmosfearful@protonmail.com>
2023-01-31 22:43:54 -0800

    Update footer links

--- carbonmark/components/shared/Footer/index.tsx
+++ carbonmark/components/shared/Footer/index.tsx
@@ -1,22 +1,9 @@
 import { cx } from "@emotion/css";
+import { Text } from "@klimadao/lib/components";
 import { Trans } from "@lingui/macro";
-import { createLinkWithLocaleQuery } from "lib/i18n";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { FC } from "react";
 
-import {
-  Anchor as A,
-  DiscordIcon,
-  GithubIcon,
-  LinkedInIcon,
-  RedditIcon,
-  RSSIcon,
-  TelegramIcon,
-  TwitterIcon,
-  YoutubeIcon,
-} from "@klimadao/lib/components";
-import { urls } from "@klimadao/lib/constants";
 import * as styles from "./styles";
 
 interface Props {
@@ -25,62 +12,31 @@ interface Props {
 }
 
 export const Footer: FC<Props> = (props) => {
-  const { locale } = useRouter();
   return (
     <footer className={cx(styles.footer(props.transparent), props.className)}>
       <div className={cx(styles.footer_content, "footer_content")}>
         <nav className={cx(styles.footer_nav, "footer_nav")}>
           <Link href="/">
-            <Trans id="shared.home">Home</Trans>
+            <Trans>Home</Trans>
           </Link>
-          <Link href="/buy">
-            <Trans id="shared.buy">Buy</Trans>
+          <Link href="/projects">
+            <Trans>Marketplace</Trans>
           </Link>
-          <a href={createLinkWithLocaleQuery(urls.stake, locale)}>
-            <Trans id="shared.stake">Stake</Trans>
-          </a>
-          <a href={createLinkWithLocaleQuery(urls.app, locale)}>
-            <Trans id="shared.bond">App</Trans>
-          </a>
-          <A href={urls.officialDocs}>
-            <Trans id="shared.docs">Docs</Trans>
-          </A>
-          <Link href="/blog">
-            <Trans id="shared.blog">Blog</Trans>
+          <Link href="/users/login">
+            <Trans>Profile</Trans>
           </Link>
-          <Link href="/contact">
-            <Trans id="shared.contact">Contact</Trans>
+          <Link href="/portfolio">
+            <Trans>Portfolio</Trans>
           </Link>
-          <Link href="/disclaimer">
-            <Trans id="shared.disclaimer">Disclaimer</Trans>
+          <Link href="/resources">
+            <Trans>Resources</Trans>
           </Link>
         </nav>
 
         <nav className={styles.footer_icons}>
-          <A href={urls.twitter}>
-            <TwitterIcon />
-          </A>
-          <A href={urls.youtube}>
-            <YoutubeIcon />
-          </A>
-          <A href={urls.discordInvite}>
-            <DiscordIcon />
-          </A>
-          <A href={urls.reddit}>
-            <RedditIcon />
-          </A>
-          <A href={urls.github}>
-            <GithubIcon />
-          </A>
-          <A href={urls.linkedIn}>
-            <LinkedInIcon />
-          </A>
-          <A href={urls.telegram}>
-            <TelegramIcon />
-          </A>
-          <A href={urls.podcast}>
-            <RSSIcon />
-          </A>
+          <Text t="caption">
+            Built with 🌳 by <a href="https://klimadao.finance">KlimaDAO</a>
+          </Text>
         </nav>
       </div>
     </footer>
