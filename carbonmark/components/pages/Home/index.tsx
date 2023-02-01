a4f9028a11b140a4cb484a2148d9fda1d284a029
Atmosfearful <atmosfearful@protonmail.com>
2023-01-31 22:45:38 -0800

    add Home layout example

--- carbonmark/components/pages/Home/index.tsx
+++ carbonmark/components/pages/Home/index.tsx
@@ -1,6 +1,10 @@
-import { Section, Text } from "@klimadao/lib/components";
-import { Trans } from "@lingui/macro";
+import { GridContainer, Section, Text } from "@klimadao/lib/components";
+import { t } from "@lingui/macro";
+import { Footer } from "components/shared/Footer";
+import { Navigation } from "components/shared/Navigation";
+import { PageHead } from "components/shared/PageHead";
 import { NextPage } from "next";
+import * as styles from "./styles";
 
 export interface Props {
   deleteme?: () => void;
@@ -8,12 +12,35 @@ export interface Props {
 
 export const Home: NextPage<Props> = () => {
   return (
-    <>
-      <Section variant="gray">
-        <Text>
-          <Trans>A new era for kleema dow</Trans>
-        </Text>
+    <GridContainer>
+      <PageHead
+        title={t`CarbonMark.com`}
+        mediaTitle={t`CarbonMark | Universal Carbon Market`}
+        metaDescription={t`Open-source and open-access carbon toolkit. Powered by KlimaDAO`}
+      />
+
+      <Navigation activePage="Home" />
+      <Section variant="gray" className={styles.hero}>
+        <div className="stack">
+          <Text t="h1">CarbonMark Cuban</Text>
+          <Text>Our Hero</Text>
+        </div>
       </Section>
-    </>
+      <Section variant="white" className={styles.section1}>
+        <div className="stack">
+          <Text t="h2">This is how I like to do layouts.</Text>
+          <Text>GridContainer -&gt; Section -&gt; Stack</Text>
+        </div>
+      </Section>
+      <Section variant="gray" className={styles.section1}>
+        <div className="stack">
+          <Text t="h3">Very easy - Not too many divs or classnames</Text>
+          <Text t="h1" as="p">
+            👍
+          </Text>
+        </div>
+      </Section>
+      <Footer />
+    </GridContainer>
   );
 };
---
+++ carbonmark/components/pages/Home/styles.ts
@@ -0,0 +1,21 @@
+import { css } from "@emotion/css";
+
+export const hero = css`
+  display: grid;
+  grid-column: full;
+  grid-template-columns: inherit;
+
+  .stack {
+    grid-column: main;
+  }
+`;
+
+export const section1 = css`
+  display: grid;
+  grid-column: full;
+  grid-template-columns: inherit;
+
+  .stack {
+    grid-column: main;
+  }
+`;
