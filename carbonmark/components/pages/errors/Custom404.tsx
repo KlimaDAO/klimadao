79915a86e78be2f9644b15214cac73b811d51e36
Atmosfearful <atmosfearful@protonmail.com>
2023-01-31 22:44:19 -0800

    add 404 and 500 pages

--- carbonmark/components/pages/errors/Custom404.tsx
+++ carbonmark/components/pages/errors/Custom404.tsx
@@ -1,11 +1,34 @@
-import { Text } from "@klimadao/lib/components";
+import { GridContainer, Section, Text } from "@klimadao/lib/components";
 
+import { PageHead } from "components/shared/PageHead";
+
+import { t, Trans } from "@lingui/macro";
+import { Footer } from "components/shared/Footer";
+import { Navigation } from "components/shared/Navigation";
 import * as styles from "./styles";
 
 export const Custom404 = () => {
   return (
-    <div className={styles.errorPageWrapper}>
-      <Text>404</Text>
-    </div>
+    <GridContainer>
+      <PageHead
+        title={t`404 - Page Not Found`}
+        mediaTitle={t`404 - Page Not Found`}
+        metaDescription={t`404 - Page Not Found`}
+      />
+      <div className={styles.errorPageWrapper}>
+        <Navigation activePage="Home" />
+        <Section variant="gray">
+          <div className={styles.textGroup}>
+            <Text t="h2" as="h2" align="center">
+              <Trans>404 - Page Not Found</Trans>
+            </Text>
+            <Text align="center">
+              <Trans>Sorry, looks like we sent you the wrong way.</Trans>
+            </Text>
+          </div>
+        </Section>
+        <Footer />
+      </div>
+    </GridContainer>
   );
 };
--- carbonmark/components/pages/errors/Custom500.tsx
+++ carbonmark/components/pages/errors/Custom500.tsx
@@ -1,11 +1,34 @@
-import { Text } from "@klimadao/lib/components";
+import { GridContainer, Section, Text } from "@klimadao/lib/components";
 
+import { PageHead } from "components/shared/PageHead";
+
+import { t, Trans } from "@lingui/macro";
+import { Footer } from "components/shared/Footer";
+import { Navigation } from "components/shared/Navigation";
 import * as styles from "./styles";
 
 export const Custom500 = () => {
   return (
-    <div className={styles.errorPageWrapper}>
-      <Text>500</Text>
-    </div>
+    <GridContainer>
+      <PageHead
+        title={t`500 - Server Error`}
+        mediaTitle={t`500 - Server Error`}
+        metaDescription={t`500 - Server Error`}
+      />
+      <div className={styles.errorPageWrapper}>
+        <Navigation activePage="Home" />
+        <Section variant="gray">
+          <div className={styles.textGroup}>
+            <Text t="h2" as="h2" align="center">
+              <Trans>500 - Server error occurred</Trans>
+            </Text>
+            <Text align="center">
+              <Trans>Sorry, something went wrong.</Trans>
+            </Text>
+          </div>
+        </Section>
+        <Footer />
+      </div>
+    </GridContainer>
   );
 };
