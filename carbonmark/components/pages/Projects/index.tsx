1b5a52e70b5c29d283446dfd3fc6a9a4e8d9d867
ladytrekker <95881624+ladytrekker@users.noreply.github.com>
2023-02-02 11:21:23 +0100

    Render price of projects (#92)

--- carbonmark/components/pages/Projects/index.tsx
+++ carbonmark/components/pages/Projects/index.tsx
@@ -7,17 +7,19 @@ import { ProjectImage } from "components/ProjectImage";
 import { PageHead } from "components/shared/PageHead";
 import { Vintage } from "components/Vintage";
 import { createProjectLink } from "lib/createUrls";
+import { formatBigToPrice } from "lib/formatNumbers";
 import { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import * as styles from "./styles";
-
 type Props = {
   projects: Project[];
 };
 
 export const Projects: NextPage<Props> = (props) => {
-  const hasProjects = !!props.projects.length;
+  const { locale } = useRouter();
 
+  const hasProjects = !!props.projects.length;
   const sortedProjects =
     hasProjects &&
     props.projects.sort((a, b) => Number(b.updatedAt) - Number(a.updatedAt));
@@ -47,6 +49,9 @@ export const Projects: NextPage<Props> = (props) => {
                   </div>
                   <div className={styles.cardContent}>
                     <Text t="h4">
+                      {formatBigToPrice(project.price, locale)}
+                    </Text>
+                    <Text t="h5">
                       {project.name || "! MISSING PROJECT NAME !"}
                     </Text>
                     <Text t="caption">{project.methodology}</Text>
