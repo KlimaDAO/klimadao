4e6995d1c227f56fefef9b9d6b1e6f9a994803c0
Atmosfearful <88635679+Atmosfearful@users.noreply.github.com>
2023-02-06 01:56:53 -0800

    add gtag (#109)

--- carbonmark/pages/_app.tsx
+++ carbonmark/pages/_app.tsx
@@ -7,6 +7,7 @@ import { useTabListener } from "@klimadao/lib/utils";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { useEffect, useRef } from "react";
 
 const loadFallbackOnServer = async () => {
@@ -61,6 +62,19 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           <Component {...pageProps} />
         </I18nProvider>
       </Web3ContextProvider>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PN73CG3');
+            `,
+        }}
+      />
     </>
   );
 }
