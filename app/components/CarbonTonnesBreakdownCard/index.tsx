0deced98716451696a0a3b8007c9eeb50cbd8d0b
Atmosfearful <atmosfearful@protonmail.com>
2023-02-16 10:54:56 -0800

    enhance carbontonnesbreakdown card

--- app/components/CarbonTonnesBreakdownCard/index.tsx
+++ app/components/CarbonTonnesBreakdownCard/index.tsx
@@ -6,18 +6,22 @@ import { FC } from "react";
 import { Text } from "@klimadao/lib/components";
 
 import BCT from "public/icons/BCT.png";
+import C3T from "public/icons/C3T.png";
 import MCO2 from "public/icons/MCO2.png";
 import NBO from "public/icons/NBO.png";
 import NCT from "public/icons/NCT.png";
+import TCO2 from "public/icons/TCO2.png";
 import UBO from "public/icons/UBO.png";
 
 import { RetirementToken, retirementTokens } from "@klimadao/lib/constants";
+import { ProjectTokenBalance } from "@klimadao/lib/types/offset";
 import { StaticImageData } from "next/image";
 import { useSelector } from "react-redux";
-import { selectCarbonRetired } from "state/selectors";
+import { selectCarbonRetired, selectProjectTokens } from "state/selectors";
 import * as styles from "./styles";
 
-type IconMap = { [key in RetirementToken]: StaticImageData };
+type Token = RetirementToken | "tco2" | "c3t";
+type IconMap = { [key in Token]: StaticImageData };
 
 const iconMap: IconMap = {
   ubo: UBO,
@@ -25,10 +29,22 @@ const iconMap: IconMap = {
   mco2: MCO2,
   nct: NCT,
   bct: BCT,
+  tco2: TCO2,
+  c3t: C3T,
 };
 
 export const CarbonTonnesBreakdownCard: FC = () => {
   const carbonRetired = useSelector(selectCarbonRetired);
+  const projectTokens = useSelector(selectProjectTokens);
+  console.log("got project tokens", projectTokens);
+  const retiredProjectTokens = Object.keys(projectTokens).reduce<
+    ProjectTokenBalance[]
+  >((arr, tkn) => {
+    const asset = projectTokens[tkn];
+    if (Number(asset.retired) > 0) return [...arr, asset];
+    return arr;
+  }, []);
+
   const retiredTokens = retirementTokens.filter(
     (tkn) => carbonRetired && Number(carbonRetired[tkn]) > 0
   );
@@ -39,7 +55,7 @@ export const CarbonTonnesBreakdownCard: FC = () => {
       <div className="header">
         <Text t="h4" className="title">
           <CloudOutlined />
-          <Trans id="offset.breakdown">Breakdown</Trans>
+          <Trans>Retirements</Trans>
         </Text>
       </div>
       <div className="cardContent">
@@ -61,11 +77,25 @@ export const CarbonTonnesBreakdownCard: FC = () => {
               <div className="image">
                 <Image src={iconMap[tkn]} width={48} height={48} alt="" />
               </div>
-              <div className="content">
-                <Text className="value">{carbonRetired?.[tkn] || 0}</Text>
-                <Text className="label" color="lightest">
-                  {tkn.toUpperCase()}
-                </Text>
+              <div className="label">
+                <Text>{carbonRetired?.[tkn] || 0}</Text>
+                <Text color="lightest">{tkn.toUpperCase()}</Text>
+              </div>
+            </div>
+          ))}
+          {retiredProjectTokens.map((tkn, index) => (
+            <div className={styles.row} key={`${tkn.symbol}-${index}`}>
+              <div className="image">
+                <Image
+                  src={iconMap[tkn.symbol.startsWith("TCO2") ? "tco2" : "c3t"]}
+                  width={48}
+                  height={48}
+                  alt=""
+                />
+              </div>
+              <div className="label">
+                <Text>{tkn.retired}</Text>
+                <Text color="lightest">{tkn.symbol}</Text>
               </div>
             </div>
           ))}
--- app/components/CarbonTonnesBreakdownCard/styles.ts
+++ app/components/CarbonTonnesBreakdownCard/styles.ts
@@ -1,13 +1,16 @@
 import { css } from "@emotion/css";
 import breakpoints from "@klimadao/lib/theme/breakpoints";
 import * as common from "@klimadao/lib/theme/common";
-import * as typography from "@klimadao/lib/theme/typography";
 
 export const card = css`
   ${common.cardSurface};
   align-content: start;
   grid-template-rows: min-content 1fr;
   gap: 0.8rem;
+  display: none;
+  ${breakpoints.large} {
+    display: grid;
+  }
   .header {
     display: flex;
     justify-content: space-between;
@@ -24,36 +27,16 @@ export const card = css`
   }
   .stack {
     display: grid;
-    gap: 0.4rem;
+    gap: 1.6rem;
     overflow: hidden;
   }
-  .value {
-    ${typography.h2_alt}
-  }
   .label {
-    ${typography.h4};
+    gap: 0.8rem;
+    display: flex;
   }
   p {
     overflow: hidden;
     text-overflow: ellipsis;
-  }
-
-  ${breakpoints.desktop} {
-    .value {
-      ${typography.h4}
-    }
-    .label {
-      ${typography.h5}
-    }
-  }
-
-  ${breakpoints.desktopLarge} {
-    .value {
-      ${typography.h2_alt}
-    }
-    .label {
-      ${typography.h4};
-    }
   }
 `;
 
@@ -61,9 +44,6 @@ export const row = css`
   display: flex;
   gap: 2.4rem;
   align-items: center;
-  border-bottom: 0.1rem solid var(--surface-03);
-  padding-bottom: 1.6rem;
-  margin-bottom: 1.6rem;
   &:last-child {
     border-bottom: none;
     margin-bottom: 0;
