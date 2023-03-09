37929b54f961df318db4fa07d3f0e63b603390f8
Michael Collins <0xemc@protonmail.com>
2023-03-09 18:37:04 +1100

    304 - Add a custom BigNumber sort for projects (#329)

--- carbonmark/components/ProjectFilterModal/constants.tsx
+++ carbonmark/components/ProjectFilterModal/constants.tsx
@@ -1,6 +1,7 @@
 import { t } from "@lingui/macro";
 import { CheckboxOption } from "components/CheckboxGroup/CheckboxGroup.types";
 import { Text } from "components/Text";
+import { BigNumber } from "ethers";
 import { categoryNames, getCategoryInfo } from "lib/getCategoryInfo";
 import { Project } from "lib/types/carbonmark";
 import { sortBy } from "lib/utils/array.utils";
@@ -15,10 +16,15 @@ export const PROJECT_SORT_OPTIONS = {
   "vintage-oldest": t`Vintage Oldest`,
 } as const;
 
+/** Sort projects by big number price @todo extract as a generic BigNumber sort fn */
+const projectPriceSort = (a: Project, b: Project) =>
+  BigNumber.from(a.price).lt(BigNumber.from(b.price)) ? -1 : 1;
+
 /** A Collection of sort functions keyed by sort option */
 export const PROJECT_SORT_FNS: Record<string, IdentityFn<Project[]>> = {
-  "price-highest": sortBy<Project>("price", "desc"),
-  "price-lowest": sortBy<Project>("price"),
+  "price-highest": (projects: Project[]) =>
+    projects.sort(projectPriceSort).reverse(),
+  "price-lowest": (projects: Project[]) => projects.sort(projectPriceSort),
   "recently-updated": sortBy<Project>("updatedAt", "desc"),
   "vintage-newest": sortBy<Project>("vintage", "desc"),
   "vintage-oldest": sortBy<Project>("vintage"),
