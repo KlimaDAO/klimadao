d0135b5f0e2b484481395cb2fc2a77b6a9c54b52
Atmosfearful <atmosfearful@protonmail.com>
2023-03-16 08:06:27 -0700

    update carbonmark address

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -35,7 +35,7 @@ const mainnet = {
   retirementAggregatorV2: "0x8cE54d9625371fb2a068986d32C85De8E6e995f8",
   liveOffsetWallet: "0xa17b52d5e17254b03dfdf7b4dff2fc0c6108faac",
   liveOffsetContract: "0xB99fAbB350bbb48b8d586835d001085c8F188BA0",
-  carbonmark: "0x235126D9CfE4790a41dd64d99446B4AF3DC574CF", // Diamond
+  carbonmark: "0x77F2014496Cd168662f74F860353BE1d16deCB2E", // Diamond
 };
 
 const testnet: typeof mainnet = {
