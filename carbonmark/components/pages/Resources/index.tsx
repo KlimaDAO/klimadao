0015a4cd897869872ca5dfb072e139288fc85582
rstycedar <94987759+rstycedar@users.noreply.github.com>
2023-03-22 07:45:48 -0700

    hide featured Articles in Carbonmark (#378)

--- carbonmark/components/pages/Resources/index.tsx
+++ carbonmark/components/pages/Resources/index.tsx
@@ -1,14 +1,11 @@
-import { GridContainer, Section } from "@klimadao/lib/components";
-import { t, Trans } from "@lingui/macro";
+import { GridContainer } from "@klimadao/lib/components";
+import { t } from "@lingui/macro";
 import { Footer } from "components/Footer";
 import { PageHead } from "components/PageHead";
 import { Navigation } from "components/shared/Navigation";
-import { Text } from "components/Text";
 import { Document, FeaturedPost } from "lib/queries";
 import { NextPage } from "next";
-import { ArticlesSlider } from "./FeaturedArticles/ArticlesSlider";
 import { ResourcesList } from "./ResourcesList";
-import * as styles from "./styles";
 
 export interface Props {
   documents: Document[];
@@ -26,7 +23,8 @@ export const Resources: NextPage<Props> = (props) => {
 
       <Navigation activePage="Resources" showThemeToggle={false} />
 
-      {!!props.featuredArticles?.length && (
+      {/* hide featured articles for now */}
+      {/* {!!props.featuredArticles?.length && (
         <Section variant="gray" className={styles.sectionHead}>
           <div className={styles.header}>
             <Text t="h1" align="center">
@@ -40,12 +38,11 @@ export const Resources: NextPage<Props> = (props) => {
           </div>
         </Section>
       )}
-
       {!!props.featuredArticles?.length && (
         <Section variant="gray" style={{ padding: "unset" }}>
           <ArticlesSlider articles={props.featuredArticles} />
         </Section>
-      )}
+      )} */}
 
       <ResourcesList documents={props.documents} />
 
