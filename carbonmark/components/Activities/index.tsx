e756ba40ebdd7ffd723024908f0fcc2771eadd91
Atmosfearful <atmosfearful@protonmail.com>
2023-04-13 17:31:57 -0700

    differentiate UserActivity and ProjectActivity

--- carbonmark/components/Activities/index.tsx
+++ carbonmark/components/Activities/index.tsx
@@ -2,33 +2,28 @@ import { Trans } from "@lingui/macro";
 import { Card } from "components/Card";
 import { Spinner } from "components/shared/Spinner";
 import { Text } from "components/Text";
-import { ActivityType } from "lib/types/carbonmark";
+import { ProjectActivity, UserActivity } from "lib/types/carbonmark";
 import { FC } from "react";
 import { Activity } from "./Activity";
 import * as styles from "./styles";
 
 interface Props {
-  activities: ActivityType[];
+  activities: ProjectActivity[] | UserActivity[];
   isLoading?: boolean;
-  showTitles?: boolean;
 }
 
 export const Activities: FC<Props> = (props) => {
-  const showTitle = props.showTitles ?? true;
-  const hasActivities = !!props.activities?.length;
-  const sortedActivities =
-    (hasActivities &&
-      props.activities
-        .sort((a, b) => Number(b.timeStamp) - Number(a.timeStamp))
-        .slice(0, 5)) ||
-    [];
+  const activities = props.activities || [];
+  const sortedActivities = activities
+    .sort((a, b) => Number(b.timeStamp) - Number(a.timeStamp))
+    .slice(0, 5);
 
   return (
     <Card>
       <Text t="h4" color="lighter">
         <Trans>Activity</Trans>
       </Text>
-      {!hasActivities && (
+      {!sortedActivities.length && (
         <Text t="body1" color="lighter">
           <i>
             <Trans id="user.activities.empty_state">No activity to show</Trans>
@@ -40,14 +35,9 @@ export const Activities: FC<Props> = (props) => {
           <Spinner />
         </div>
       )}
-      {!!sortedActivities.length &&
-        sortedActivities.map((activity) => (
-          <Activity
-            key={activity.id}
-            activity={activity}
-            showTitle={showTitle}
-          />
-        ))}
+      {sortedActivities.map((activity) => (
+        <Activity key={activity.id} activity={activity} />
+      ))}
     </Card>
   );
 };
