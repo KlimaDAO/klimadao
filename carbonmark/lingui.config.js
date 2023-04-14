0258343515e1df4f6c1c4b90330bf937f155d79b
Atmosfearful <atmosfearful@protonmail.com>
2023-04-14 14:13:52 -0700

    swap env vars for lingui

--- carbonmark/lingui.config.js
+++ carbonmark/lingui.config.js
@@ -2,16 +2,16 @@
 const config = require("../lingui.config.js");
 
 const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
-const TRANSLATIONIO_KEY_SITE = process.env.TRANSLATIONIO_KEY_SITE;
+const TRANSLATIONIO_KEY_CARBONMARK = process.env.TRANSLATIONIO_KEY_CARBONMARK;
 
-if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_SITE) {
+if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_CARBONMARK) {
   console.log("Running on GITHUB without the API key => exit");
   throw new Error("ON GITHUB WITHOUT THE API KEY");
-} else if (TRANSLATIONIO_KEY_SITE) {
+} else if (TRANSLATIONIO_KEY_CARBONMARK) {
   // Adds translation io service if env var is available
   config.service = {
     name: "TranslationIO",
-    apiKey: TRANSLATIONIO_KEY_SITE,
+    apiKey: TRANSLATIONIO_KEY_CARBONMARK,
   };
 }
 
