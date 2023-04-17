8f67add2f3524228011e6dd1cde11872c3aff800
Atmosfearful <atmosfearful@protonmail.com>
2023-04-17 15:44:11 -0700

    format-all for carbon-projects CMS

--- carbon-projects/sanity.config.ts
+++ carbon-projects/sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./schemas";
 
 export default defineConfig({
-  name: 'default',
-  title: 'project-data',
+  name: "default",
+  title: "project-data",
 
-  projectId: 'l6of5nwi',
-  dataset: 'production',
+  projectId: "l6of5nwi",
+  dataset: "production",
 
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
