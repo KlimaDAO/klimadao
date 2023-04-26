be7488283170a6fb6cb5e9a6b8afd95505406184
ladytrekker <ladytrekker@protonmail.com>
2023-04-26 10:27:29 +0200

    Use SWR to render Profile data

--- carbonmark/components/pages/Users/index.tsx
+++ carbonmark/components/pages/Users/index.tsx
@@ -4,26 +4,31 @@ import { Layout } from "components/Layout";
 import { PageHead } from "components/PageHead";
 import { Spinner } from "components/shared/Spinner";
 import { useConnectedUser } from "hooks/useConnectedUser";
+import { useFetchUser } from "hooks/useFetchUser";
+import { fetcher } from "lib/fetcher";
 import { User } from "lib/types/carbonmark";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
+import { SWRConfig } from "swr";
 import { SellerConnected } from "./SellerConnected";
 import { SellerUnconnected } from "./SellerUnconnected";
 
-type Props = {
+export type PageProps = {
   userAddress: string;
   userDomain: string | null;
   carbonmarkUser: User | null;
 };
 
-export const Users: NextPage<Props> = (props) => {
+const Page: NextPage<PageProps> = (props) => {
+  const { carbonmarkUser } = useFetchUser(props.userAddress);
+
   const { isConnectedUser, isUnconnectedUser } = useConnectedUser(
     props.userAddress
   );
   const [isLoading, setIsLoading] = useState(false);
 
   const userName =
-    props.userDomain || props.carbonmarkUser?.handle || props.userAddress;
+    props.userDomain || carbonmarkUser?.handle || props.userAddress;
 
   // Wait until web3 is ready
   useEffect(() => {
@@ -36,10 +41,10 @@ export const Users: NextPage<Props> = (props) => {
     <>
       <PageHead
         title={t`${
-          props.carbonmarkUser?.handle || concatAddress(props.userAddress)
+          carbonmarkUser?.handle || concatAddress(props.userAddress)
         } | Profile | Carbonmark`}
         mediaTitle={`${
-          props.carbonmarkUser?.handle || concatAddress(props.userAddress)
+          carbonmarkUser?.handle || concatAddress(props.userAddress)
         }'s Profile on Carbonmark`}
         metaDescription={t`Create and edit listings, and track your activity with your Carbonmark profile.`}
       />
@@ -51,13 +56,13 @@ export const Users: NextPage<Props> = (props) => {
           <SellerConnected
             userAddress={props.userAddress}
             userName={userName}
-            carbonmarkUser={props.carbonmarkUser}
+            carbonmarkUser={carbonmarkUser}
           />
         )}
 
         {isUnconnectedUser && (
           <SellerUnconnected
-            carbonmarkUser={props.carbonmarkUser}
+            carbonmarkUser={carbonmarkUser}
             userName={userName}
           />
         )}
@@ -65,3 +70,16 @@ export const Users: NextPage<Props> = (props) => {
     </>
   );
 };
+
+export const Users: NextPage<PageProps> = (props) => (
+  <SWRConfig
+    value={{
+      fetcher,
+      fallback: {
+        [`/api/users/${props.userAddress}?type=wallet`]: props.carbonmarkUser,
+      },
+    }}
+  >
+    <Page {...props} />
+  </SWRConfig>
+);
