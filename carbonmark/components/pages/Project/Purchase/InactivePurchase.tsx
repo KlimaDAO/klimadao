8b04b54e8a12f582240d8796c4f6cdd16ad72e9d
ladytrekker <ladytrekker@protonmail.com>
2023-05-19 17:19:50 +0200

    Fix build

--- carbonmark/components/pages/Project/Purchase/InactivePurchase.tsx
+++ carbonmark/components/pages/Project/Purchase/InactivePurchase.tsx
@@ -1,10 +1,10 @@
 import { Trans } from "@lingui/macro";
 import { Card } from "components/Card";
+import { ProjectHeader } from "components/pages/Project/ProjectHeader";
 import { Text } from "components/Text";
 import { Listing, Project } from "lib/types/carbonmark";
 import { FC } from "react";
 import { Price } from "./Price";
-import { ProjectHeader } from "./ProjectHeader";
 import * as styles from "./styles";
 
 export interface Props {
--- carbonmark/components/pages/Project/Retire/types.ts
+++ carbonmark/components/pages/Project/Retire/types.ts
@@ -1,13 +1,11 @@
-import {
-  CarbonmarkPaymentMethod,
-  CarbonmarkRetirementToken,
-} from "lib/types/carbonmark";
+import { CarbonToken } from "@klimadao/lib/constants";
+import { CarbonmarkPaymentMethod } from "lib/types/carbonmark";
 
 export type FormValues = {
   projectAddress: string;
   paymentMethod: CarbonmarkPaymentMethod;
   maxAmountIn: string;
-  retirementToken: CarbonmarkRetirementToken;
+  retirementToken: CarbonToken;
   quantity: string;
   beneficiaryAddress: string;
   beneficiaryName: string;
