c86b76559f0423cbc198e099c44e0e9b53a169c3
ladytrekker <ladytrekker@protonmail.com>
2023-05-22 14:39:15 +0200

    Add fiat to payment methods

--- carbonmark/lib/getPaymentMethods.ts
+++ carbonmark/lib/getPaymentMethods.ts
@@ -1,15 +1,18 @@
 import { CarbonmarkPaymentMethod } from "lib/types/carbonmark";
 import { StaticImageData } from "next/legacy/image";
+import FIAT from "public/icons/Fiat.png";
 import USDC from "public/icons/USDC.png";
 
 export type CarbonmarkPaymentMethodMap = {
   [key in CarbonmarkPaymentMethod]: {
     id: string;
     icon: StaticImageData;
-    label: Uppercase<CarbonmarkPaymentMethod>;
+    label: Uppercase<CarbonmarkPaymentMethod> | "Credit Card";
+    disabled: boolean;
   };
 };
 
 export const carbonmarkPaymentMethodMap: CarbonmarkPaymentMethodMap = {
-  usdc: { id: "usdc", icon: USDC, label: "USDC" },
+  usdc: { id: "usdc", icon: USDC, label: "USDC", disabled: false },
+  fiat: { id: "fiat", icon: FIAT, label: "Credit Card", disabled: true },
 };
