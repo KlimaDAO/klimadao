3550a2f415f3bedd91d09e02ef731df1adf47a19
0xAeterno <97446324+0xAeterno@users.noreply.github.com>
2023-05-23 23:51:48 +1000

    rough placement of retirement details

--- carbonmark/lib/retirementCertificates/index.ts
+++ carbonmark/lib/retirementCertificates/index.ts
@@ -29,6 +29,7 @@ type Params = {
 const KLIMA_GREEN = "#00cc33";
 const BLUE = "#0019FF";
 const GRAY = "#626266";
+const BLACK = "#000000";
 const PRIMARY_FONT_COLOR = "#313131";
 const SECONDARY_FONT_COLOR = "#767676";
 const spacing = {
@@ -70,12 +71,12 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
   });
 
   const setupFonts = () => {
-    doc.registerFont("Poppins Bold", Buffer.from(PoppinsBold, "base64"));
+    doc.registerFont("Poppins-Bold", Buffer.from(PoppinsBold, "base64"));
     doc.registerFont(
-      "Poppins Semibold",
+      "Poppins-Semibold",
       Buffer.from(PoppinsSemiBold, "base64")
     );
-    doc.registerFont("DM Sans", Buffer.from(DMSansRegular, "base64"));
+    doc.registerFont("DMSans", Buffer.from(DMSansRegular, "base64"));
   };
 
   const printHeader = (): void => {
@@ -85,19 +86,86 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
     //   height: 28,
     // });
 
-    doc.font("Poppins Bold");
+    doc.font("Poppins-Bold");
     doc.fontSize(16);
     doc.fillColor(GRAY);
     doc.text("Proof of", spacing.margin, 20);
     doc.fontSize(24);
     doc.fillColor(BLUE);
-    doc.text("Carbon Credit Retirement", spacing.margin, 35);
+    doc.text("Carbon Credit Retirement", spacing.margin, 37);
+  };
 
-    doc.lineWidth(3);
-    doc.moveTo(spacing.margin, 148);
-    doc.lineTo(491, 148);
-    doc.strokeColor(KLIMA_GREEN);
-    doc.stroke();
+  const printRetirementDetails = (): void => {
+    const retirementAmount =
+      Number(params.retirement.amount) < 0.01
+        ? "< 0.01"
+        : trimWithLocale(params.retirement.amount, 2, "en");
+
+    doc.font("Poppins-Semibold");
+    doc.fontSize(14);
+    doc.fillColor(GRAY);
+    doc.text("APRIL 24, 2023", spacing.margin, 92, {
+      characterSpacing: 0.3,
+    });
+
+    doc.font("Poppins-Bold");
+    doc.fontSize(60);
+    doc.fillColor(BLACK);
+    doc.text(`2,000t`, spacing.margin, 100, {
+      characterSpacing: -2,
+    });
+    // doc.text(`${retirementAmount}t`, spacing.margin, 169);
+
+    doc.font("Poppins-Semibold");
+    doc.fontSize(14);
+    doc.fillColor(GRAY);
+    doc.text("VERIFIED TONNES OF CARBON RETIRED", spacing.margin, 175, {
+      characterSpacing: 0.3,
+    });
+
+    doc.text("BENEFICIARY:", spacing.margin, 220, {
+      characterSpacing: 0.3,
+    });
+
+    const beneficaryText =
+      params.retirement.beneficiary || params.retirement.beneficiaryAddress;
+    doc.text("BENEFICIARY:", spacing.margin, 220, {
+      characterSpacing: 0.3,
+    });
+
+    doc.font("Poppins-Bold");
+    doc.fontSize(20);
+    doc.fillColor(BLACK);
+    doc.text(beneficaryText, spacing.margin, 240, { width: 375 });
+
+    const beneficiaryNameBlockHeight = doc.heightOfString(beneficaryText, {
+      width: 375,
+    });
+
+    const retirementMessage = params.retirement.retirementMessage;
+    doc.font("DMSans");
+    doc.fontSize(16);
+    doc.text(
+      `“${retirementMessage}”`,
+      spacing.margin,
+      240 + beneficiaryNameBlockHeight + 20,
+      { width: 375 }
+    );
+
+    const retirementMessageBlockHeight = doc.heightOfString(retirementMessage, {
+      width: 375,
+    });
+
+    const disclaimer =
+      "This represents the permanent retirement of a digital carbon asset. This retirement and the associated data are immutable public records.";
+    doc.fontSize(12);
+    doc.fillColor(GRAY);
+    doc.text(
+      disclaimer,
+      spacing.margin,
+      240 + beneficiaryNameBlockHeight + 20 + retirementMessageBlockHeight + 20,
+      { width: 375 }
+    );
   };
 
   const printFeatureImage = async (): Promise<void> => {
@@ -111,60 +179,28 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
     });
   };
 
-  const printRetirementDetails = (): void => {
-    const retirementAmount =
-      Number(params.retirement.amount) < 0.01
-        ? "< 0.01"
-        : trimWithLocale(params.retirement.amount, 2, "en");
-
-    doc.fontSize(28);
-    doc.font("Poppins Semibold");
-    doc.text(`${retirementAmount} tonnes`, spacing.margin, 169);
-
-    const beneficaryText =
-      params.retirement.beneficiary || params.retirement.beneficiaryAddress;
-    doc.font("Poppins Bold");
-    doc.lineGap(-13);
-    doc.text(beneficaryText, spacing.margin, 200, { width: 450 });
-
-    const beneficiaryNameBlockHeight = doc.heightOfString(beneficaryText, {
-      width: 450,
-    });
-
-    const retirementMessage = params.retirement.retirementMessage;
-    doc.font("Poppins Semibold");
-    doc.fontSize(12);
-    doc.lineGap(-1.5);
-    doc.text(
-      retirementMessage,
-      spacing.margin,
-      200 + beneficiaryNameBlockHeight + 16,
-      { width: 450 }
-    );
-  };
-
   const printTransactionDetails = (): void => {
     doc.fontSize(11);
-    doc.font("Poppins Bold");
+    doc.font("Poppins-Bold");
     doc.text(
       "Beneficiary Address:",
       spacing.margin,
       spacing.transactionDetails
     );
-    doc.font("Poppins Semibold");
+    doc.font("Poppins-Semibold");
     doc.text(
       params.retirement.beneficiaryAddress,
       spacing.margin,
       spacing.transactionDetails + 15
     );
 
-    doc.font("Poppins Bold");
+    doc.font("Poppins-Bold");
     doc.text(
       "Transaction ID:",
       spacing.margin,
       spacing.transactionDetails + 30
     );
-    doc.font("Poppins Semibold");
+    doc.font("Poppins-Semibold");
     doc.text(
       params.retirement.transaction.id,
       spacing.margin,
@@ -239,10 +275,10 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
     let startPosition = spacing.projectDetails;
     projectDetails.forEach((detail) => {
       const label = `${detail.label}:`;
-      doc.font("Poppins Bold");
+      doc.font("Poppins-Bold");
       doc.text(label, spacing.margin, startPosition);
 
-      doc.font("Poppins Semibold");
+      doc.font("Poppins-Semibold");
       doc.text(
         `${detail.value}`,
         spacing.margin + doc.widthOfString(label) + 9,
@@ -255,7 +291,7 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
 
   const printMossProjectDetails = () => {
     const linkText = "Learn more ";
-    doc.font("Poppins Bold");
+    doc.font("Poppins-Bold");
     doc.fillColor(SECONDARY_FONT_COLOR);
     doc.text(linkText, spacing.margin, spacing.projectDetails + 30, {
       link: `${urls.carbonDashboard}/MCO2`,
@@ -270,7 +306,7 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
 
   const printRetirementLink = (): void => {
     const text = "View this retirement on ";
-    doc.font("Poppins Bold");
+    doc.font("Poppins-Bold");
     doc.text(text, spacing.margin, spacing.retirementLink);
     doc.fillColor(KLIMA_GREEN);
     doc.text(
@@ -286,10 +322,10 @@ export const generateCertificate = (params: Params): PDFKit.PDFDocument => {
   printHeader();
   printFeatureImage();
   printRetirementDetails();
-  printTransactionDetails();
-  printProjectDetails();
+  // printTransactionDetails();
+  // printProjectDetails();
   if (isMossRetirement) printMossProjectDetails();
-  printRetirementLink();
+  // printRetirementLink();
 
   return doc;
 };
