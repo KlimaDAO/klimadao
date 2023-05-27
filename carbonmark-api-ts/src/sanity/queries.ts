a1c95476b27e0d540ee2fbc1042287c728dd2e51
emc <0xemc@protonmail.com>
2023-05-27 17:53:16 +1000

    Add Sanity(#1033)

---
+++ carbonmark-api-ts/src/sanity/queries.ts
@@ -0,0 +1,31 @@
+import groq from "groq";
+
+const fetchProjects = groq`*[_type == 'project' && registry == $registry && registryProjectId == $registryProjectId][0] {
+    country,
+    description,
+    "geolocation": {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [geolocation.lng, geolocation.lat]
+      }
+    },
+    methodologies[]->{ "id": id.current, category, name },
+    name,
+    region,
+    registry,
+    url,
+    registryProjectId
+  }`;
+
+const fetchAllProjects = groq`
+  *[_type == 'project'] {
+    "id": id.current,
+    description,
+    registry,
+    registryProjectId,
+    methodologies[]->{  "id": id.current, category, name },
+    name,
+  }`;
+
+export { fetchProjects, fetchAllProjects };
