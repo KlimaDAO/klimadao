2b83e76a200a98404e2e4a1e7b7306bb965dca85
emc <0xemc@protonmail.com>
2023-06-16 13:53:47 +1000

    fix: debug route on carbonmark-api

---
+++ carbonmark-api/routes/debug.js
@@ -0,0 +1,22 @@
+/** Selected ENVs to display */
+const DEBUG_KEYS = [
+  "POOL_PRICES_GRAPH_API_URL",
+  "ASSETS_GRAPH_API_URL",
+  "CARBON_OFFSETS_GRAPH_API_URL",
+  "GRAPH_API_URL",
+  "VERCEL_ENV",
+];
+
+module.exports = async function (fastify, opts) {
+  fastify.route({
+    method: "GET",
+    path: "/debug",
+    handler: async function (request, reply) {
+      const envs = DEBUG_KEYS.reduce(
+        (res, curr) => ({ ...res, [curr]: process.env[curr] }),
+        {}
+      );
+      return reply.send(JSON.stringify(envs));
+    },
+  });
+};
