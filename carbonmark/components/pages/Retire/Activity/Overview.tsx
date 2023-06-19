e0c8ee1b90ebf21fe34c97393a61682bb5d6a086
ladytrekker <ladytrekker@protonmail.com>
2023-06-19 16:51:17 +0200

    Better tonnes formmating with locale

--- carbonmark/components/pages/Retire/Activity/Overview.tsx
+++ carbonmark/components/pages/Retire/Activity/Overview.tsx
@@ -2,15 +2,18 @@ import { cx } from "@emotion/css";
 import {
   getTotalCarbonRetired,
   getTotalRetirements,
+  trimWithLocale,
   useWeb3,
 } from "@klimadao/lib/utils";
 import { t } from "@lingui/macro";
 import { Card } from "components/Card";
 import { Text } from "components/Text";
+import { useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
 import * as styles from "./styles";
 
 export const RetireOverview: FC = () => {
+  const { locale } = useRouter();
   const { isConnected, address } = useWeb3();
   const [isLoadingTotals, setIsLoadingTotals] = useState(false);
   const [totalRetirements, setTotalRetirements] = useState<null | number>(0.0);
@@ -57,7 +60,8 @@ export const RetireOverview: FC = () => {
         {isLoadingTotals && <Text>{t`Loading...`}</Text>}
         {!isLoadingTotals && (
           <Text t="h2" className={cx({ error: !!error })}>
-            {totalCarbonRetired}
+            {trimWithLocale(totalCarbonRetired || "0", 2, locale)}
+            {t`t`}
           </Text>
         )}
       </div>
