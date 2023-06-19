f5268cc2104f754f642e0645f7720763b33f85e1
ladytrekker <ladytrekker@protonmail.com>
2023-06-19 13:15:29 +0200

    First basic Retire Page Set-Up

---
+++ carbonmark/components/pages/Retire/index.tsx
@@ -0,0 +1,56 @@
+import { concatAddress, useWeb3 } from "@klimadao/lib/utils";
+import { t, Trans } from "@lingui/macro";
+import { CopyAddressButton } from "components/CopyAddressButton";
+import { Layout } from "components/Layout";
+import { LoginButton } from "components/LoginButton";
+import { PageHead } from "components/PageHead";
+import { Text } from "components/Text";
+import { useGetDomainFromAddress } from "hooks/useGetDomainFromAddress";
+import { NextPage } from "next";
+import * as styles from "./styles";
+
+export const Retire: NextPage = () => {
+  const { address, isConnected } = useWeb3();
+  // collect nameserviceDomain Data if connected and domain is in URL
+  const connectedDomain = useGetDomainFromAddress(address);
+
+  const beneficiary = connectedDomain?.name || address;
+  const displayName =
+    connectedDomain?.name || (address && concatAddress(address));
+
+  return (
+    <>
+      <PageHead
+        title={t`Retire | Carbonmark`}
+        mediaTitle={t`Retire | Carbonmark`}
+        metaDescription={t`View a complete overview of the digital carbon that you retired. `}
+      />
+
+      <Layout>
+        <div className={styles.container}>
+          <div>
+            <Text t="h2">
+              <Trans>Carbon Retirements</Trans>
+            </Text>
+            {address && isConnected && (
+              <div className={styles.beneficiary}>
+                <Text t="body4">
+                  <Trans>for beneficiary</Trans>{" "}
+                  <span className="highlight">{displayName}</span>
+                </Text>
+                <CopyAddressButton
+                  address={beneficiary}
+                  variant="transparent"
+                  className="copyButton"
+                />
+              </div>
+            )}
+          </div>
+          <div className={styles.retireControls}>
+            <LoginButton />
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
---
+++ carbonmark/components/pages/Retire/styles.ts
@@ -0,0 +1,44 @@
+import { css } from "@emotion/css";
+import breakpoints from "@klimadao/lib/theme/breakpoints";
+
+export const container = css`
+  grid-column: main;
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+  gap: 2.4rem;
+`;
+
+export const retireControls = css`
+  grid-column: main;
+  flex-direction: row-reverse;
+  display: none;
+
+  ${breakpoints.desktop} {
+    display: flex;
+  }
+`;
+
+export const spinnerContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const beneficiary = css`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+
+  .highlight {
+    color: var(--bright-blue);
+  }
+
+  .copyButton {
+    background: none;
+    background-color: transparent !important;
+    svg path {
+      fill: var(--bright-blue);
+    }
+  }
+`;
