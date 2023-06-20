913b863b641466ac4d1f712c5a4136f45b018483
ladytrekker <ladytrekker@protonmail.com>
2023-06-20 13:20:06 +0200

    Remove Loading spinner on initial load

--- carbonmark/components/pages/Retire/index.tsx
+++ carbonmark/components/pages/Retire/index.tsx
@@ -4,7 +4,6 @@ import { CopyAddressButton } from "components/CopyAddressButton";
 import { Layout } from "components/Layout";
 import { LoginButton } from "components/LoginButton";
 import { PageHead } from "components/PageHead";
-import { Spinner } from "components/shared/Spinner";
 import { Text } from "components/Text";
 import { useGetDomainFromAddress } from "hooks/useGetDomainFromAddress";
 import { NextPage } from "next";
@@ -12,15 +11,13 @@ import { RetireActivity } from "./Activity";
 import * as styles from "./styles";
 
 export const Retire: NextPage = () => {
-  const { isConnected, address, initializing } = useWeb3();
+  const { isConnected, address } = useWeb3();
   // collect nameserviceDomain Data if connected and domain is in URL
   const connectedDomain = useGetDomainFromAddress(address);
 
   const beneficiary = connectedDomain?.name || address;
   const displayName =
     connectedDomain?.name || (address && concatAddress(address));
-
-  const isConnectedUser = isConnected && address;
 
   return (
     <>
@@ -53,12 +50,6 @@ export const Retire: NextPage = () => {
           <div className={styles.retireControls}>
             <LoginButton />
           </div>
-
-          {!isConnectedUser && initializing && (
-            <div className={styles.spinnerContainer}>
-              <Spinner />
-            </div>
-          )}
 
           <RetireActivity />
         </div>
