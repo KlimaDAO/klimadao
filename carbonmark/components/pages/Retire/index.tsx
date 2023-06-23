c3849bef44240421155f758df6d8da7f90a9b84d
ladytrekker <ladytrekker@protonmail.com>
2023-06-23 16:31:18 +0200

    Render basic featured projects

--- carbonmark/components/pages/Retire/index.tsx
+++ carbonmark/components/pages/Retire/index.tsx
@@ -1,13 +1,16 @@
 import { concatAddress, useWeb3 } from "@klimadao/lib/utils";
 import { t, Trans } from "@lingui/macro";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
 import { CopyAddressButton } from "components/CopyAddressButton";
 import { Layout } from "components/Layout";
 import { LoginButton } from "components/LoginButton";
 import { PageHead } from "components/PageHead";
+import { ProjectCard } from "components/ProjectCard";
 import { Text } from "components/Text";
 import { useGetDomainFromAddress } from "hooks/useGetDomainFromAddress";
 import { Project } from "lib/types/carbonmark";
 import { NextPage } from "next";
+import Link from "next/link";
 import { RetireActivity } from "./Activity";
 import * as styles from "./styles";
 
@@ -16,6 +19,7 @@ export type PageProps = {
 };
 
 export const Retire: NextPage<PageProps> = (props) => {
+  console.log("props", props);
   const { isConnected, address } = useWeb3();
   // collect nameserviceDomain Data if connected and domain is in URL
   const connectedDomain = useGetDomainFromAddress(address);
@@ -57,6 +61,37 @@ export const Retire: NextPage<PageProps> = (props) => {
           </div>
 
           <RetireActivity />
+        </div>
+
+        <div className={styles.fullWhite}>
+          <div className={styles.content}>
+            <div className={styles.cardsHeader}>
+              <Text t="h4" className={styles.textWithIcon}>
+                <LocalPoliceIcon className="featured" fontSize="inherit" />
+                <Trans>Retire from a featured project</Trans>
+              </Text>
+
+              <Link href="/projects">
+                <Text t="body4" className={styles.textLink}>
+                  <Trans>View all Projects</Trans>
+                </Text>
+              </Link>
+            </div>
+
+            <Text className={styles.cardsDescription}>
+              <Trans>
+                We’ve hand-curated some of the best projects based on strict
+                criteria, such as price, veracity, and global environmental
+                impact.
+              </Trans>
+            </Text>
+
+            <div className={styles.cardsList}>
+              {props.featuredProjects.map((p) => (
+                <ProjectCard key={p.id} project={p} />
+              ))}
+            </div>
+          </div>
         </div>
       </Layout>
     </>
--- carbonmark/components/pages/Retire/styles.ts
+++ carbonmark/components/pages/Retire/styles.ts
@@ -41,3 +41,83 @@ export const beneficiary = css`
     }
   }
 `;
+
+export const fullWhite = css`
+  grid-column: full;
+
+  display: grid;
+  grid-template-columns: inherit;
+  background-color: var(--white);
+`;
+
+export const content = css`
+  grid-column: main;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 2rem 1.6rem;
+
+  ${breakpoints.large} {
+    margin: 4rem 2rem;
+  }
+
+  ${breakpoints.desktop} {
+    margin: 4rem;
+  }
+`;
+
+export const cardsHeader = css`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  ${breakpoints.large} {
+    gap: 2.4rem;
+  }
+
+  ${breakpoints.desktop} {
+    justify-content: space-between;
+  }
+`;
+
+export const textWithIcon = css`
+  display: flex;
+  gap: 0.4rem;
+  align-items: center;
+
+  .featured {
+    path {
+      fill: var(--yellow);
+    }
+  }
+`;
+
+export const textLink = css`
+  display: flex;
+  gap: 0.4rem;
+  align-items: center;
+  color: var(--bright-blue);
+  text-transform: uppercase;
+
+  &:hover,
+  &:visited {
+    text-decoration: underline;
+  }
+`;
+
+export const cardsDescription = css`
+  text-align: center;
+  padding: 0 2rem;
+
+  ${breakpoints.desktop} {
+    text-align: inherit;
+    padding: 0;
+  }
+`;
+
+export const cardsList = css`
+  display: flex;
+  gap: 2rem;
+`;
