f4b2aad33f7f59b14bbd48b240d1f270a9ed180e
emc <0xemc@protonmail.com>
2023-07-03 19:09:26 +1000

    Re-add networks to config

--- carbonmark/lib/constants.ts
+++ carbonmark/lib/constants.ts
@@ -8,14 +8,15 @@ type Environment = "production" | "preview" | "development";
 /** True if actually deployed on the production domain (not a preview/staging domain, not local dev) */
 export const IS_PRODUCTION =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
-
 /** True if local development (not preview deployment) */
 export const IS_LOCAL_DEVELOPMENT = process.env.NODE_ENV === "development";
-
 /** For preview deployments on Vercel */
 export const IS_PREVIEW = !IS_PRODUCTION && !IS_LOCAL_DEVELOPMENT;
 
 export const NEXT_PUBLIC_MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+/** Exposed via env vars, this is an infura id to be used in the browser, in getStaticProvider, as a fallback for polygon-rpc */
+export const CLIENT_INFURA_ID = process.env.NEXT_PUBLIC_CLIENT_INFURA_ID;
 
 const ENVIRONMENT: Environment =
   new LogicTable({
@@ -47,6 +48,11 @@ export const getConnectErrorStrings = () => ({
 });
 
 export const config = {
+  networks: {
+    production: "mainnet",
+    preview: "mainnet",
+    development: "mainnet",
+  },
   urls: {
     baseUrl: {
       production: "https://www.carbonmark.com",
@@ -85,5 +91,6 @@ export const urls = {
   intro: "/blog/introducing-carbonmark",
 };
 
-/** Exposed via env vars, this is an infura id to be used in the browser, in getStaticProvider, as a fallback for polygon-rpc */
-export const CLIENT_INFURA_ID = process.env.NEXT_PUBLIC_CLIENT_INFURA_ID;
+export const DEFAULT_NETWORK = config.networks[ENVIRONMENT] as
+  | "mainnet"
+  | "testnet";
