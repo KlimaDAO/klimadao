00bb692e92aaa7845b4d741b5891f8578f6de0b6
ladytrekker <ladytrekker@protonmail.com>
2023-07-05 11:40:13 +0200

    Replace with lib utils for offset with card in App

--- app/components/views/Offset/lib/getFiatRetirementCost.ts
+++ app/components/views/Offset/lib/getFiatRetirementCost.ts
@@ -1,4 +1,5 @@
-import { FIAT_RETIREMENT_API_URL } from "lib/constants";
+import { getFiatRetirementCost as getFiatRetirementCostLib } from "@klimadao/lib/utils";
+import { BASE_URL, IS_PRODUCTION } from "lib/constants";
 
 interface Params {
   beneficiary_address: string | null;
@@ -9,38 +10,10 @@ interface Params {
   project_address: string | null;
 }
 
-interface QuoteResponse {
-  url: "";
-  totalPriceCents: string;
-  networkFeeCents: string;
-}
-
-export const getFiatRetirementCost = async (
-  params: Params
-): Promise<string> => {
-  try {
-    const res = await fetch(FIAT_RETIREMENT_API_URL, {
-      body: JSON.stringify({
-        ...params,
-        cancel_url: "https://app.klimadao.finance/#/offset",
-        referrer: "klimadao",
-        mode: "quote",
-      }),
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    if (!res.ok) {
-      const { message, name } = await res.json();
-      const e = new Error(message);
-      e.name = name;
-      throw e;
-    }
-    const quote: QuoteResponse = await res.json();
-    return (Number(quote.totalPriceCents) / 100).toString();
-  } catch (e) {
-    console.error("getFiatRetirementCost Error", e);
-    throw e;
-  }
-};
+export const getFiatRetirementCost = async (params: Params): Promise<string> =>
+  await getFiatRetirementCostLib({
+    isProduction: IS_PRODUCTION,
+    cancelUrl: `${BASE_URL}/#/offset`,
+    referrer: "klimadao",
+    retirement: params,
+  });
