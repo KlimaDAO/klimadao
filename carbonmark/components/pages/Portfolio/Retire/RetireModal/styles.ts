f2ea19aaec24532d4b7ec02982a3284144de42d1
0xMakka <keith.33m@gmail.com>
2023-07-10 09:52:33 +0100

    replace spinner after retire modal submit

--- carbonmark/components/pages/Portfolio/Retire/RetireModal/styles.ts
+++ carbonmark/components/pages/Portfolio/Retire/RetireModal/styles.ts
@@ -1,4 +1,4 @@
-import { css, keyframes } from "@emotion/css";
+import { css } from "@emotion/css";
 
 export const formatParagraph = css`
   p {
@@ -10,7 +10,7 @@ export const processingTitle = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
 
   @media (max-width: 376px) {
     font-size: 22px;
@@ -27,41 +27,8 @@ export const processingRetirement = css`
   align-items: center;
 `;
 
-// Spinner
-const rotate = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
 export const processingSpinner = css`
-  position: relative !important;
-  width: 120px !important;
-  height: 120px !important;
-  border: 6px solid #fceabe !important;
-  border-radius: 50% !important;
-  margin: 0 auto !important;
-
-  &:before {
-    content: "";
-    position: absolute;
-    top: -6px;
-    left: -6px;
-    right: -6px;
-    bottom: -6px;
-    border: 6px solid gray;
-    border-color: #4e3d42 transparent transparent;
-    border-radius: 50%;
-    animation: ${rotate} 1.2s linear infinite;
-  }
-
-  & > div:first-child {
-    display: none !important;
-  }
-  & > div:last-child {
-    display: none !important;
-  }
+  width: 12rem !important;
+  height: 12rem !important;
+  transform: scale(2.25);
 `;
