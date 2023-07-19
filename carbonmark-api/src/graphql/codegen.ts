ba0a2077a731b19d3cc0fbf4617a6fa7337d264b
emc <0xemc@protonmail.com>
2023-07-19 18:48:30 +1000

    chore:1034 add mock generation

--- carbonmark-api/src/graphql/codegen.ts
+++ carbonmark-api/src/graphql/codegen.ts
@@ -1,7 +1,8 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
-import { GRAPH_URLS } from "src/constants/graphs.constants";
+import { GRAPH_URLS } from "../constants/graphs.constants";
 
 const GENERATED_DIR = "src/.generated/types";
+const GENERATED_MOCKS_DIR = "src/.generated/mocks";
 const DOCUMENTS_DIR = "src/graphql";
 
 const plugins = [
@@ -23,6 +24,18 @@ const generates = Object.entries(schema).reduce<CodegenConfig["generates"]>(
         `${DOCUMENTS_DIR}/${key}.fragments.gql`,
       ],
       plugins,
+    },
+    [`${GENERATED_MOCKS_DIR}/${key}.mocks.ts`]: {
+      schema,
+      plugins: [
+        {
+          "typescript-mock-data": {
+            typesFile: `${GENERATED_DIR}/${key}.types.ts`,
+            typeNames: "change-case-all#pascalCase",
+            transformUnderscore: false,
+          },
+        },
+      ],
     },
   }),
   {}
