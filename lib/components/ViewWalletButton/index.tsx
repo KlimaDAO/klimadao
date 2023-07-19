20ef4835937b260022e962351899d90ea5b73f7e
sprrwhwk <hi@selim.co>
2023-07-19 18:23:26 +0300

    remove onClick event listener

--- lib/components/ViewWalletButton/index.tsx
+++ lib/components/ViewWalletButton/index.tsx
@@ -26,7 +26,6 @@ export const ViewWalletButton: FC<Props> = (props) => {
         className={styles.viewButton}
         icon={<Launch />}
         iconPos={"suffix"}
-        onClick={}
       ></ButtonPrimary>
     </div>
   );
