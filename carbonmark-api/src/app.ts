d9f99ec0bcddd02fe8a82bcc343bb5c504d97af6
emc <0xemc@protonmail.com>
2023-07-20 10:30:29 +1000

    fix(app): Correct path to .env.local file

--- carbonmark-api/src/app.ts
+++ carbonmark-api/src/app.ts
@@ -4,7 +4,7 @@ import { FastifyPluginAsync } from "fastify";
 import { join } from "path";
 
 // This is not pretty but we need to reference the env file at the root of the monorepo
-const result = dotenv.config({ path: join(process.cwd(), ".env.local") });
+const result = dotenv.config({ path: join(process.cwd(), "../.env.local") });
 
 if (result.error) {
   console.error("Error loading .env file", result.error);
