2bc023b29b91e37d30c4aab03a4213bd3bc1a3fb
psparacino <tufnelenterprises@gmail.com>
2023-07-23 21:00:15 -0400

    updated testing

--- carbonmark-api/src/speedTests/test.ts
+++ carbonmark-api/src/speedTests/test.ts
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 
 
 import * as dotenv from "dotenv";
-dotenv.config({ path: "../.vercel/.env.development.local" });
+dotenv.config({ path: "../../../.vercel/.env.development.local" });
 
 if (!process.env.FIREBASE_ADMIN_CERT) {
     throw new Error("Environment variable FIREBASE_ADMIN_CERT is undefined");
@@ -20,13 +20,20 @@ const getOriginalIds = async () => {
     return ids;
   };
 
+  const getTestingIds = async () => {
+    const snapshot = await db.collection('usersTesting').get();
+    const ids = snapshot.docs.map(doc => doc.id);
+    return ids;
+  };
+
+
   const whereTest = async (ids: string[]) => {
     const start = Date.now();
   
     await Promise.all(ids.map(id => db.collection('usersTesting').where('address', '==', id).get()));
   
     const end = Date.now();
-    console.log(`whereTest duration: ${end - start} ms`);
+    console.info(`whereTest duration: ${end - start} ms`);
   };
 
 
@@ -37,14 +44,23 @@ const getOriginalIds = async () => {
     await db.getAll(...ids.map(id => db.collection('usersTesting').doc(id)));
   
     const end = Date.now();
-    console.log(`getAllTest duration: ${end - start} ms`);
+    console.info(`getAllTest duration: ${end - start} ms`);
   };
 
 
   getOriginalIds().then(ids => {
+    console.info("19 users return out of 1019");
     whereTest(ids).then(() => {
-      getAllTest(ids);
+        getAllTest(ids).then(() => {
+            getTestingIds().then(ids => {
+                console.info("Return all in collection");
+                whereTest(ids).then(() => {
+                    getAllTest(ids);
+                });
+            });
+        }); 
     });
-  });
+});
+
   
   
