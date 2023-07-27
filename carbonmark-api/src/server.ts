307f8b1738a2ebf3a8deea7ec6364349e49f84fb
emc <0xemc@protonmail.com>
2023-07-28 09:27:38 +1000

    PR Updates

--- carbonmark-api/src/server.ts
+++ carbonmark-api/src/server.ts
@@ -3,6 +3,13 @@ import app from "./app";
 
 const PORT = 3003;
 
+/**
+ * This server is required for local development because the API is deployed
+ * via Vercel serverless which handles incoming requests. By separating concerns (server & app)
+ * we can easily switch deployment environments and test.
+ *
+ * **Note** This is not required for tests as we use `app.inject` to mock requests to the FastifyInstance
+ */
 async function main() {
   const server = fastify();
   try {
--- carbonmark-api/test/flows/authentication.test.ts
+++ carbonmark-api/test/flows/authentication.test.ts
@@ -15,7 +15,7 @@ describe("Authentication flow", () => {
 
   // Setup the server
   afterEach(async () => await app.close());
-  beforeAll(async () => {
+  beforeEach(async () => {
     app = await build();
   });
 
@@ -47,6 +47,54 @@ describe("Authentication flow", () => {
       })
       .then((d) => d.body);
 
-    console.log("response", response);
+    const token = JSON.parse(response).token;
+
+    expect(typeof token).toBe("string");
+  });
+
+  test("should fail authentication for invalid user", async () => {
+    /** 1. Login */
+    const { nonce } = await app
+      .inject({
+        method: "POST",
+        url: `${DEV_URL}/users/login`,
+        body: {
+          wallet: wallet.address,
+        },
+      })
+      .then((d) => d.json());
+
+    /** 2. Build and sign the authentication message */
+    const message = process.env.AUTHENTICATION_MESSAGE + nonce;
+    const signed_message = await wallet.signMessage(message);
+
+    /** 3. Request  JRPC Token with invalid wallet */
+    const response = await app
+      .inject({
+        method: "POST",
+        url: `${DEV_URL}/users/login/verify`,
+        body: {
+          wallet: "0xInvalidWalletAddress",
+          signed_message,
+        },
+      })
+      .then((d) => d.json());
+
+    expect(response.error).toBeTruthy();
+  });
+
+  test("should fail login for invalid address", async () => {
+    /** 1. Login */
+    const response = await app
+      .inject({
+        method: "POST",
+        url: `${DEV_URL}/users/login`,
+        body: {
+          wallet: "0xInvalidWalletAddress",
+        },
+      })
+      .then((d) => d.json());
+
+    expect(response.error).toBeTruthy();
   });
 });
