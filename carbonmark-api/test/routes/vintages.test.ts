1d695dbfda02c9ce49cd5f814e1f3976a2ae205b
emc <0xemc@protonmail.com>
2023-08-04 11:11:43 +1000

    refactor: update import statements for GRAPH_URLS

--- carbonmark-api/test/routes/vintages.test.ts
+++ carbonmark-api/test/routes/vintages.test.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import nock from "nock";
+import { GRAPH_URLS } from "../../src/graphql/codegen.constants";
 import { aProject } from "../../src/.generated/mocks/marketplace.mocks";
-import { GRAPH_URLS } from "../../src/constants/graphs.constants";
 import { build } from "../helper";
 import { DEV_URL } from "../test.constants";
 import { ERROR } from "./routes.mock";
