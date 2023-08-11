bd4b177d5150885f14678e986f4f499b50cd4545
biwano <biwano@gmail.com>
2023-08-11 15:26:08 +0200

    Imporved Navigation

--- carbonmark-data/components/Layout/DesktopSidebar.tsx
+++ carbonmark-data/components/Layout/DesktopSidebar.tsx
@@ -2,28 +2,21 @@ import { FC } from "react";
 import * as styles from "./styles";
 
 import { CarbonmarkDataLogo } from "components/Graphics/CarbonmarkDataLogo";
-import { OffVsOnIcon } from "components/Graphics/OffVsOnIcon";
-import { OverviewIcon } from "components/Graphics/OverviewIcon";
-import { RetirementTrendsIcon } from "components/Graphics/RetirementTrendsIcon";
-import { SupplyIcon } from "components/Graphics/SupplyIcon";
-import { TokenDetailsIcon } from "components/Graphics/TokenDetailsIcon";
 import { useRouter } from "next/router";
-import { SidebarItem } from "./SidebarItem";
+import { DesktopSidebarItem } from "./DesktopSidebarItem";
+import { navItems } from "./NavItems";
 
 export const DesktopSidebar: FC = () => {
     const { pathname } = useRouter();
     return (<>
-        <div className={styles.desktopHeader}>
+        <div className={styles.desktopSidebar}>
             <CarbonmarkDataLogo></CarbonmarkDataLogo>
             <div aria-describedby="title">Carbon Dashboard</div>
             <div aria-describedby="links">
-                <SidebarItem url="/" icon={OverviewIcon}>Overview</SidebarItem>
-                <SidebarItem url="/off_vs_on_chain" icon={OffVsOnIcon}>Off vs On-Chain</SidebarItem>
-                <SidebarItem url="/supply" icon={SupplyIcon}>Supply</SidebarItem>
-                <SidebarItem url="/retirement_trends" icon={RetirementTrendsIcon}>Retirement Trends</SidebarItem>
-                <SidebarItem url="/token_details" icon={TokenDetailsIcon}>Token Details</SidebarItem>
-
-                <hr />
+                {navItems.map(navItem =>
+                    <DesktopSidebarItem navItem={navItem} key={navItem.url}></DesktopSidebarItem>
+                )}
+                < hr />
             </div>
 
         </div >
---
+++ carbonmark-data/components/Layout/NavItems.tsx
@@ -0,0 +1,51 @@
+
+import {
+    OffVsOnIconPath,
+    OverviewFilledIconPath,
+    OverviewIconPath,
+
+    RetirementTrendsIconPath,
+
+    SupplyIconPath,
+
+    TokenDetailsIconPath
+} from "components/Graphics/Icons";
+
+export interface NavItem {
+    label: string;
+    iconPath: () => JSX.Element;
+    activeIconPath: () => JSX.Element;
+    url: string;
+}
+export const navItems: Array<NavItem> = [
+    {
+        label: "Overview",
+        iconPath: OverviewIconPath,
+        activeIconPath: OverviewFilledIconPath,
+        url: "/"
+    },
+    {
+        label: "Off vs On-Chain",
+        iconPath: OffVsOnIconPath,
+        activeIconPath: OffVsOnIconPath,
+        url: "/off_vs_on_chain"
+    },
+    {
+        label: "Supply",
+        iconPath: SupplyIconPath,
+        activeIconPath: SupplyIconPath,
+        url: "/supply"
+    },
+    {
+        label: "Retirement Trends",
+        iconPath: RetirementTrendsIconPath,
+        activeIconPath: RetirementTrendsIconPath,
+        url: "/retirement_trends"
+    },
+    {
+        label: "Token Details",
+        iconPath: TokenDetailsIconPath,
+        activeIconPath: RetirementTrendsIconPath,
+        url: "/token_details"
+    },
+];
