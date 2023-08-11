88295ccc6bf0efd02851bef79a9d2ca772a2b76a
biwano <biwano@gmail.com>
2023-08-11 18:15:42 +0200

    Responsive charts

--- carbonmark-data/components/Charts/styles.ts
+++ carbonmark-data/components/Charts/styles.ts
@@ -6,9 +6,15 @@ export const chartRow = css`
 export const chartContainer = css`
     box-shadow: 0px 1px 3px 0px #0000001F;
     min-height: 284px;
+    height: 284px;
     background-color: var(--surface-04);
     padding: 12px;
+    display: flex;
+    flex-flow: column;
     h3 {
         margin-bottom: 24px;
+    }
+    div {
+        flex: 1 1 auto;
     }
 `
