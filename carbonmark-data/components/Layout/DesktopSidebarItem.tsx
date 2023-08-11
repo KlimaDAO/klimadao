409fe6c7be0c69c89672f5c23418fcf6f05f1304
biwano <biwano@gmail.com>
2023-08-11 15:26:08 +0200

    Imporved Navigation

---
+++ carbonmark-data/components/Layout/DesktopSidebarItem.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { DesktopSidebarItemIcon } from "./DesktopSidebarItemIcon";
+import { NavItem } from "./NavItems";
+import * as styles from "./styles";
+
+interface Props {
+    navItem: NavItem,
+    key?: string
+}
+
+export const DesktopSidebarItem: FC<Props> = ({ navItem }) => {
+    return (
+        <a className={styles.desktopSidebarItem} href={navItem.url}>
+            <DesktopSidebarItemIcon>{navItem.iconPath()}</DesktopSidebarItemIcon>
+            {navItem.label}
+        </a>
+    );
+};
