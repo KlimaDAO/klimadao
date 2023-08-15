4e84dde5258d8e628ebc224dd6264bda03e44517
biwano <biwano@gmail.com>
2023-08-15 17:02:16 +0200

    Makes translations work again

--- carbonmark-data/components/Layout/NavItems.tsx
+++ carbonmark-data/components/Layout/NavItems.tsx
@@ -15,35 +15,37 @@ export interface NavItem {
     activeIconPath: JSX.Element;
     url: string;
 }
-export const navItems: Array<NavItem> = [
-    {
-        label: t`Overview`,
-        iconPath: OverviewIconPath(),
-        activeIconPath: OverviewFilledIconPath(),
-        url: "/"
-    },
-    {
-        label: "Off vs On-Chain",
-        iconPath: OffVsOnIconPath(),
-        activeIconPath: OffVsOnIconPath(),
-        url: "/off_vs_on_chain"
-    },
-    {
-        label: "Supply",
-        iconPath: SupplyIconPath(),
-        activeIconPath: SupplyIconPath(),
-        url: "/supply"
-    },
-    {
-        label: "Retirement Trends",
-        iconPath: RetirementTrendsIconPath(),
-        activeIconPath: RetirementTrendsIconPath(),
-        url: "/retirement_trends"
-    },
-    {
-        label: "Token Details",
-        iconPath: TokenDetailsIconPath(),
-        activeIconPath: RetirementTrendsIconPath(),
-        url: "/token_details"
-    },
-];
+export const navItems = (): Array<NavItem> => {
+    return [
+        {
+            label: t`Overview`,
+            iconPath: OverviewIconPath(),
+            activeIconPath: OverviewFilledIconPath(),
+            url: "/"
+        },
+        {
+            label: t`Off vs On-Chain`,
+            iconPath: OffVsOnIconPath(),
+            activeIconPath: OffVsOnIconPath(),
+            url: "/off_vs_on_chain"
+        },
+        {
+            label: t`Supply`,
+            iconPath: SupplyIconPath(),
+            activeIconPath: SupplyIconPath(),
+            url: "/supply"
+        },
+        {
+            label: t`Retirement Trends`,
+            iconPath: RetirementTrendsIconPath(),
+            activeIconPath: RetirementTrendsIconPath(),
+            url: "/retirement_trends"
+        },
+        {
+            label: t`Token Details`,
+            iconPath: TokenDetailsIconPath(),
+            activeIconPath: RetirementTrendsIconPath(),
+            url: "/token_details"
+        },
+    ];
+}
