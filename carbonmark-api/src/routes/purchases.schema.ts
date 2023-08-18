e3f899f8bf9d8871cd5f12ea01603155af6d66f7
Atmosfearful <atmosfearful@protonmail.com>
2023-08-17 17:25:59 -0700

    improve example for Purchase

--- carbonmark-api/src/routes/purchases.schema.ts
+++ carbonmark-api/src/routes/purchases.schema.ts
@@ -10,37 +10,49 @@ const Purchase = Type.Object({
   }),
   amount: Type.String({
     description: "Stringified 18 decimal BigNumber",
+    examples: ["1000000000000000000"],
   }),
   buyer: Type.Object({
     id: Type.String({
       description: "Address of the buyer",
+      examples: ["0xAAA699f2098ac92c2f4914979fcb22aba86d259"],
     }),
   }),
   seller: Type.Object({
     id: Type.String({
-      description: "Wallet address of the seller",
+      description: "Address of the seller",
+      examples: ["0xBBB699f2098ac92c2f4914979fcb22aba86d259"],
     }),
   }),
   listing: Type.Object({
     id: Type.String({
       description: "ID of the listing that was purchased",
-      examples: ["0x1234"],
+      examples: ["0x1"],
     }),
     project: Type.Object({
-      country: Type.String(),
+      country: Type.String({
+        examples: ["China"],
+      }),
       key: Type.String({
         examples: ["VCS-191"],
       }),
-      methodology: Type.String(),
-      name: Type.String(),
+      methodology: Type.String({
+        examples: ["ACM0002"],
+      }),
+      name: Type.String({
+        examples: ["4×50 MW Dayingjiang- 3 Hydropower Project Phases 1&2"],
+      }),
       projectID: Type.String({
         examples: ["191"],
       }),
-      vintage: Type.String(),
+      vintage: Type.String({
+        examples: ["2008"],
+      }),
     }),
   }),
   price: Type.String({
     description: "Stringified 6 decimal BigNumber",
+    examples: ["1000000"],
   }),
 });
 
@@ -56,29 +68,6 @@ const Params = Type.Object({
 });
 
 type PurchaseParams = Static<typeof Params>;
-
-const exampleReturn: PurchaseResponse = {
-  id: "0xcad9383fba33aaad6256304ef7b103f3f00b21afbaffbbff14423bf074b699e8",
-  amount: "1000000000000000000",
-  buyer: {
-    id: "0x1234",
-  },
-  seller: {
-    id: "0x1234",
-  },
-  listing: {
-    id: "0x1234",
-    project: {
-      country: "China",
-      key: "VCS-191",
-      methodology: "ACM0002",
-      name: "4×50 MW Dayingjiang- 3 Hydropower Project Phases 1&2",
-      projectID: "191",
-      vintage: "2008",
-    },
-  },
-  price: "230000",
-};
 
 const schema: FastifySchema = {
   summary: "Purchase details",
@@ -105,7 +94,6 @@ const schema: FastifySchema = {
           schema: Purchase,
         },
       },
-      examples: [exampleReturn],
     },
   },
 };
