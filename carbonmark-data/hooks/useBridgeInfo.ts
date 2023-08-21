69b6ae156a2df3d3fe542acc737ca5660a7dd27f
biwano <biwano@gmail.com>
2023-08-21 16:01:58 +0200

    Improved hooks code.

--- carbonmark-data/hooks/useBridgeInfo.ts
+++ carbonmark-data/hooks/useBridgeInfo.ts
@@ -2,7 +2,7 @@ import { useParams } from "next/navigation";
 
 // Returns bridge info given a bridge parameter in the url
 function useBridgeInfo(): { bridge: string; bridgeLabel: string } {
-  const bridge = useParams().bridge;
+  const { bridge } = useParams();
   // Captalize bridge
   const bridgeLabel = bridge.charAt(0).toUpperCase() + bridge.slice(1);
   return {
--- carbonmark-data/hooks/useChainInfo.ts
+++ carbonmark-data/hooks/useChainInfo.ts
@@ -2,7 +2,7 @@ import { useParams } from "next/navigation";
 
 // Returns bridge info given a bridge parameter in the url
 function useChainInfo(): { chain: string; chainLabel: string } {
-  const chain = useParams().chain;
+  const { chain } = useParams();
   // Captalize chain
   const chainLabel = chain.charAt(0).toUpperCase() + chain.slice(1);
   return {
