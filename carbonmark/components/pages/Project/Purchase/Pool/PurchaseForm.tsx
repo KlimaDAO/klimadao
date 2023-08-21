956afbe7ee8bf97408921638997e4b994d7ef176
0xMakka <keith.33m@gmail.com>
2023-08-21 10:18:06 +0100

    add checks on input value

--- carbonmark/components/pages/Project/Purchase/Pool/PurchaseForm.tsx
+++ carbonmark/components/pages/Project/Purchase/Pool/PurchaseForm.tsx
@@ -108,8 +108,13 @@ export const PurchaseForm: FC<Props> = (props) => {
     }
   };
 
-  const getApprovalValue = () =>
-    getPoolApprovalValue(inputValues?.totalPrice!, inputValues?.paymentMethod!);
+  const getApprovalValue = () => {
+    if (!inputValues?.totalPrice || !inputValues.paymentMethod) return "0";
+    return getPoolApprovalValue(
+      inputValues.totalPrice,
+      inputValues.paymentMethod
+    );
+  };
 
   // compare with total price including fees
   const hasApproval = () => {
