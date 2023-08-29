221b34814b244c1a3cc546cf3dfff16269a74aaa
biwano <biwano@gmail.com>
2023-08-29 15:16:44 +0200

    Standard two columns layout

--- carbonmark-data/app/[locale]/overview/page.tsx
+++ carbonmark-data/app/[locale]/overview/page.tsx
@@ -4,14 +4,13 @@ import layout from "theme/layout.module.scss";
 
 import HistoricalPriceCard from "components/cards/HistoricalPriceCard";
 import TokenizedCreditsByBridgeCard from "components/cards/TokenizedCreditsByBridgeCard";
-import styles from "./styles.module.scss";
 
 /** Overview page (index/landing page) captured via rewrite in next.config.js*/
 export default function OverviewPage() {
   return (
     <div>
       <h1>{t`State of the digital carbon market`}</h1>
-      <div className={styles.global}>
+      <div className={layout.twoColumns}>
         <div className={layout.cardStackedRows}>
           <div className={layout.cardRow}>
             <VerraCreditsCard></VerraCreditsCard>
--- carbonmark-data/app/[locale]/trends/by-token/page.tsx
+++ carbonmark-data/app/[locale]/trends/by-token/page.tsx
@@ -1,5 +1,21 @@
 import RetirementTrendsPage from "components/pages/RetirementTrendsPage";
+import layout from "theme/layout.module.scss";
 
 export default function RetirementTrendsByTokenPage() {
-  return <RetirementTrendsPage>By Token…</RetirementTrendsPage>;
+  return (
+    <RetirementTrendsPage>
+      <div className={layout.twoColumns}>
+        <div className={layout.cardStackedRows}>
+          <div className={layout.cardRow}>KlimaDAO Retirements by Pool</div>
+          <div className={layout.cardRow}>KlimaDAO Retirements by Pool</div>
+          <div className={layout.cardRow}>
+            Detailed list of KlimaDAO Retirements
+          </div>
+        </div>
+        <div className={layout.cardStackedRows}>
+          Carbon Pool Redemptions / Retirements
+        </div>
+      </div>
+    </RetirementTrendsPage>
+  );
 }
