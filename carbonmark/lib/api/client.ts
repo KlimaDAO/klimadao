7e51cfff180abc98b2d756c058a621ee441984c9
emc <0xemc@protonmail.com>
2023-09-07 19:10:58 +1000

    formatting fix

--- carbonmark/lib/api/client.ts
+++ carbonmark/lib/api/client.ts
@@ -1,5 +1,4 @@
-import type schema from '.generated/carbonmark-api.schema'
-import { createClient, type NormalizeOAS } from 'fets'
+import type schema from ".generated/carbonmark-api.schema";
+import { createClient, type NormalizeOAS } from "fets";
 
-export const client = createClient<NormalizeOAS<typeof schema>>({})
- 
+export const client = createClient<NormalizeOAS<typeof schema>>({});
