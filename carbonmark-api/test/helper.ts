713bdf62d7206b14676dfef73d62a9d178475233
emc <0xemc@protonmail.com>
2023-09-09 17:37:30 +1000

    test fixes

--- carbonmark-api/test/helper.ts
+++ carbonmark-api/test/helper.ts
@@ -13,7 +13,7 @@ type Args = {
  */
 export async function build(args?: Args) {
   // Create a new Fastify instance
-  let fastify: FastifyInstance = Fastify({ logger: true });
+  let fastify: FastifyInstance = Fastify({ logger: false });
 
   // Register the application with the Fastify instance
   await fastify.register(app);
