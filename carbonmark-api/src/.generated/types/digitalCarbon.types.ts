22761866db37742b1f03491bbf57a71b630f0476
Atmosfearful <atmosfearful@protonmail.com>
2023-09-11 06:13:48 -0700

    regenerate types

--- carbonmark-api/src/.generated/types/digitalCarbon.types.ts
+++ carbonmark-api/src/.generated/types/digitalCarbon.types.ts
@@ -1,3 +1,5 @@
+import { GraphQLClient } from 'graphql-request';
+import { GraphQLClientRequestHeaders } from 'graphql-request/build/cjs/types';
 import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
