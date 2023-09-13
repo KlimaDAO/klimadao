f5b69fa32eeadab39f9693d43f94eaa17400944b
Atmosfearful <atmosfearful@protonmail.com>
2023-09-13 16:05:59 -0700

    add testnet usdc

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -52,7 +52,7 @@ const testnet: typeof mainnet = {
   pklima_exercise: "0xBCE4486256bb306BF49e43DfdaFBc0A6660e95F9",
   sklima: "0xDe0cD0D51b9981BaB50DB974a1877c1C01b86e91",
   wsklima: "",
-  usdc: "0x284A5F4d90a49F7eb21C055eA3C824603314B1E7",
+  usdc: "0xd4a086eD700578dC564B46004Fa92C20d5148B8F",
   klimaUsdcLp: "",
   klimaBctLp: "0xb7225519550ED89C9B36c88d57d6059F698AaE97",
   klimaMco2Lp: "0x64a3b8cA5A7e406A78e660AE10c7563D9153a739",
