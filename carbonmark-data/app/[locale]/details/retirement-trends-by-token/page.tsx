952a6531b040f092cf68aa1ebfb16b1dc92bb7c0
biwano <biwano@gmail.com>
2023-09-15 12:19:29 +0200

    Adds card to the retirements by token detail page

--- carbonmark-data/app/[locale]/details/retirement-trends-by-token/page.tsx
+++ carbonmark-data/app/[locale]/details/retirement-trends-by-token/page.tsx
@@ -1,11 +1,16 @@
 import { t } from "@lingui/macro";
+import KlimaDAORetirementsByTokenBarCard from "components/cards/retirementTrends/KlimaDAORetirementsByTokenBarCard";
 import DetailPage from "components/pages/DetailPage";
 
 export default function RetirementTrendsByTokenDetailPage() {
   return (
     <DetailPage
       pageTitle={t`Retirements by token`}
-      card={<></>}
+      card={
+        <KlimaDAORetirementsByTokenBarCard
+          isDetailPage={true}
+        ></KlimaDAORetirementsByTokenBarCard>
+      }
       overview={t`Lorem Ipsum`}
     />
   );
