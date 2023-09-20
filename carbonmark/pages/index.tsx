e31995700968592badf1ddde6947053c91ebc0d8
Atmosfearful <atmosfearful@protonmail.com>
2023-09-19 23:35:42 -0700

    remove debug

--- carbonmark/pages/index.tsx
+++ carbonmark/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Home } from "components/pages/Home";
 import { getCarbonmarkProject } from "lib/carbonmark";
-import { urls } from "lib/constants";
 import { loadTranslation } from "lib/i18n";
 import { GetStaticProps } from "next";
 
@@ -8,11 +7,9 @@ const defaultProjectKeys = ["VCS-674-2014", "VCS-292-2020", "VCS-981-2017"];
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const translation = await loadTranslation(ctx.locale);
-  console.log("generating", urls.api.projects);
   const projects = await Promise.all(
     defaultProjectKeys.map((project) => getCarbonmarkProject(project))
   );
-  console.log(projects)
   return {
     props: {
       projects,
