8aacdf121c3b4ad39b6d1549031cf6c73244ba5d
biwano <biwano@gmail.com>
2023-09-25 11:17:36 +0200

    Adds a custom legend component

---
+++ carbonmark-data/components/charts/helpers/CustomLegendItem/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+
+/** Renders a legend item
+ * color: color of the dot
+ * text: Text accompanying the dot
+ */
+export default function CustomLegendItem(props: {
+  color: string;
+  text: string;
+}) {
+  return (
+    <div className={styles.legendItem}>
+      <span
+        className={styles.dot}
+        style={{ backgroundColor: props.color }}
+      ></span>
+      {props.text}
+    </div>
+  );
+}
