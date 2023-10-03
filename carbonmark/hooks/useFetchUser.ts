3444f8ecf42e867db0399cd563b539701363345a
emc <0xemc@protonmail.com>
2023-10-04 10:22:29 +1100

    chore:1493 useFetchUser to use api/client

--- carbonmark/hooks/useFetchUser.ts
+++ carbonmark/hooks/useFetchUser.ts
@@ -1,21 +1,21 @@
+import { ClientT, client } from "lib/api/client";
 import { urls } from "lib/constants";
-import { fetcher } from "lib/fetcher";
-import { User } from "lib/types/carbonmark.types";
 import type { SWRConfiguration } from "swr";
 import useSWR from "swr";
 
-type Opts = SWRConfiguration & {
-  network?: "mumbai" | "polygon";
-};
+type GetUserFnT = ClientT["/users/{walletOrHandle}"]["get"];
 
-export const useFetchUser = (address?: string, options?: Opts) => {
-  const { network = "polygon", ...swrConfig } = options || {};
-  const { data, ...rest } = useSWR<User>(
-    address
-      ? `${urls.api.users}/${address}?type=wallet&network=${network}`
-      : null,
-    fetcher,
-    swrConfig
+export const useFetchUser = (
+  args: Parameters<GetUserFnT>[0],
+  options?: SWRConfiguration
+) => {
+  const fetchUser = async () =>
+    (await client["/users/{walletOrHandle}"].get(args)).json();
+
+  const { data, ...rest } = useSWR(
+    `${urls.api.users}/${args.params.walletOrHandle}&network=${args.query?.network}`,
+    fetchUser,
+    options
   );
 
   const carbonmarkUser = !!data?.wallet ? data : null;
--- carbonmark/lib/api/client.ts
+++ carbonmark/lib/api/client.ts
@@ -5,3 +5,5 @@ import { urls } from "lib/constants";
 export const client = createClient<NormalizeOAS<typeof schema>>({
   endpoint: urls.api.base,
 });
+
+export type ClientT = typeof client;
