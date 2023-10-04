187d120f4610b1e789d2a62876f0a7332a64cc00
biwano <biwano@gmail.com>
2023-10-04 20:38:19 +0200

    Set origin of credits table skeleton height

--- carbon/components/cards/tokenDetails/TokenOriginsCard/index.tsx
+++ carbon/components/cards/tokenDetails/TokenOriginsCard/index.tsx
@@ -3,13 +3,18 @@ import ChartCard, { CardProps } from "../../ChartCard";
 
 import { TokenDetailsProps } from "components/cards/tokenDetails/helpers";
 import DataTable from "components/charts/helpers/DataTable";
+import { standardVerticalDataTableHeight } from "components/charts/helpers/DataTable/configurations/helpers";
 import { creditsQueryParamsFromProps } from "lib/charts/aggregators/getAggregatedCreditsByProjects";
 
 export default function TokenOriginsCard(props: CardProps & TokenDetailsProps) {
   const params = creditsQueryParamsFromProps(props);
   const chart = (
     /* @ts-expect-error async Server component */
-    <DataTable configurationKey="TokenOriginsList" params={params} />
+    <DataTable
+      configurationKey="TokenOriginsList"
+      params={params}
+      height={standardVerticalDataTableHeight}
+    />
   );
 
   return (
