2b4f43939813ab10593e75bd2a6d51fb03e404be
Atmosfearful <atmosfearful@protonmail.com>
2023-10-04 10:25:59 -0700

    chore(carbonmark) consume v2.0.0-5

--- carbonmark/.generated/carbonmark-api.schema.ts
+++ carbonmark/.generated/carbonmark-api.schema.ts
@@ -2,7 +2,7 @@ export default {
   "openapi": "3.0.3",
   "info": {
     "title": "Carbonmark REST API",
-    "description": "\nWelcome to the API Reference docs for **version 2.0.0-4** of the Carbonmark REST API. Use this API to view assets, prices, supply, activity and more.\n## Quick start\nBe sure to prefix a version number, otherwise your application will be exposed to breaking changes.\n\n~~~ts\nconst res = await fetch(\"https://v1.api.carbonmark.com/projects\");\nconst projects = await res.json();\n~~~\n\nFor a developer guides and example implementations, or to learn more about Carbonmark and Digital Carbon Market, view our product knowledge base at <a href=\"https://docs.carbonmark.com\">docs.carbonmark.com</a>.\n## \n",
+    "description": "\nWelcome to the API Reference docs for **version 2.0.0-5** of the Carbonmark REST API. Use this API to view assets, prices, supply, activity and more.\n## Quick start\nBe sure to prefix a version number, otherwise your application will be exposed to breaking changes.\n\n~~~ts\nconst res = await fetch(\"https://v1.api.carbonmark.com/projects\");\nconst projects = await res.json();\n~~~\n\nFor a developer guides and example implementations, or to learn more about Carbonmark and Digital Carbon Market, view our product knowledge base at <a href=\"https://docs.carbonmark.com\">docs.carbonmark.com</a>.\n## \n",
     "termsOfService": "https://www.carbonmark.com/blog/terms-of-use",
     "contact": {
       "name": "Support",
@@ -12,7 +12,7 @@ export default {
       "name": "MIT",
       "url": "https://github.com/KlimaDAO/klimadao/blob/main/LICENSE"
     },
-    "version": "2.0.0-4"
+    "version": "2.0.0-5"
   },
   "components": {
     "schemas": {
@@ -2327,7 +2327,7 @@ export default {
           },
           {
             "schema": {
-              "default": "1696379549",
+              "default": "1696438740",
               "type": "string"
             },
             "example": "1620000000",
@@ -2790,7 +2790,7 @@ export default {
           },
           {
             "schema": {
-              "default": "1696379549",
+              "default": "1696438740",
               "type": "string"
             },
             "example": "1620000000",
