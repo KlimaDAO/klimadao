8f7d84f979c0a1a6907d6b2df95f62fec064006c
biwano <biwano@gmail.com>
2023-10-04 14:27:39 +0200

    Fixed Skeleton text not showing

--- carbon/components/Skeleton/index.tsx
+++ carbon/components/Skeleton/index.tsx
@@ -1,7 +1,7 @@
 import { t } from "@lingui/macro";
 import styles from "./styles.module.scss";
 export default function Skeleton(props: { className?: string; text?: string }) {
-  const text = props.text === "undefined" ? t`Fetching data...` : props.text;
+  const text = props.text === undefined ? t`Fetching data...` : props.text;
   return (
     <div className={styles.skeleton + " " + props.className}>
       <p>{text}</p>
