87bd4b73ed3e05594dc029153b9ad5540e93e8af
emc <0xemc@protonmail.com>
2023-10-06 07:18:35 +1100

    chore:1493 Fix client fetcher

--- carbonmark/lib/api/client.ts
+++ carbonmark/lib/api/client.ts
@@ -1,11 +1,21 @@
 import type schema from ".generated/carbonmark-api.schema";
 import { createClient, type NormalizeOAS } from "fets";
 import { urls } from "lib/constants";
-import { fetcher } from "lib/fetcher";
 
 export const client = createClient<NormalizeOAS<typeof schema>>({
   endpoint: urls.api.base,
-  fetchFn: fetcher,
+  // Temporary solution until swr integration #1639 is finished
+  fetchFn: async (...args) => {
+    const res = await fetch(...args);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+      throw new Error((await res.json()).message);
+    }
+
+    return res;
+  },
 });
 
 export type ClientT = typeof client;
