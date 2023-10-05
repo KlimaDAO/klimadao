88665cf0f223810b5ec9e563ccf46c7c3aeba1bc
emc <0xemc@protonmail.com>
2023-10-05 14:15:44 +1100

    chore:1493 PR Fixes

--- carbonmark/hooks/useFetchUser.ts
+++ carbonmark/hooks/useFetchUser.ts
@@ -7,15 +7,20 @@ type GetUserFnT = ClientT["/users/{walletOrHandle}"]["get"];
 
 export const useFetchUser = (
   args: Parameters<GetUserFnT>[0],
-  options?: SWRConfiguration
+  options?: {
+    initOpts: Parameters<GetUserFnT>[1];
+    swrOpts: SWRConfiguration;
+  }
 ) => {
   const fetchUser = async () =>
-    (await client["/users/{walletOrHandle}"].get(args)).json();
+    (
+      await client["/users/{walletOrHandle}"].get(args, options?.initOpts)
+    ).json();
 
   const { data, ...rest } = useSWR(
     `${urls.api.users}/${args.params.walletOrHandle}&network=${args.query?.network}`,
     fetchUser,
-    options
+    options?.swrOpts
   );
 
   const carbonmarkUser = !!data?.wallet ? data : null;
--- carbonmark/lib/api/client.ts
+++ carbonmark/lib/api/client.ts
@@ -1,9 +1,11 @@
 import type schema from ".generated/carbonmark-api.schema";
 import { createClient, type NormalizeOAS } from "fets";
 import { urls } from "lib/constants";
+import { fetcher } from "lib/fetcher";
 
 export const client = createClient<NormalizeOAS<typeof schema>>({
   endpoint: urls.api.base,
+  fetchFn: fetcher,
 });
 
 export type ClientT = typeof client;
