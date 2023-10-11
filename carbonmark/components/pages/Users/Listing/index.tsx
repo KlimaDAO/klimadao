2d3e32aecb7cc69bc9708c93f6e1a6ba701a0be0
Atmosfearful <atmosfearful@protonmail.com>
2023-10-11 15:35:18 -0700

    feat(Listing): connect badges

--- carbonmark/components/pages/Users/Listing/index.tsx
+++ carbonmark/components/pages/Users/Listing/index.tsx
@@ -24,6 +24,9 @@ export const Listing: FC<Props> = (props) => {
   const { locale } = useRouter();
   const project = props.listing.project;
   const category = project?.category as CategoryName;
+  const isExpired =
+    Number(props.listing.expiration) <= Math.floor(Date.now() / 1000);
+  const isInvalid = false; // TODO https://github.com/KlimaDAO/klimadao/issues/1586
 
   return (
     <Card>
@@ -34,8 +37,8 @@ export const Listing: FC<Props> = (props) => {
           <ProjectKey projectKey={project.key} />
         </div>
         <div>
-          <Badge type="Invalid" />
-          <Badge type="Expired" />
+          {isInvalid && <Badge type="Invalid" />}
+          {isExpired && <Badge type="Expired" />}
         </div>
       </div>
       <Link href={createProjectLink(project)}>
