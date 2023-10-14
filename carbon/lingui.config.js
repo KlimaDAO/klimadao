4fc24e8b6ae784b1896da14f14c72f05ca498510
Atmosfearful <atmosfearful@protonmail.com>
2023-10-14 05:41:53 -0700

    TRANSLATIONIO_KEY_CARBON_DATA

--- carbon/lingui.config.js
+++ carbon/lingui.config.js
@@ -2,17 +2,16 @@
 const config = require("../lingui.config.js");
 
 const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
-const TRANSLATIONIO_KEY_CARBONMARK_DATA =
-  process.env.TRANSLATIONIO_KEY_CARBONMARK_DATA;
+const TRANSLATIONIO_KEY_CARBON_DATA = process.env.TRANSLATIONIO_KEY_CARBON_DATA;
 
-if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_CARBONMARK_DATA) {
+if (GITHUB_WORKSPACE && !TRANSLATIONIO_KEY_CARBON_DATA) {
   console.log("Running on GITHUB without the API key => exit");
   throw new Error("ON GITHUB WITHOUT THE API KEY");
-} else if (TRANSLATIONIO_KEY_CARBONMARK_DATA) {
+} else if (TRANSLATIONIO_KEY_CARBON_DATA) {
   // Adds translation io service if env var is available
   config.service = {
     name: "TranslationIO",
-    apiKey: TRANSLATIONIO_KEY_CARBONMARK_DATA,
+    apiKey: TRANSLATIONIO_KEY_CARBON_DATA,
   };
 }
 
