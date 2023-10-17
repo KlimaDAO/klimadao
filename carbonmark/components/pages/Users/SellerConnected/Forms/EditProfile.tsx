5ffc2713c9408fa4d164b8f9d3f93e87bf47e382
emc <0xemc@protonmail.com>
2023-10-17 20:16:26 +1100

    format-all

--- carbonmark/components/pages/Users/SellerConnected/Forms/EditProfile.tsx
+++ carbonmark/components/pages/Users/SellerConnected/Forms/EditProfile.tsx
@@ -147,23 +147,23 @@ export const EditProfile: FC<Props> = (props) => {
               "handle",
               !isExistingUser // validate only if handle can be changed
                 ? {
-                  required: {
-                    value: true,
-                    message: t`Handle is required`,
-                  },
-                  pattern: {
-                    value: VALID_HANDLE_REGEX, // no special characters!
-                    message: t`Handle should not contain any special characters`,
-                  },
-                  validate: {
-                    isAddress: (v) =>
-                      !isAddress(v) || // do not allow polygon addresses
-                      t`Handle should not be an address`,
-                    isNewHandle: async (v) =>
-                      (await fetchIsNewHandle(v)) || // ensure unique handles
-                      t`Sorry, this handle already exists`,
-                  },
-                }
+                    required: {
+                      value: true,
+                      message: t`Handle is required`,
+                    },
+                    pattern: {
+                      value: VALID_HANDLE_REGEX, // no special characters!
+                      message: t`Handle should not contain any special characters`,
+                    },
+                    validate: {
+                      isAddress: (v) =>
+                        !isAddress(v) || // do not allow polygon addresses
+                        t`Handle should not be an address`,
+                      isNewHandle: async (v) =>
+                        (await fetchIsNewHandle(v)) || // ensure unique handles
+                        t`Sorry, this handle already exists`,
+                    },
+                  }
                 : undefined
             ),
           }}
