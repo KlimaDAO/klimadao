92b0be4e91a85a82d125fcfe029ae86f2b4b5c72
0xMakkkka <92357475+0xMakka@users.noreply.github.com>
2023-10-19 22:55:31 +0100

    Makka / 1711 add confirm text to create listing modal (#1721)
    * add confirm text to create listing modal
    
    * remove text and replace with new sentence

--- carbonmark/components/CreateListing/index.tsx
+++ carbonmark/components/CreateListing/index.tsx
@@ -155,14 +155,14 @@ export const CreateListing: FC<Props> = (props) => {
             transferred out of your wallet when a sale is completed.
           </Trans>
         </Text>
-        {getTotalAssetApproval(inputValues) > Number(inputValues?.amount) && (
-          <Text t="body1" color="lighter">
+        <Text t="body1" color="lighter">
+          <strong>
             <Trans>
-              The value below reflects the sum of all of your listings for this
-              specific token.
+              The Confirm amount below reflects the sum of all your listings for
+              this specific token.
             </Trans>
-          </Text>
-        )}
+          </strong>
+        </Text>
       </div>
     );
   };
