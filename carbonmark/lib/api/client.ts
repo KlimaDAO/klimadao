6ab4fa35c4b946b8c41e699d8449a44ce49bd911
emc <0xemc@protonmail.com>
2023-10-24 10:58:47 +1000

    Update client

--- carbonmark/lib/api/client.ts
+++ carbonmark/lib/api/client.ts
@@ -1,21 +1,21 @@
 import { urls } from "lib/constants";
 
-export type RequestConfig<Data = unknown> = {
+export type RequestConfig<TVariables = unknown> = {
   method: "get" | "put" | "patch" | "post" | "delete";
   url: string;
   params?: unknown;
-  body?: Data;
+  data?: TVariables;
   headers?: HeadersInit;
 };
 
-export const fetchClient = async <TData>(
-  request: RequestConfig
+export const fetchClient = async <TData, TVariables = unknown>(
+  request: RequestConfig<TVariables>
 ): Promise<TData> => {
   const response = await fetch(
     `${urls.api.base}${request.url}${request.params}`,
     {
       method: request.method,
-      body: JSON.stringify(request.body),
+      body: JSON.stringify(request.data),
       headers: {
         "Content-Type": "application/json",
         ...request.headers,
