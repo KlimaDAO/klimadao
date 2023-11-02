e00842ebadf9152a9c1989caf98c468b24f284c2
biwano <biwano@gmail.com>
2023-11-02 10:11:40 +0100

    carbon: Added a hool to handle URL persistent state

--- carbon/components/OptionsSwitcher/index.tsx
+++ carbon/components/OptionsSwitcher/index.tsx
@@ -7,8 +7,9 @@
  * value: initial value of the widget
  * className: className to apply to the widget
  */
+import { useQueryParam } from "hooks/useQueryParam";
 import { OptionChangeHandler, Options } from "lib/charts/options";
-import { Key, useEffect, useState } from "react";
+import { Key } from "react";
 import styles from "./styles.module.scss";
 
 export default function OptionsSwitcher<T extends Key>(props: {
@@ -19,43 +20,14 @@ export default function OptionsSwitcher<T extends Key>(props: {
   className?: string;
   readonly?: boolean;
 }) {
-  // The widget initial value is the first option value or the value from the url query
-  let initialValue = props.value || props.options[0].value;
-  if ("undefined" !== typeof window) {
-    const url = new URL(window.location.href);
-    initialValue = (url.searchParams.get(props.name) as T) || initialValue;
-  }
-  const [selected, setSelected] = useState<T>(
-    props.value || props.options[0].value
-  );
   const readonly = props.readonly ?? false;
+  const [selected, setSelected] = useQueryParam<T>({
+    key: props.name,
+    defaultValue: props.value || props.options[0].value,
+    readonly,
+    onValueChange: props.onSelectionChange,
+  });
 
-  // Notify the parent of the initial value
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    initialValue = (url.searchParams.get(props.name) as T) || initialValue;
-    setSelected(initialValue);
-    if (!readonly && props.onSelectionChange) {
-      props.onSelectionChange(initialValue);
-    }
-  }, []);
-
-  const select: OptionChangeHandler<T> = (value) => {
-    if (props.readonly) return;
-    // Change value
-    setSelected(value);
-
-    // Update URL if possible
-    if ("undefined" !== typeof history.pushState) {
-      const url = new URL(window.location.href);
-      url.searchParams.set(props.name, String(value));
-      history.pushState({}, document.title, url.href);
-    }
-    // Notify parent
-    if (!readonly && props.onSelectionChange) {
-      return props.onSelectionChange(value);
-    }
-  };
   return (
     // FIXME: this doesn't let you override certain styles
     <ul
@@ -66,7 +38,7 @@ export default function OptionsSwitcher<T extends Key>(props: {
         .map((option) => (
           <li
             key={option.value}
-            onClick={() => select(option.value)}
+            onClick={() => setSelected(option.value)}
             data-selected={selected == option.value}
           >
             {option.label}
