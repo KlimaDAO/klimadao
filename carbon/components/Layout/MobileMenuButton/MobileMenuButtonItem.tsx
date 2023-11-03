3fc53c073dc1fe5759f006c12b1572746c817f84
biwano <biwano@gmail.com>
2023-11-03 17:04:46 +0100

    [Klima data] Fix for mobile menu (#1814)
    * Added a nav menu pop up
    
    * carbon: use drawer for the mobile menu button
    
    * carbon: Refactor layout
    
    * carbon removed unused css class fullwidth
    
    * carbon: Hide drawer when clicked
    
    * carbon: Makes mobile menu follow designs
    
    * carbon: make mobile menu closable via the button
    
    * carbon: replace redirections with shallow pages
    
    * carbon: hide language button
    
    * carbon: use flex-start instead of start in cardHeader css

---
+++ carbon/components/Layout/MobileMenuButton/MobileMenuButtonItem.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@mui/material";
+import Link from "components/Link";
+import { useIsPathnameActive } from "hooks/useIsPathnameActive";
+import { FC } from "react";
+import { NavItem } from "../NavItems";
+import styles from "./styles.module.scss";
+
+interface Props {
+  navItem: NavItem;
+}
+export const MobileMenuButtonItem: FC<Props> = (props) => {
+  const active = useIsPathnameActive(props.navItem.url);
+  return (
+    <Link href={props.navItem.url}>
+      <Button className={styles.button} aria-selected={active}>
+        <span>{props.navItem.icon}</span>
+        {props.navItem.label}
+      </Button>
+    </Link>
+  );
+};
