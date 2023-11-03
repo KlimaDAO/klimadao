ff2d3888a97d930b2f5b5abb0b154bf5f2d5e596
emc <0xemc@protonmail.com>
2023-11-03 17:21:46 +1100

    Square buttons and assets

--- examples/retirement-demo/src/components/form.tsx
+++ examples/retirement-demo/src/components/form.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { Autocomplete, Button, TextField } from "@mui/material";
-import { useState } from "react";
+import { range } from "lodash";
+import { random } from "lodash/fp";
+import { useEffect, useState } from "react";
 
-const assets = [
-    { id: 1, name: "Asset 1" },
-    { id: 2, name: "Asset 2" },
-    { id: 3, name: "Asset 3" }
-];
 
 const cities = [
     { id: 1, label: "New York" },
@@ -19,7 +16,16 @@ const cities = [
 export default function Form() {
     const [estimate, setEstimate] = useState(0)
     const [distance, setDistance] = useState(0)
-    return <form className="grid gap-2 relative top-1/3">
+    const [assets, setAssets] = useState<any[]>([])
+    const [asset, setAsset] = useState<any>()
+
+    useEffect(() => {
+        fetch("https://api.carbonmark.com/projects")
+            .then(res => res.json())
+            .then(projects => setAssets(range(3).map(random(projects.length)).map(n => projects[n])))
+    }, [])
+
+    return <div className="flex flex-col gap-2 relative top-1/3 justify-center items-center">
         <Autocomplete
             size="small"
             disablePortal
@@ -36,10 +42,32 @@ export default function Form() {
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="To" />}
         />
+
         <div className="grid p-3">
             <span className="text-center">Distance: {distance} km</span>
             <span className="text-center">Carbon Estimate: {estimate} tonnes</span>
+            <span className="text-center">Offset Cost: ${estimate} USD</span>
         </div>
+        <div className="grid grid-cols-3 gap-3" >
+
+            {assets.map((thisAsset, index) => (
+                <Button
+                    sx={{
+                        width: "150px",
+                        height: "150px",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        background: asset === thisAsset ? "blue" : "transparent"
+                    }}
+                    variant="outlined"
+                    onClick={() => setAsset(thisAsset)}
+                    key={index}
+                >
+                    {thisAsset?.name}
+                </Button>
+            ))}
+        </div>
+
         <Button size="large">Retire</Button>
-    </form>
-}
+    </div>
+} 
