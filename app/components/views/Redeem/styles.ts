56a0eeb5265ba7aa3c5af5f3fb6f130d1a2270a5
biwano <biwano@gmail.com>
2023-11-22 21:18:33 +0100

    app: Fix padding on redeem card

---
+++ app/components/views/Redeem/styles.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/css";
+import breakpoints from "@klimadao/lib/theme/breakpoints";
+
+export const redeemCard = css`
+  ${breakpoints.desktop} {
+    grid-row: 2;
+  }
+`;
