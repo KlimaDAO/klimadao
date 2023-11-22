95a477a7ee18cd5554e4a758db1e61a821daca1c
biwano <biwano@gmail.com>
2023-11-22 10:48:18 +0100

    api: add sentry integration

--- carbonmark-api/api/serverless.ts
+++ carbonmark-api/api/serverless.ts
@@ -3,6 +3,7 @@
  * See: https://www.fastify.io/docs/latest/Guides/Serverless/#vercel
  */
 import Fastify from "fastify";
+import packageJson from "../package.json";
 import app from "../src/app";
 
 // Read the .env file.
@@ -12,6 +13,13 @@ dotenv.config();
 // Instantiate Fastify with some config
 const fastify = Fastify({
   logger: false,
+});
+
+// Registry sentry plugin
+fastify.register(import("@immobiliarelabs/fastify-sentry"), {
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.VERCEL_GIT_COMMIT_REF,
+  release: packageJson.version,
 });
 
 // Register your application as a normal plugin.
