82323f593b23583cf573a78dfb2319c4a667a16c
0xMakkkka <92357475+0xMakka@users.noreply.github.com>
2023-11-27 17:48:45 +0000

    Makka / 1690 remove scrollbar width (#1705)
    * remove scrollbar width
    
    * add outline offset
    
    ---------
    
    Co-authored-by: Michael Collins <0xemc@protonmail.com>

--- carbonmark/components/pages/Portfolio/Retire/CustomizableModal/styles.ts
+++ carbonmark/components/pages/Portfolio/Retire/CustomizableModal/styles.ts
@@ -89,6 +89,7 @@ export const modalContent = (
     border: 0.4rem solid transparent;
     border-radius: 0.8rem;
     background-clip: padding-box;
+    outline-offset: -1px;
   }
 
   &::-webkit-scrollbar {
