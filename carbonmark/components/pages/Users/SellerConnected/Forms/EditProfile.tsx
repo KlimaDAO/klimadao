622ec6ef304bea9e2e47e807f6d1936391b05442
emc <0xemc@protonmail.com>
2023-11-27 12:50:37 +0100

    chore: format-all

--- carbonmark/components/pages/Users/SellerConnected/Forms/EditProfile.tsx
+++ carbonmark/components/pages/Users/SellerConnected/Forms/EditProfile.tsx
@@ -1,4 +1,10 @@
-import { getUsersWalletorhandle, postLogin, postLoginVerify, postUsers, putUsersWallet } from ".generated/carbonmark-api-sdk/clients";
+import {
+  getUsersWalletorhandle,
+  postLogin,
+  postLoginVerify,
+  postUsers,
+  putUsersWallet,
+} from ".generated/carbonmark-api-sdk/clients";
 import { useWeb3 } from "@klimadao/lib/utils";
 import { Trans, t } from "@lingui/macro";
 import { ButtonPrimary } from "components/Buttons/ButtonPrimary";
@@ -91,14 +97,14 @@ export const EditProfile: FC<Props> = (props) => {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${verifyResponse.token}`,
-          }
+          },
         });
       } else {
         response = await postUsers(values, {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${verifyResponse.token}`,
-          }
+          },
         });
       }
 
@@ -150,23 +156,23 @@ export const EditProfile: FC<Props> = (props) => {
               "handle",
               !isExistingUser // validate only if handle can be changed
                 ? {
-                  required: {
-                    value: true,
-                    message: t`Handle is required`,
-                  },
-                  pattern: {
-                    value: VALID_HANDLE_REGEX, // no special characters!
-                    message: t`Handle should not contain any special characters`,
-                  },
-                  validate: {
-                    isAddress: (v) =>
-                      !isAddress(v) || // do not allow polygon addresses
-                      t`Handle should not be an address`,
-                    isNewHandle: async (v) =>
-                      (await fetchIsNewHandle(v)) || // ensure unique handles
-                      t`Sorry, this handle already exists`,
-                  },
-                }
+                    required: {
+                      value: true,
+                      message: t`Handle is required`,
+                    },
+                    pattern: {
+                      value: VALID_HANDLE_REGEX, // no special characters!
+                      message: t`Handle should not contain any special characters`,
+                    },
+                    validate: {
+                      isAddress: (v) =>
+                        !isAddress(v) || // do not allow polygon addresses
+                        t`Handle should not be an address`,
+                      isNewHandle: async (v) =>
+                        (await fetchIsNewHandle(v)) || // ensure unique handles
+                        t`Sorry, this handle already exists`,
+                    },
+                  }
                 : undefined
             ),
           }}
