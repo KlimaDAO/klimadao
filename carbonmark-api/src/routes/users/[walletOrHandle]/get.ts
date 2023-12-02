17714505cffff9ba78b1743e2314ca889651fbb1
psparacino <tufnelenterprises@gmail.com>
2023-12-01 19:42:18 -0500

    clean up logs

--- carbonmark-api/src/routes/users/[walletOrHandle]/get.ts
+++ carbonmark-api/src/routes/users/[walletOrHandle]/get.ts
@@ -28,8 +28,8 @@ const handler = (fastify: FastifyInstance) =>
   ): Promise<User | void> {
     try {
       const { query, params } = request;
-      const network = query.network ?? "polygon";
 
+      const network = query.network ?? "polygon";
       const walletOrHandle = params.walletOrHandle.toLowerCase();
 
       // Fetch the firebase UserProfile first
