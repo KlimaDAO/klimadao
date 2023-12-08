1012ad0a9b86ba55d148145b1e6414f2dff924e3
psparacino <tufnelenterprises@gmail.com>
2023-12-07 22:21:17 -0500

    revert to old digitalCarbon subgraph, new one missing

--- carbonmark-api/src/app.constants.ts
+++ carbonmark-api/src/app.constants.ts
@@ -13,7 +13,9 @@ const POLYGON_URLS = {
   marketplace: `${GRAPH_API_ROOT_ID}/QmTfM5TvokuyKXCGWL3wTLZ1z4TqRtQ7PdVfQRVU39ovUC`,
   assets: `${GRAPH_API_ROOT}/cujowolf/klima-refi-current-holdings`,
   tokens: `${GRAPH_API_ROOT}/klimadao/klimadao-pairs`,
-  digitalCarbon: `${GRAPH_API_ROOT_ID}/Qmeu2aQpLb89xn8QXaQv4efCtBm1oKzcTri37SGkjp6tXG`,
+  // Temp revert because old subgraph down
+  // digitalCarbon: `${GRAPH_API_ROOT_ID}/Qmeu2aQpLb89xn8QXaQv4efCtBm1oKzcTri37SGkjp6tXG`,
+  digitalCarbon: `${GRAPH_API_ROOT}/klimadao/polygon-digital-carbon`,
   icr: `${GRAPH_API_ROOT}/skjaldbaka17/carbon-registry-polygon`,
 };
 
