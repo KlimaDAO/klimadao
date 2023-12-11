e685c6bd96117588001b4d49ddc896e343b63f04
psparacino <tufnelenterprises@gmail.com>
2023-12-11 13:23:54 -0500

    corrected purchase and activites amounts

--- carbonmark-api/src/utils/helpers/fetchMarketplaceListings.ts
+++ carbonmark-api/src/utils/helpers/fetchMarketplaceListings.ts
@@ -82,3 +82,40 @@ export const fetchMarketplaceListings = async (
 
   return [listingsWithProfiles];
 };
+
+// /**
+//  * Query the subgraph for active marketplace listings and project data for the given project
+//  * Filters out deleted, sold-out and inactive listings
+//  * Fetches seller profile info from firebase
+//  */
+// export const fetchMarketplaceListings = async (
+//   sdk: GQL_SDK,
+//   { key, vintage, expiresAfter, fastify }: Params
+// ): Promise<[Listing[], Activity[]]> => {
+//   const { project } = await sdk.marketplace.getProjectById({
+//     projectId: key + "-" + vintage,
+//     expiresAfter: expiresAfter ?? String(Math.floor(Date.now() / 1000)),
+//   });
+//   const filteredListings = project?.listings?.filter(isActiveListing) || [];
+//   const filteredActivities =
+//     project?.activities?.filter(filterUnsoldActivity) || [];
+
+//   const formattedListings = filteredListings.map(formatListing);
+
+//   const formattedActivities = filteredActivities.map((activity) => ({
+//     ...activity,
+//     price: activity.price ? utils.formatUnits(activity.price, 6) : null,
+//     previousPrice: activity.previousPrice
+//       ? utils.formatUnits(activity.previousPrice, 6)
+//       : null,
+//     amount: activity.amount
+//       ? key.startsWith("ICR")
+//         ? activity.amount
+//         : utils.formatUnits(activity.amount, 18)
+//       : null,
+//     previousAmount: activity.previousAmount
+//       ? key.startsWith("ICR")
+//         ? activity.amount
+//         : utils.formatUnits(activity.previousAmount, 18)
+//       : null,
+//   }));
