694433fab41a70bf41484f28389b098f874c4a46
emc <0xemc@protonmail.com>
2023-12-13 20:27:38 +0100

    chore:1998 Remove another set of jest mocks

--- carbonmark-api/test/routes/projects/get.test.ts
+++ carbonmark-api/test/routes/projects/get.test.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { pick, set } from "lodash";
+import { pick } from "lodash";
 import nock from "nock";
 import { GRAPH_URLS, SANITY_URLS } from "../../../src/app.constants";
 import { formatUSDC } from "../../../src/utils/crypto.utils";
@@ -11,35 +11,65 @@ import { DEV_URL } from "../../test.constants";
 
 const mockCmsProject = fixtures.cms.cmsProject;
 const mockCmsProjectContent = fixtures.cms.cmsProjectContent;
-const mockMarketplaceListing = fixtures.marketplace.projectWithListing;
+const mockMarketplaceProject = fixtures.marketplace.projectWithListing;
 
-const poolPrices = {
-  bct: {
-    poolName: "bct",
-    defaultPrice: "1.23456",
-    selectiveRedeemPrice: "2.23456",
-  },
-  nct: {
-    poolName: "nct",
-    defaultPrice: "2.23456",
-    selectiveRedeemPrice: "3.23456",
-  },
-  ubo: {
-    poolName: "ubo",
-    defaultPrice: "3.23456",
-    selectiveRedeemPrice: "4.23456",
-  },
-  nbo: {
-    poolName: "nbo",
-    defaultPrice: "4.23456",
-    selectiveRedeemPrice: "5.23456",
-  },
-};
+// const poolPrices = {
+//   bct: {
+//     poolName: "bct",
+//     defaultPrice: "1.23456",
+//     selectiveRedeemPrice: "2.23456",
+//   },
+//   nct: {
+//     poolName: "nct",
+//     defaultPrice: "2.23456",
+//     selectiveRedeemPrice: "3.23456",
+//   },
+//   ubo: {
+//     poolName: "ubo",
+//     defaultPrice: "3.23456",
+//     selectiveRedeemPrice: "4.23456",
+//   },
+//   nbo: {
+//     poolName: "nbo",
+//     defaultPrice: "4.23456",
+//     selectiveRedeemPrice: "5.23456",
+//   },
+// };
 
-//@todo this is super fragile, need to refactor or use nock
-jest.mock("../../../src/utils/helpers/fetchAllPoolPrices", () => ({
-  fetchAllPoolPrices: jest.fn(() => poolPrices),
-}));
+/**
+ * These prices were taken from:
+ * https://api.thegraph.com/subgraphs/name/klimadao/klimadao-pairs/graphql?query=query+getPoolPrices+%7B%0A++prices%3A+pairs+%7B%0A++++address%3A+id%0A++++price%3A+currentprice%0A++%7D%0A%7D
+ */
+const poolPrices = [
+  {
+    address: "0x1e67124681b402064cd0abe8ed1b5c79d2e02f64",
+    price: "0.311174065811505447018165551152376",
+  },
+  {
+    address: "0x251ca6a70cbd93ccd7039b6b708d4cb9683c266c",
+    price: "0.8529955578518731393522091442647001",
+  },
+  {
+    address: "0x5400a05b8b45eaf9105315b4f2e31f806ab706de",
+    price: "0.6492787471397749867199739284682707",
+  },
+  {
+    address: "0x5786b267d35f9d011c4750e0b0ba584e1fdbead1",
+    price: "1.092628601152088880161020307822194",
+  },
+  {
+    address: "0x64a3b8ca5a7e406a78e660ae10c7563d9153a739",
+    price: "0.7481205011989642003183059989606811",
+  },
+  {
+    address: "0x9803c7ae526049210a1725f7487af26fe2c24614",
+    price: "0.3138886194197642137304571092583792",
+  },
+  {
+    address: "0xdb995f975f1bfc3b2157495c47e4efb31196b2ca",
+    price: "1.171491410337993099122290956500872",
+  },
+];
 
 /** We want to mock network requests without breaking other utils */
 jest.mock("../../../src/routes/projects/get.utils", () => {
@@ -80,6 +110,12 @@ describe("GET /projects", () => {
         },
       })
       .persist();
+
+    nock(GRAPH_URLS["polygon"].tokens)
+      .post("")
+      .reply(200, {
+        data: { prices: poolPrices },
+      });
   });
   afterEach(async () => await fastify.close());
 
@@ -147,7 +183,7 @@ describe("GET /projects", () => {
         country: {
           id: mockCmsProject.country,
         },
-        price: poolPrices["bct"].defaultPrice,
+        price: "0.313889",
         listings: null,
         key: digitalCarbon.digitalCarbonProject.projectID,
         location: {
@@ -177,7 +213,7 @@ describe("GET /projects", () => {
 
     nock(GRAPH_URLS["polygon"].marketplace)
       .post("")
-      .reply(200, { data: { projects: [mockMarketplaceListing] } });
+      .reply(200, { data: { projects: [mockMarketplaceProject] } });
 
     const response = await fastify.inject({
       method: "GET",
@@ -215,21 +251,21 @@ describe("GET /projects", () => {
         })),
 
         /** Marketplace Data */
-        vintage: mockMarketplaceListing.vintage,
-        key: mockMarketplaceListing.key,
+        vintage: mockMarketplaceProject.vintage,
+        key: mockMarketplaceProject.key,
         updatedAt: marketplace.projectWithListing.listings?.[0].updatedAt,
         listings: [
           {
-            ...pick(mockMarketplaceListing.listings![0], [
-            "active",
-            "batchPrices",
-            "batches",
-            "deleted",
-            "id",
-            "tokenAddress",
-          ]),
-            updatedAt: Number(mockMarketplaceListing.listings![0].updatedAt),
-            createdAt: Number(mockMarketplaceListing.listings![0].createdAt),
+            ...pick(mockMarketplaceProject.listings![0], [
+              "active",
+              "batchPrices",
+              "batches",
+              "deleted",
+              "id",
+              "tokenAddress",
+            ]),
+            updatedAt: Number(mockMarketplaceProject.listings![0].updatedAt),
+            createdAt: Number(mockMarketplaceProject.listings![0].createdAt),
           },
         ],
         price: "99",
@@ -284,28 +320,27 @@ describe("GET /projects", () => {
       .post("")
       .reply(200, {
         data: {
-          carbonProjects: [
-            {
-              ...digitalCarbon.digitalCarbonProject,
-            },
-          ],
+          carbonProjects: [digitalCarbon.digitalCarbonProject],
         },
       });
-    // override so listing is cheaper
-    const project = set(
-      marketplace.projectWithListing,
-      "listings[0].singleUnitPrice",
-      "1234560"
-    );
     nock(GRAPH_URLS["polygon"].marketplace)
       .post("")
-      .reply(200, { data: { projects: [project] } });
+      .reply(200, { data: { projects: [mockMarketplaceProject] } });
 
     const response = await fastify.inject({
       method: "GET",
       url: `${DEV_URL}/projects`,
     });
     const data = response.json();
-    expect(data[1].price).toStrictEqual(poolPrices.bct.defaultPrice);
+    expect(data[1].price).toStrictEqual(
+      //Because BigInt is converted to a string etc we need to do this nonsense
+      Math.floor(
+        Number(
+          formatUSDC(
+            mockMarketplaceProject.listings?.[0].singleUnitPrice ?? "0"
+          )
+        )
+      ).toString()
+    );
   });
 });
