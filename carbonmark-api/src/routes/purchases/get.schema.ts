58933ed2e99cb2a4f47c1b958c0a3de389be3996
emc <0xemc@protonmail.com>
2023-12-18 17:38:01 +0100

    fix: /purchases route

---
+++ carbonmark-api/src/routes/purchases/get.schema.ts
@@ -0,0 +1,24 @@
+import { Static, Type } from "@sinclair/typebox";
+import { NetworkParamModel } from "src/models/NetworkParam.model";
+import { PurchaseModel } from "src/models/Purchase.model";
+
+export const querystring = Type.Object({
+  network: Type.Optional(Type.Ref(NetworkParamModel)),
+});
+export type Querystring = Static<typeof querystring>;
+export const schema = {
+  summary: "Purchase details",
+  description:
+    "Retrieve the details of a purchase by its ID (transaction hash)",
+  querystring,
+  response: {
+    200: {
+      description: "Successful response with listing details",
+      content: {
+        "application/json": {
+          schema: Type.Array(PurchaseModel),
+        },
+      },
+    },
+  },
+};
