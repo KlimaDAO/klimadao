43e611c2396ba16592de8087d171036abfc6fa88
psparacino <tufnelenterprises@gmail.com>
2024-01-08 10:25:33 -0500

    api: remove unneeded files

--- carbonmark-api/src/utils/ICR/icr.utils.ts
+++ carbonmark-api/src/utils/ICR/icr.utils.ts
@@ -1,50 +1,6 @@
-import fetch from "node-fetch";
-import type { NetworkParam } from "../../../src/models/NetworkParam.model";
-import type { IcrProject } from "../ICR/icr.types";
-import { ICR_API } from "./ICR_API_endpoints";
-
 export const convertIcrCountryCodeToName = (code: string) => {
   if (!code) return;
   const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
   const country = regionNames.of(code);
   return country;
-};
-
-export const fetchAllICRProjects = async (
-  network: NetworkParam
-): Promise<IcrProject[]> => {
-  const { ICR_API_URL, ICR_API_KEY } = ICR_API(network);
-  let allProjects: IcrProject[] = [];
-  let page = 0;
-  const limit = 50;
-  let shouldFetchProjects = true;
-
-  while (shouldFetchProjects) {
-    const response = await fetch(
-      ICR_API_URL + `/public/projects/list?page=${page}&limit=${limit}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${ICR_API_KEY}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    // remove any projects with no associated carbon credits
-    const filteredProjects: IcrProject[] = data.projects.filter(
-      (project: IcrProject) => project.carbonCredits.length > 0
-    );
-
-    allProjects = [...allProjects, ...filteredProjects];
-    if (filteredProjects.length < limit) {
-      shouldFetchProjects = false;
-      break;
-    }
-    page++;
-  }
-
-  return allProjects;
 };
