733a0f068b210b47bd7774473c31de7f7b4e7536
emc <0xemc@protonmail.com>
2024-02-02 13:26:21 +0100

    fix:2117 Padding & Font

--- carbonmark/components/Footer/styles.ts
+++ carbonmark/components/Footer/styles.ts
@@ -10,7 +10,7 @@ export const footer = (transparent = false) => css`
   padding: 4rem 1.6rem;
 
   ${breakpoints.desktop} {
-    padding: 4rem;
+    padding: 4rem 8rem;
     gap: 3rem;
   }
 
@@ -32,6 +32,9 @@ export const footerNav = css`
   flex-direction: column;
   flex-wrap: wrap;
   gap: 2rem;
+  a {
+    font-family: poppins;
+  }
 
   ${breakpoints.desktop} {
     flex-direction: row;
