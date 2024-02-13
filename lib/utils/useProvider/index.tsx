fb2cd0147ca1a5395a656e49254922d251c7702b
psparacino <tufnelenterprises@gmail.com>
2024-02-13 15:28:46 -0500

    cm: web3 hook returns lowercase address

--- lib/utils/useProvider/index.tsx
+++ lib/utils/useProvider/index.tsx
@@ -150,7 +150,7 @@ export const useProvider = (
       const newState: ConnectedWeb3State = {
         provider,
         signer,
-        address,
+        address: address.toLowerCase(),
         network,
         networkLabel: isTestnetChainId(network.chainId) ? "mumbai" : "polygon",
         isConnected: true,
