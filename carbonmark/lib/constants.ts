b747b7f9194ac80c2628d5cbecda7b810914d3f6
psparacino <tufnelenterprises@gmail.com>
2024-02-16 09:54:28 -0500

    cm: revert api version

--- carbonmark/lib/constants.ts
+++ carbonmark/lib/constants.ts
@@ -28,7 +28,7 @@ const SHORT_COMMIT_HASH = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(
 );
 
 /** When incrementing this API version, be sure to update TypeScript types to reflect API changes */
-export const API_PROD_URL = "https://v7.0.0.api.carbonmark.com";
+export const API_PROD_URL = "https://v6.2.0.api.carbonmark.com";
 
 /**
  * Optional preview URL can be provided via env var.
