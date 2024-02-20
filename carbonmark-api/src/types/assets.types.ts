b3276cd3955b17b3a5413ae0416e8c7e63cccdf6
emc <0xemc@protonmail.com>
2024-02-20 21:57:17 +0100

    chore: migrate carbonmark-api to carbonmark/monorepo

--- carbonmark-api/src/types/assets.types.ts
+++
@@ -1,5 +0,0 @@
-import { GetHoldingsByWalletQuery } from "../.generated/types/assets.types";
-
-export type Holding = NonNullable<
-  GetHoldingsByWalletQuery["accounts"]
->[number]["holdings"][number];
