addef2b07aa1d2ac85f1550f4b7e0ed5e31fa089
0xMakka <keith.33m@gmail.com>
2024-02-21 14:50:06 +0000

    add disclaimer text pt1

--- carbonmark/components/pages/Blog/Post/index.tsx
+++ carbonmark/components/pages/Blog/Post/index.tsx
@@ -102,43 +102,43 @@ export const PostPage = (props: PostProps) => {
                       comment="Long sentence"
                     >
                       The information provided in this blog post pertaining to
-                      KlimaDAO (“KlimaDAO”), its crypto-assets, business assets,
-                      strategy, and operations, is for general informational
-                      purposes only and is not a formal offer to sell or a
-                      solicitation of an offer to buy any securities, options,
-                      futures, or other derivatives related to securities in any
-                      jurisdiction and its content is not prescribed by
-                      securities laws. Information contained in this blog post
-                      should not be relied upon as advice to buy or sell or hold
-                      such securities or as an offer to sell such securities.
-                      This blog post does not take into account nor does it
-                      provide any tax, legal or investment advice or opinion
-                      regarding the specific investment objectives or financial
-                      situation of any person. KlimaDAO and its agents,
-                      advisors, directors, officers, employees and shareholders
-                      make no representation or warranties, expressed or
-                      implied, as to the accuracy of such information and
-                      KlimaDAO expressly disclaims any and all liability that
-                      may be based on such information or errors or omissions
-                      thereof. KlimaDAO reserves the right to amend or replace
-                      the information contained herein, in part or entirely, at
-                      any time, and undertakes no obligation to provide the
-                      recipient with access to the amended information or to
-                      notify the recipient thereof. The information contained in
-                      this blog post supersedes any prior blog post or
-                      conversation concerning the same, similar or related
-                      information. Any information, representations or
-                      statements not contained herein shall not be relied upon
-                      for any purpose. Neither KlimaDAO nor any of its
-                      representatives shall have any liability whatsoever, under
-                      contract, tort, trust or otherwise, to you or any person
-                      resulting from the use of the information in this blog
-                      post by you or any of your representatives or for
-                      omissions from the information in this blog post.
-                      Additionally, KlimaDAO undertakes no obligation to comment
-                      on the expectations of, or statements made by, third
-                      parties in respect of the matters discussed in this blog
-                      post.
+                      Carbonmark (“Carbonmark”), its digital-assets, business
+                      assets, strategy, and operations, is for general
+                      informational purposes only and is not a formal offer to
+                      sell or a solicitation of an offer to buy any securities,
+                      options, futures, or other derivatives related to
+                      securities in any jurisdiction and its content is not
+                      prescribed by securities laws. Information contained in
+                      this blog post should not be relied upon as advice to buy
+                      or sell or hold such securities or as an offer to sell
+                      such securities. This blog post does not take into account
+                      nor does it provide any tax, legal or investment advice or
+                      opinion regarding the specific investment objectives or
+                      financial situation of any person. Carbonmark and its
+                      agents, advisors, directors, officers, employees and
+                      shareholders make no representation or warranties,
+                      expressed or implied, as to the accuracy of such
+                      information and Carbonmark expressly disclaims any and all
+                      liability that may be based on such information or errors
+                      or omissions thereof. Carbonmark reserves the right to
+                      amend or replace the information contained herein, in part
+                      or entirely, at any time, and undertakes no obligation to
+                      provide the recipient with access to the amended
+                      information or to notify the recipient thereof. The
+                      information contained in this blog post supersedes any
+                      prior blog post or conversation concerning the same,
+                      similar or related information. Any information,
+                      representations or statements not contained herein shall
+                      not be relied upon for any purpose. Neither Carbonmark nor
+                      any of its representatives shall have any liability
+                      whatsoever, under contract, tort, trust or otherwise, to
+                      you or any person resulting from the use of the
+                      information in this blog post by you or any of your
+                      representatives or for omissions from the information in
+                      this blog post. Additionally, Carbonmark undertakes no
+                      obligation to comment on the expectations of, or
+                      statements made by, third parties in respect of the
+                      matters discussed in this blog post.
                     </Trans>
                   </Text>
                 </div>
