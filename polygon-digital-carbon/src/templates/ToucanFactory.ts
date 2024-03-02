51716cea135e6cb4ba2d50a797e7021f8c92cdd6
Cujowolf <cujowolf12@gmail.com>
2024-03-02 17:00:34 -0600

    Puro datasource creation

--- polygon-digital-carbon/src/templates/ToucanFactory.ts
+++ polygon-digital-carbon/src/templates/ToucanFactory.ts
@@ -1,19 +1,20 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
-import { ToucanCarbonOffsets } from '../../generated/templates'
+import { ToucanCarbonOffsets, ToucanPuroCarbonOffsets } from '../../generated/templates'
 import { TokenCreated } from '../../generated/ToucanFactory/ToucanCarbonOffsetsFactory'
 import { loadOrCreateCarbonCredit, updateCarbonCreditWithCall } from '../utils/CarbonCredit'
 import { createTokenWithCall } from '../utils/Token'
 
 export function handleNewTCO2(event: TokenCreated): void {
+  ToucanCarbonOffsets.create(event.params.tokenAddress)
   setupNewToucanCredit(event.params.tokenAddress, 'VERRA')
 }
 
 export function handleNewPuroTCO2(event: TokenCreated): void {
+  ToucanPuroCarbonOffsets.create(event.params.tokenAddress)
   setupNewToucanCredit(event.params.tokenAddress, 'PURO_EARTH')
 }
 
 function setupNewToucanCredit(tokenAddress: Address, registry: string, tokenId: BigInt | null = null): void {
-  ToucanCarbonOffsets.create(tokenAddress)
   loadOrCreateCarbonCredit(tokenAddress, 'TOUCAN', null)
   createTokenWithCall(tokenAddress)
   updateCarbonCreditWithCall(tokenAddress, registry)
