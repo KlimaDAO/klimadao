186e3c61e36d298b0c5ca8d680d85bdb05633a1a
Atmosfearful <atmosfearful@protonmail.com>
2024-05-22 18:02:58 -0700

    Remove examples folder

--- examples/nextjs-typescript-integration/utils/getContractInfo.ts
+++
@@ -1,9 +0,0 @@
-const contractInfo = {
-  klimaRetirementAggregatorV2: {
-    address: "0x8cE54d9625371fb2a068986d32C85De8E6e995f8",
-  },
-};
-
-export const getContractInfo = (key: keyof typeof contractInfo) => {
-  return contractInfo[key];
-};
--- examples/nextjs-typescript-integration/utils/isDefaultProjectAddress.ts
+++
@@ -1,9 +0,0 @@
-const knownDefaultProjects = [
-  "0xb139c4cc9d20a3618e9a2268d73eff18c496b991", // bct
-  "0x6362364a37f34d39a1f4993fb595dab4116daf0d", // nct
-  "0xd6ed6fae5b6535cae8d92f40f5ff653db807a4ea", // ubo
-  "0xb6ea7a53fc048d6d3b80b968d696e39482b7e578", // nbo
-].map((addr) => addr.toLowerCase());
-
-export const isDefaultProjectAddress = (projectAddress: string): boolean =>
-  knownDefaultProjects.includes(projectAddress.toLowerCase());
