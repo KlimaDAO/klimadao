ece54795732017f9b73b4880e9a9d3495626eb0e
psparacino <tufnelenterprises@gmail.com>
2024-06-27 14:50:38 -0400

    create account for all holdings

--- polygon-digital-carbon/src/utils/Holding.ts
+++ polygon-digital-carbon/src/utils/Holding.ts
@@ -2,8 +2,10 @@ import { Address, BigInt, Bytes, log } from '@graphprotocol/graph-ts'
 import { Holding, Token } from '../../generated/schema'
 import { ZERO_BI } from '../../../lib/utils/Decimals'
 import { createICRTokenID } from './Token'
+import { loadOrCreateAccount } from './Account'
 
 export function loadOrCreateHolding(account: Address, token: Address, tokenId: BigInt | null): Holding {
+  loadOrCreateAccount(account)
   let id = tokenId !== null ? account.concat(token).concatI32(tokenId.toI32()) : account.concat(token)
 
   let tokenEntityId: Bytes
