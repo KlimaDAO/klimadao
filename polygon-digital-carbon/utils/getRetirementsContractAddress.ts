1c57e2386e8f05f91b40d6351c480d160bc00f27
psparacino <tufnelenterprises@gmail.com>
2024-06-28 13:48:02 -0400

    add conditional for Retirements address for local

--- polygon-digital-carbon/utils/getRetirementsContractAddress.ts
+++ polygon-digital-carbon/utils/getRetirementsContractAddress.ts
@@ -2,7 +2,9 @@ import { AMOY_KLIMA_CARBON_RETIREMENTS_CONTRACT, KLIMA_CARBON_RETIREMENTS_CONTRA
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 
 export function getRetirementsContractAddress(network: string): Address {
-  return network == 'matic' ? KLIMA_CARBON_RETIREMENTS_CONTRACT : AMOY_KLIMA_CARBON_RETIREMENTS_CONTRACT
+  return network == 'matic' || network == 'mainnet'
+    ? KLIMA_CARBON_RETIREMENTS_CONTRACT
+    : AMOY_KLIMA_CARBON_RETIREMENTS_CONTRACT
 }
 
 export function getC3RetireRequestId(fromToken: Address, index: BigInt): string {
