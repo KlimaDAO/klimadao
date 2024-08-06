20823a13424052eff672d1a2069c747a4b0409fa
psparacino <tufnelenterprises@gmail.com>
2024-08-06 16:26:19 -0400

    add eco registry projects to lib

---
+++ lib/utils/EcoRegistryProjectInfo.ts
@@ -0,0 +1,18 @@
+export const ECO_REGISTRY_PROJECT_INFO = [
+  [
+    '0xa8853ffc5a0aeab7d31631a4b87cb12c0b289c6c',
+    'ECO-114',
+    'PROYECTO REDD+ PAZCÍFICO SUR',
+    'MLU-REDD+',
+    'Forestry',
+    'Colombia',
+  ],
+  [
+    '0x4b69a69a80048e321da1d751f0f9dc39b5d63454',
+    'ECO-22',
+    'Proyecto Conservación ARLEQUÍN REDD+',
+    'MLU-REDD+',
+    'Forestry',
+    'Colombia',
+  ],
+]
