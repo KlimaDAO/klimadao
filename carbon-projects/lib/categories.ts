7c86c89f00dce1bab218a75adfa9ca2c4e6695f9
Marcus Aurelius <91024694+0xAurelius@users.noreply.github.com>
2024-09-16 11:42:03 -0400

    Add fields for name and category override to ProjectContent (#2385)

---
+++ carbon-projects/lib/categories.ts
@@ -0,0 +1,11 @@
+export const categories = [
+  { title: "Energy Efficiency", value: "Energy Efficiency" },
+  { title: "Forestry", value: "Forestry" },
+  { title: "Industrial Processing", value: "Industrial Processing" },
+  { title: "Renewable Energy", value: "Renewable Energy" },
+  { title: "Blue Carbon", value: "Blue Carbon" },
+  { title: "Agriculture", value: "Agriculture" },
+  { title: "Construction", value: "Construction" },
+  { title: "Biochar", value: "Biochar" },
+  { title: "Other", value: "Other" },
+];
