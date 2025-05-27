fadb691f8cd99d03b8d3ecae6987b46df79da785
emc <0xemc@protonmail.com>
2025-05-28 04:46:14 +1000

    Banner updates

--- app/components/FairLaunchBanner/index.tsx
+++ app/components/FairLaunchBanner/index.tsx
@@ -1,17 +1,22 @@
+import { Trans } from "@lingui/macro";
 import Link from "next/link";
 import * as styles from "./styles";
 
 export const FairLaunchBanner = () => (
   <div className={styles.fairLaunchBanner}>
-    <div>The Klima 2.0 fair launch period has begun 🎉</div>
     <div>
-      Go to{" "}
-      <Link href="https://app.klimaprotocol.com">app.klimaprotocol.com</Link> to
-      get started or see the{" "}
-      <Link href="klimadao.finance/resources/how-to-join-the-klima-fair-launch-step-by-step-guide-for-klima-holders-and-future-ones">
-        fair launch guide
-      </Link>{" "}
-      to learn more.
+      <Trans>The Klima 2.0 fair launch period has begun 🎉</Trans>
+    </div>
+    <div>
+      <Trans>
+        Go to{" "}
+        <Link href="https://app.klimaprotocol.com">app.klimaprotocol.com</Link>{" "}
+        to get started or see the{" "}
+        <Link href="https://klimadao.finance/resources/how-to-join-the-klima-fair-launch-step-by-step-guide-for-klima-holders-and-future-ones">
+          fair launch guide
+        </Link>{" "}
+        to learn more.
+      </Trans>
     </div>
   </div>
 );
